d706fe81349d3facb98ddefc4376d042e504dcd6
Erik Nijland <e.n.nijland@gmail.com>
2016-09-15 15:02:47 +0200

    Proper alt & title attributes without repeating stuff in the template.

--- modules/map/components/toggle-fullscreen/toggle-fullscreen.component.js
+++ modules/map/components/toggle-fullscreen/toggle-fullscreen.component.js
@@ -12,9 +12,9 @@
             controllerAs: 'vm'
         });
 
-    DpToggleFullscreenController.$inject = ['store', 'ACTIONS'];
+    DpToggleFullscreenController.$inject = ['$scope', 'store', 'ACTIONS'];
 
-    function DpToggleFullscreenController (store, ACTIONS) {
+    function DpToggleFullscreenController ($scope, store, ACTIONS) {
         var vm = this;
 
         vm.toggle = function () {
@@ -23,5 +23,15 @@
                 payload: !vm.isFullscreen
             });
         };
+
+        $scope.$watch('vm.isFullscreen', function () {
+            if (vm.isFullscreen) {
+                vm.buttonIcon = 'minimize';
+                vm.buttonText = 'Kaart verkleinen';
+            } else {
+                vm.buttonIcon = 'fullscreen';
+                vm.buttonText = 'Kaart vergroten';
+            }
+        });
     }
 })();
