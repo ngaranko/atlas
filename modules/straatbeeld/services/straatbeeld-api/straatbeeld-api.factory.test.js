fa804e5f2aec22391611775e637edec1ccf8face
Marco <temp@temps-mbp-2.basis.lan>
2016-09-20 13:41:50 +0200

    Straatbeeld API tests

--- modules/straatbeeld/services/straatbeeld-api/straatbeeld-api.factory.test.js
+++ modules/straatbeeld/services/straatbeeld-api/straatbeeld-api.factory.test.js
@@ -2,7 +2,8 @@ fdescribe('Straatbeeld API Factory', function () {
 
     var straatbeeldApi,
         $http,
-        $httpBackend;
+        $httpBackend,
+        geojson;
 
 
     beforeEach(function () {
@@ -12,13 +13,19 @@ fdescribe('Straatbeeld API Factory', function () {
             },
             sharedConfig: {
                 RADIUS: 100
+            },
+            geojson: {
+                getCenter: function () {
+                    return [52.3747994036985, 4.91359770418102];
+                }
             }
         });
 
-        angular.mock.inject(function (_straatbeeldApi_, _$http_, _$httpBackend_) {
+        angular.mock.inject(function (_straatbeeldApi_, _$http_, _$httpBackend_, _geojson_) {
             straatbeeldApi = _straatbeeldApi_;
             $http = _$http_;
             $httpBackend = _$httpBackend_;
+            geojson = _geojson_;
         });
 
     });
@@ -35,8 +42,8 @@ fdescribe('Straatbeeld API Factory', function () {
 
     describe('restructures API response', function () {
 
-        //httpbackend
-        //
+        var response;
+
         beforeEach(function () {
             $httpBackend.whenGET('http://example.com/example/ABC/?radius=100').respond({
                 images: {
@@ -51,10 +58,10 @@ fdescribe('Straatbeeld API Factory', function () {
                     ]
                 },
                 adjacent: [{
-                        pano_id: 'TMX7315120208-000054_pano_0002_000177',
-                        heading: 116.48,
-                        distance: 10.14
-                    },
+                    pano_id: 'TMX7315120208-000054_pano_0002_000177',
+                    heading: 116.48,
+                    distance: 10.14
+                },
                     {
                         pano_id: 'TMX7315120208-000054_pano_0002_000178',
                         heading: 127.37,
@@ -62,28 +69,50 @@ fdescribe('Straatbeeld API Factory', function () {
                     }],
                 timestamp: '2016-05-19T13:04:15.341110Z'
             });
-        });
 
-        it('converts date string to Javascript date format', function () {
-            var response;
+            spyOn(geojson, 'getCenter').and.callThrough();
 
-            straatbeeldApi.getImageDataById('ABC').then(function(_response_){
-                response = _response_;               
+            straatbeeldApi.getImageDataById('ABC').then(function (_response_) {
+                response = _response_;
             });
 
             $httpBackend.flush();
-            expect(response.date).toBe(new Date('2016-05-19T13:04:15.341110Z'));
+        });
+
+        it('converts date string to Javascript date format', function () {
+
+            expect(response.date).toEqual(new Date('2016-05-19T13:04:15.341110Z'));
 
 
         });
         it('maps hotspot data to proper subset', function () {
+            expect(response.hotspots).toEqual([{
+                panoId: 'TMX7315120208-000054_pano_0002_000177',
+                heading: 116.48,
+                distance: 10.14
+            }, {
+                panoId: 'TMX7315120208-000054_pano_0002_000178',
+                heading: 127.37,
+                distance: 5.25
+            }]);
 
         });
         it('maps geoJson point to lat/lon notation', function () {
 
-        });
-        it('fetches equirectangular image', function () {
+            expect(geojson.getCenter).toHaveBeenCalledWith({
+                type: 'Point',
+                coordinates: [
+                    4.91359770418102,
+                    52.3747994036985,
+                    46.9912552172318
+                ]
+            });
 
+            expect(response.location).toEqual([52.3747994036985, 4.91359770418102]);
+        });
+
+        it('fetches equirectangular image', function () {
+            expect(response.image).toBe('http://example.com/example/plaatje.png');
         });
 
     });
