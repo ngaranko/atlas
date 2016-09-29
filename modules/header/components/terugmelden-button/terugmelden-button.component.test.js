40e7a327ea21709132ff2346953d47096addec98
Boris Arkenaar <boris@arkenaar.com>
2016-09-29 11:48:10 +0200

    mail body voor terumeldmail aangepast

--- modules/header/components/terugmelden-button/terugmelden-button.component.test.js
+++ modules/header/components/terugmelden-button/terugmelden-button.component.test.js
@@ -42,7 +42,11 @@ describe('The atlas-terugmelden-button component', function () {
         expect(component.find('a').attr('href'))
             .toBe('mailto:terugmelding.basisinformatie@amsterdam.nl?subject=Terugmelding%20atlas.amsterdam.nl&body=Te' +
                 'rugmeldingen%20voor%20de%20pagina%3A%20http%3A%2F%2Fwww.example.com%2Fpath%2Ffilename.html%3Ffoo%3Db' +
-                'ar%23baz%0A%0A');
+                'ar%23baz%0A%0ABeschrijf%20zo%20volledig%20mogelijk%20van%20welk%20onjuist%20gegeven%20je%20een%20mel' +
+                'ding%20wilt%20maken%3A%0A-%20Welk%20gegeven%20is%20kennelijk%20onjuist%20of%20ontbreekt%3F%0A-%20Wee' +
+                't%20je%20wat%20het%20wel%20zou%20moeten%20zijn%3F%0A-%20Waarop%20is%20jouw%20constatering%20gebaseer' +
+                'd%3F%20Omschrijf%20de%20reden%20en%20voeg%20indien%20mogelijk%20relevante%20documenten%20in%20de%20b' +
+                'ijlage%20toe%20(bijvoorbeeld%3A%20een%20bouwtekening%2C%20koopakte%2C%20et%20cetera).');
     });
 
     it('has transclude enabled', function () {
