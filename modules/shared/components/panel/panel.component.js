fe03e7574b731b76e2c77df45253c2b44ed170cf
Tiety Kooistra <tietyk@hotmail.com>
2016-10-05 16:45:45 +0200

    panel voor disclaimer, infopanelen en badges

---
+++ modules/shared/components/panel/panel.component.js
@@ -0,0 +1,31 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('dpShared')
+        .component('dpPanel', {
+            bindings: {
+                isPanelVisible: '=',
+                type: '@',
+                canClose: '=',
+                className: '@'
+            },
+            transclude: true,
+            templateUrl: 'modules/shared/components/panel/panel.html',
+            controller: DpPanelController,
+            controllerAs: 'vm'
+        });
+
+    // type :
+    // Danger (rode vormgeving)
+    // Warning (blauwe vormgeving)
+    // Default (grijze vormgeving) als niet wordt toegevoegd
+
+    function DpPanelController() {
+        var vm = this;
+
+        vm.close = function () {
+            vm.isPanelVisible = false;
+        };
+    }
+})();
