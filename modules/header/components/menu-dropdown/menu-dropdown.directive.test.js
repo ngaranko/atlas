07062cb7949f43ef4f0fa403d61e3c57a0ca5465
Erik Nijland <e.n.nijland@gmail.com>
2016-10-13 09:57:56 +0200

    Different options notation

--- modules/header/components/menu-dropdown/menu-dropdown.directive.test.js
+++ modules/header/components/menu-dropdown/menu-dropdown.directive.test.js
@@ -26,7 +26,7 @@ describe('The atlas-menu-dropdown directive', function () {
         });
     });
 
-    function getDirective (hasPrintButton) {
+    function getDirective (options) {
         var directive,
             element,
             scope;
@@ -37,7 +37,7 @@ describe('The atlas-menu-dropdown directive', function () {
         document.body.appendChild(element);
 
         scope = $rootScope.$new();
-        scope.hasPrintButton = hasPrintButton;
+        scope.hasPrintButton = options.hasPrintButton;
 
         directive = $compile(element)(scope);
         scope.$apply();
@@ -46,7 +46,7 @@ describe('The atlas-menu-dropdown directive', function () {
     }
 
     it('should initialize with the dropdown closed', function () {
-        var directive = getDirective(true);
+        var directive = getDirective({hasPrintButton: true});
 
         expect(directive.find('.menu-dropdown').length).toBe(0);
         expect(directive.find('atlas-print-button').length).toBe(0);
@@ -55,7 +55,7 @@ describe('The atlas-menu-dropdown directive', function () {
     });
 
     it('should toggle the visibility of the menu items when you click menu button', function () {
-        var directive = getDirective(true);
+        var directive = getDirective({hasPrintButton: true});
 
         // Click it once
         directive.find('.site-header__menu__item--toggle').eq(0).click();
@@ -76,7 +76,7 @@ describe('The atlas-menu-dropdown directive', function () {
     });
 
     it('changes the styling of the toggle button depending on the state of the dropdown', function () {
-        var directive = getDirective(true);
+        var directive = getDirective({hasPrintButton: true});
 
         // When closed
         expect(directive.find('.site-header__menu__item--toggle').attr('class'))
@@ -90,7 +90,7 @@ describe('The atlas-menu-dropdown directive', function () {
     });
 
     it('should hide the menu items if you click elsewhere on the page', function () {
-        var directive = getDirective(true);
+        var directive = getDirective({hasPrintButton: true});
 
         // Open the dropdown
         directive.find('.site-header__menu__item--toggle').eq(0).click();
@@ -102,8 +102,8 @@ describe('The atlas-menu-dropdown directive', function () {
     });
 
     it('supports multiple, standalone, dropdown menu\'s on one page', function () {
-        var directive1 = getDirective(true),
-            directive2 = getDirective(true);
+        var directive1 = getDirective({hasPrintButton: true}),
+            directive2 = getDirective({hasPrintButton: true});
 
         expect(directive1.find('.menu-dropdown').length).toBe(0);
         expect(directive2.find('.menu-dropdown').length).toBe(0);
@@ -121,12 +121,12 @@ describe('The atlas-menu-dropdown directive', function () {
         var directive;
 
         // With a print button
-        directive = getDirective(true);
+        directive = getDirective({hasPrintButton: true});
         directive.find('.site-header__menu__item--toggle').eq(0).click();
         expect(directive.find('.menu-dropdown').text()).toContain('Printversie');
 
         // Without a print button
-        directive = getDirective(false);
+        directive = getDirective({hasPrintButton: false});
         directive.find('.site-header__menu__item--toggle').eq(0).click();
         expect(directive.find('.menu-dropdown').text()).not.toContain('Printversie');
     });
