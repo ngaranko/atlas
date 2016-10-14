1f1db2c0b8012811e7a4c51488ac0c40f58cc64f
Erik Nijland <e.n.nijland@gmail.com>
2016-10-14 10:45:06 +0200

    The file detail-config.factory.js is now gone. It has been replaced by shared-config.factory.js. It contains straatbeeld-thumbnail configuration. Which is need in both detail and search-results. So used in 2 modules, it belongs in shared.

--- modules/shared/components/straatbeeld-thumbnail/straatbeeld-thumbnail.component.test.js
+++ modules/shared/components/straatbeeld-thumbnail/straatbeeld-thumbnail.component.test.js
@@ -12,7 +12,7 @@ describe('The dp-straatbeeld-thumbnail component', function () {
         angular.mock.module(
             'dpShared',
             {
-                detailConfig: {
+                sharedConfig: {
                     STRAATBEELD_THUMB_URL: 'http://fake.straatbeeld.url/path/',
                     RADIUS: 50,
                     THUMBNAIL_WIDTH: 240
