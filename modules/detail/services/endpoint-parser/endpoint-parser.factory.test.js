91f1f49828a21e65915533cb5c83aedad33456f6
Boris Arkenaar <boris@arkenaar.com>
2016-10-17 12:45:44 +0200

    unit tests toegevoegd voor `getStelselpediaKey`

--- modules/detail/services/endpoint-parser/endpoint-parser.factory.test.js
+++ modules/detail/services/endpoint-parser/endpoint-parser.factory.test.js
@@ -16,19 +16,47 @@ describe('The endpointParser factory', function () {
         });
     });
 
-    it('the getTemplateUrl function returns an template URL based on an endpoint', function () {
-        expect(endpointParser.getTemplateUrl('http://www.api-root.com/bag/nummeraanduiding/123456/'))
-            .toBe('modules/detail/components/detail/templates/bag/nummeraanduiding.html');
+    describe('getTemplateUrl', () => {
+        it('returns a template URL based on an endpoint', () => {
+            expect(endpointParser.getTemplateUrl('http://www.api-root.com/bag/nummeraanduiding/123456/'))
+                .toBe('modules/detail/components/detail/templates/bag/nummeraanduiding.html');
 
-        expect(endpointParser.getTemplateUrl('http://www.api-root.com/brk/object/789/'))
-            .toBe('modules/detail/components/detail/templates/brk/object.html');
+            expect(endpointParser.getTemplateUrl('http://www.api-root.com/brk/object/789/'))
+                .toBe('modules/detail/components/detail/templates/brk/object.html');
 
-        expect(endpointParser.getTemplateUrl('http://www.api-root.com/meetbouten/meetbout/654/'))
-            .toBe('modules/detail/components/detail/templates/meetbouten/meetbout.html');
+            expect(endpointParser.getTemplateUrl('http://www.api-root.com/meetbouten/meetbout/654/'))
+                .toBe('modules/detail/components/detail/templates/meetbouten/meetbout.html');
+        });
+
+        it('has special exceptions for zakelijk recht (BRK)', () => {
+            expect(endpointParser.getTemplateUrl('http://www.api-root.com/brk/zakelijk-recht/' +
+                    'some-id-with-numbers-123456/subject/'))
+                .toBe('modules/detail/components/detail/templates/brk/subject.html');
+        });
     });
 
-    it('getTemplateUrl has special exceptions for zakelijk recht (BRK)', function () {
-        expect(endpointParser.getTemplateUrl('http://www.api-root.com/brk/zakelijk-recht/some-id-with-numbers-123456/' +
-            'subject/')).toBe('modules/detail/components/detail/templates/brk/subject.html');
+    describe('getStelselpediaKey', () => {
+        it('returns a stelselpedia key (uppercased) based on an endpoint', () => {
+            expect(endpointParser.getStelselpediaKey('http://www.api-root.com/bag/nummeraanduiding/123456/'))
+                .toBe('NUMMERAANDUIDING');
+
+            expect(endpointParser.getStelselpediaKey('http://www.api-root.com/brk/object/789/'))
+                .toBe('OBJECT');
+
+            expect(endpointParser.getStelselpediaKey('http://www.api-root.com/meetbouten/meetbout/654/'))
+                .toBe('MEETBOUT');
+        });
+
+        it('turns dashes (-) in the endpoint into underscores (_) in the stelselpedia key', () => {
+            expect(endpointParser.getStelselpediaKey('http://www.api-root.com/brk/object-wkpb/' +
+                    'NL.KAD.OnroerendeZaak.123456/'))
+                .toBe('OBJECT_WKPB');
+        });
+
+        it('has special exceptions for zakelijk recht (BRK)', () => {
+            expect(endpointParser.getStelselpediaKey('http://www.api-root.com/brk/zakelijk-recht/' +
+                    'some-id-with-numbers-123456/subject/'))
+                .toBe('SUBJECT');
+        });
     });
 });
