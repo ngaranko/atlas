7fbf8272872e64d46779908916c9249e5737f41d
Marco Kotrotsos <kotrotsos@gmail.com>
2016-10-18 15:39:16 +0200

    reverted class-names deletion because class-names are ok for qa

---
+++ modules/detail/components/nummeraanduiding/nummeraanduiding.component.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('dpDetail')
+        .component('dpNummeraanduidingHeader', {
+            bindings: {
+                heading: '@',
+                metaData: '=',
+                hoofdadres: '=',
+                verblijfsobjectEndpoint: '@'
+            },
+            templateUrl: 'modules/detail/components/nummeraanduiding/nummeraanduiding.html',
+            controller: DpNummeraanduidingHeaderController,
+            controllerAs: 'vm'
+        });
+
+    DpNummeraanduidingHeaderController.$inject = ['api'];
+
+    function DpNummeraanduidingHeaderController (api) {
+        var vm = this;
+
+        api.getByUrl(vm.verblijfsobjectEndpoint).then(function (vboData) {
+            vm.isGevormd = Number(vboData.status.code) === 18;
+
+            if (vm.isGevormd) {
+                vm.headingHtml = '<em>' + vm.heading + '</em>';
+                vm.statusOmschrijving = vboData.status.omschrijving;
+            }
+        });
+    }
+})();
