930de79847f2ef789d98800670a8002cf3b0278a
vegter <j.vegter@amsterdam.nl>
2016-11-09 09:31:47 +0100

    Max radius to find images set on 10km

--- modules/straatbeeld/services/straatbeeld-api/straatbeeld-api.factory.js
+++ modules/straatbeeld/services/straatbeeld-api/straatbeeld-api.factory.js
@@ -14,7 +14,7 @@
         };
 
         function getImageDataByLocation (location) {
-            const MAX_RADIUS = 1000;
+            const MAX_RADIUS = 10000;
             return api.getByUrl(straatbeeldConfig.STRAATBEELD_ENDPOINT +
                 `?lat=${location[0]}&lon=${location[1]}&radius=${MAX_RADIUS}`)
                 .then(imageData);
