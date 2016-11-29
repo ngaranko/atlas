c5f0de6df56398b1ddf14dff3d563d2ce32bc7fd
Erik Nijland <e.n.nijland@gmail.com>
2016-11-29 11:47:06 +0100

    Making sure the u-align--right class is added to a div (is has no effect on a span).

--- modules/data-selection/components/formatter/align-right/align-right.filter.test.js
+++ modules/data-selection/components/formatter/align-right/align-right.filter.test.js
@@ -12,6 +12,7 @@ describe('The alignRight filter', function () {
     it('wraps a div with class u-align-right around the input (String)', function () {
         const output = angular.element(alignRightFilter('some text'));
 
+        expect(output[0].tagName).toBe('DIV');
         expect(output.attr('class')).toBe('u-align--right');
         expect(output.text()).toBe('some text');
     });
