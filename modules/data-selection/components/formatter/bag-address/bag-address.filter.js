94d759ab838ec204052ddc5dfbaf8fd06250854a
Erik Nijland <e.n.nijland@gmail.com>
2016-11-29 11:52:45 +0100

    Different structure. Nummer is always the same here, no longer part of the ternary syntax.

--- modules/data-selection/components/formatter/bag-address/bag-address.filter.js
+++ modules/data-selection/components/formatter/bag-address/bag-address.filter.js
@@ -8,7 +8,7 @@
     function bagAddressFilter () {
         return function (input) {
             const nummer = input.huisnummer + input.huisletter,
-                fullNummer = input.huisnummer_toevoeging ? nummer + '-' + input.huisnummer_toevoeging : nummer;
+                fullNummer = nummer + (input.huisnummer_toevoeging ? '-' + input.huisnummer_toevoeging : '');
 
             return input._openbare_ruimte_naam + ' ' + fullNummer;
         };
