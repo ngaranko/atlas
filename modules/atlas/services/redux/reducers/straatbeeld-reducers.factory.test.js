70f73ea94c1db740ec0f9949a9d2e1b3120ef6a8
vegter <j.vegter@amsterdam.nl>
2016-12-06 19:14:16 +0100

    Test fullscreen straatbeeld reducer

--- modules/atlas/services/redux/reducers/straatbeeld-reducers.factory.test.js
+++ modules/atlas/services/redux/reducers/straatbeeld-reducers.factory.test.js
@@ -157,22 +157,22 @@ describe('Straatbeeld reducers factory', function () {
         });
 
         it('can set straatbeeld explicitly to fullscreen', function () {
-            inputState.detail = {
-                endpoint: 'bag/verblijfsobject/123/',
-                geometry: 'aap',
-                isLoading: false,
-                isInvisible: true
+            inputState.straatbeeld = {
             };
 
-            let newState = straatbeeldReducers[ACTIONS.STRAATBEELD_FULLSCREEN.id](inputState, payload);
+            let newState = straatbeeldReducers[ACTIONS.STRAATBEELD_FULLSCREEN.id](inputState);
             expect(newState.straatbeeld.isFullscreen).toBeUndefined();
 
             payload = true;
             newState = straatbeeldReducers[ACTIONS.STRAATBEELD_FULLSCREEN.id](newState, payload);
             expect(newState.straatbeeld.isFullscreen).toBe(true);
+            newState = straatbeeldReducers[ACTIONS.STRAATBEELD_FULLSCREEN.id](newState);
+            expect(newState.straatbeeld.isFullscreen).toBe(true);
 
             payload = false;
             newState = straatbeeldReducers[ACTIONS.STRAATBEELD_FULLSCREEN.id](newState, payload);
+            expect(newState.straatbeeld.isFullscreen).toBe(false);
+            newState = straatbeeldReducers[ACTIONS.STRAATBEELD_FULLSCREEN.id](newState);
             expect(newState.straatbeeld.isFullscreen).toBe(false);
         });
     });
