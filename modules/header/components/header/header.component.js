8e1868ab69930d79d05581eff5cc0e59e003e2a2
Jurien Vegter <amsjurien@gmail.com>
2016-12-12 16:29:52 +0100

    Encapsulate classes in factories Provide for constants to define user settings

--- modules/header/components/header/header.component.js
+++ modules/header/components/header/header.component.js
@@ -14,15 +14,10 @@
             controllerAs: 'vm'
         });
 
-    DpHeaderController.$inject = ['user', 'httpStatus'];
+    DpHeaderController.$inject = ['user'];
 
-    function DpHeaderController (user, httpStatus) {
+    function DpHeaderController (user) {
         var vm = this;
-
-        vm.registerError = (event) => {
-            httpStatus.registerError(httpStatus.SERVER_ERROR);
-            event.preventDefault();
-        };
 
         vm.isLoggedIn = function () {
             return user.getStatus().isLoggedIn;
