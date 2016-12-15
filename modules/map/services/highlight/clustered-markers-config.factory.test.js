f941cec33055b129fd8c2b3284402a490b02ec7c
Erik Nijland <e.n.nijland@gmail.com>
2016-12-15 10:29:59 +0100

    The test is now making sure L.divIcon is used with the proper options.

--- modules/map/services/highlight/clustered-markers-config.factory.test.js
+++ modules/map/services/highlight/clustered-markers-config.factory.test.js
@@ -1,12 +1,14 @@
 describe('The clusteredMarkersConfig factory', function () {
     let clusteredMarkersConfig,
-        mockedCluster;
+        mockedCluster,
+        L;
 
     beforeEach(function () {
         angular.mock.module('dpMap');
 
-        angular.mock.inject(function (_clusteredMarkersConfig_) {
+        angular.mock.inject(function (_clusteredMarkersConfig_, _L_) {
             clusteredMarkersConfig = _clusteredMarkersConfig_;
+            L = _L_;
         });
 
         mockedCluster = {
@@ -14,12 +16,17 @@ describe('The clusteredMarkersConfig factory', function () {
                 return 123;
             }
         };
+
+        spyOn(L, 'divIcon').and.returnValue('MOCKED_DIV_ICON');
     });
 
     it('has an iconCreateFunction that returns a Leaflet icon with custom HTML', function () {
         const icon = clusteredMarkersConfig.iconCreateFunction(mockedCluster);
 
-        expect(icon.options.className).toBe('o-highlight-cluster');
-        expect(icon.options.html).toBe('<div class="o-highlight-cluster__text">123</div>');
+        expect(icon).toBe('MOCKED_DIV_ICON');
+        expect(L.divIcon).toHaveBeenCalledWith(jasmine.objectContaining({
+            html: '<div class="o-highlight-cluster__text">123</div>',
+            className: 'o-highlight-cluster'
+        }));
     });
 });
