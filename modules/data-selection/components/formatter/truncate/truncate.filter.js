a7395f1451953006542b801dfcdd83575f3c5d72
vegter <j.vegter@amsterdam.nl>
2016-12-20 19:58:26 +0100

    truncate

--- modules/data-selection/components/formatter/truncate/truncate.filter.js
+++ modules/data-selection/components/formatter/truncate/truncate.filter.js
@@ -6,9 +6,22 @@
         .filter('truncate', truncate);
 
     function truncate () {
-        return function (input) {
-            let html = input.replace(/<[^>]+>/gm, '');
-            return html.length > 250 ? html.substring(0, 250) + '...' : html;
+        const MAX_LENGTH = 250;
+        const ELLIPSES = '...';
+
+        return function (input, maxLength = MAX_LENGTH) {
+            if (angular.isString(input)) {
+                let truncated = input.replace(/<[^>]+>/gm, '');
+                if (truncated.length > maxLength) {
+                    truncated = truncated.substr(0, maxLength);
+                    let lastSpace = truncated.lastIndexOf(' ');
+                    truncated = truncated.substr(0, lastSpace < 0 ? maxLength : lastSpace);
+                    truncated += ELLIPSES;
+                }
+                return truncated;
+            } else {
+                return input;
+            }
         };
     }
 })();
