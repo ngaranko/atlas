8f0fe1747f3f61b6d8de034a71f6fdf8ba24412b
Marco Kotrotsos <kotrotsos@gmail.com>
2016-12-22 12:44:02 +0100

    click on dataset

--- modules/data-selection/components/cards/cards.component.js
+++ modules/data-selection/components/cards/cards.component.js
@@ -7,7 +7,21 @@
             bindings: {
                 content: '<'
             },
+            controller: DpDataSelectionCardsController,
             templateUrl: 'modules/data-selection/components/cards/cards.html',
             controllerAs: 'vm'
         });
+
+    DpDataSelectionCardsController.$inject = ['store', 'ACTIONS'];
+
+    function DpDataSelectionCardsController (store, ACTIONS) {
+        let vm = this;
+
+        vm.fetch_detail = function (currentPage) {
+            store.dispatch({
+                type: ACTIONS.FETCH_DETAIL,
+                payload: currentPage
+            });
+        };
+    }
 })();
