d0348cc0b646186690e5cfc1812f78e558f109fe
eriknijland <Fender12!>
2017-01-06 15:08:15 +0100

    Removed the manual webdriver-manager update step.

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,4 +1,5 @@
 exports.config = {
+    //seleniumServerJar: 'webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar',
     framework: 'jasmine',
     specs: 'e2e/*.test.js',
     capabilities: {
