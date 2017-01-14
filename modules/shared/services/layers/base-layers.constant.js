88445e216a2774bdb8458a5d65a89fdf5846aece
vegter <j.vegter@amsterdam.nl>
2017-01-14 11:33:18 +0100

    Shorter ids

--- modules/shared/services/layers/base-layers.constant.js
+++ modules/shared/services/layers/base-layers.constant.js
@@ -9,59 +9,59 @@
                 label: 'Topografie',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/topo_RD/{z}/{x}/{y}.png'
             }, {
-                slug: 'luchtfoto_2016',
+                slug: 'lf2016',
                 label: 'Luchtfoto 2016',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/lufo2016_RD/{z}/{x}/{y}.jpeg'
             }, {
-                slug: 'luchtfoto_2015',
+                slug: 'lf2015',
                 label: 'Luchtfoto 2015',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/lufo2015_RD/{z}/{x}/{y}.jpeg'
             }, {
-                slug: 'luchtfoto_2014',
+                slug: 'lf2014',
                 label: 'Luchtfoto 2014',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/lufo2014_RD/{z}/{x}/{y}.jpeg'
             }, {
-                slug: 'luchtfoto_2013',
+                slug: 'lf2013',
                 label: 'Luchtfoto 2013',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/lufo2013_RD/{z}/{x}/{y}.jpeg'
             }, {
-                slug: 'luchtfoto_2012',
+                slug: 'lf2012',
                 label: 'Luchtfoto 2012',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/lufo2012_RD/{z}/{x}/{y}.jpeg'
             }, {
-                slug: 'luchtfoto_2011',
+                slug: 'lf2011',
                 label: 'Luchtfoto 2011',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/lufo2011_RD/{z}/{x}/{y}.jpeg'
             }, {
-                slug: 'luchtfoto_2010',
+                slug: 'lf2010',
                 label: 'Luchtfoto 2010',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/lufo2010_RD/{z}/{x}/{y}.jpeg'
             }, {
-                slug: 'luchtfoto_2009',
+                slug: 'lf2009',
                 label: 'Luchtfoto 2009',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/lufo2009_RD/{z}/{x}/{y}.jpeg'
             }, {
-                slug: 'luchtfoto_2008',
+                slug: 'lf2008',
                 label: 'Luchtfoto 2008',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/lufo2008_RD/{z}/{x}/{y}.jpeg'
             }, {
-                slug: 'luchtfoto_2007',
+                slug: 'lf2007',
                 label: 'Luchtfoto 2007',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/lufo2007_RD/{z}/{x}/{y}.jpeg'
             }, {
-                slug: 'luchtfoto_2006',
+                slug: 'lf2006',
                 label: 'Luchtfoto 2006',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/lufo2006_RD/{z}/{x}/{y}.jpeg'
             }, {
-                slug: 'luchtfoto_2005',
+                slug: 'lf2005',
                 label: 'Luchtfoto 2005',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/lufo2005_RD/{z}/{x}/{y}.jpeg'
             }, {
-                slug: 'luchtfoto_2004',
+                slug: 'lf2004',
                 label: 'Luchtfoto 2004',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/lufo2004_RD/{z}/{x}/{y}.jpeg'
             }, {
-                slug: 'luchtfoto_2003',
+                slug: 'lf2003',
                 label: 'Luchtfoto 2003',
                 urlTemplate: 'https://{s}.datapunt.amsterdam.nl/lufo2003_RD/{z}/{x}/{y}.jpeg'
             }
