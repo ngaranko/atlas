e815c264beacce424142e78cb38d398fedefaff6
eriknijland <Fender12!>
2017-01-18 14:13:41 +0100

    Added tabs to switch between datasets.

--- modules/data-selection/components/header/header.component.js
+++ modules/data-selection/components/header/header.component.js
@@ -10,6 +10,21 @@
                 headerTitle: '<'
             },
             templateUrl: 'modules/data-selection/components/header/header.html',
-            controllerAs: 'vm'
+            controllerAs: 'vm',
+            controller: DpDataSelectionHeaderController
         });
+
+    DpDataSelectionHeaderController.$inject = ['DATA_SELECTION_CONFIG'];
+
+    function DpDataSelectionHeaderController (DATA_SELECTION_CONFIG) {
+        let vm = this;
+
+        vm.tabs = ['bag', 'hr'].map(dataset => {
+            return {
+                dataset: dataset,
+                title: DATA_SELECTION_CONFIG[dataset].TITLE,
+                isActive: vm.state.dataset === dataset
+            };
+        });
+    }
 })();
