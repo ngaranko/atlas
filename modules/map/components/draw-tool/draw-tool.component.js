27310fbc169480f0e1ef9c49f8b545f5f655a43e
vegter <amsjurien@gmail.com>
2017-01-25 10:32:40 +0100

    Follow drawmode from url Update edit tooltip

--- modules/map/components/draw-tool/draw-tool.component.js
+++ modules/map/components/draw-tool/draw-tool.component.js
@@ -21,11 +21,18 @@
         drawTool.initialize(vm.map, onFinishShape, onDrawingMode);
 
         $scope.$watch('vm.state.drawingMode', function (drawingMode) {
+            // enable is handled by the polygon markers watch method
+            if (!drawingMode) {
+                drawTool.disable();
+            }
         });
 
         $scope.$watch('vm.polygon.markers', function (polygon) {
             if (!drawTool.isEnabled()) {
                 drawTool.setPolygon(vm.polygon.markers);
+                if (vm.state.drawingMode) {
+                    drawTool.enable();
+                }
             }
         }, true);
 
@@ -44,7 +51,7 @@
                 store.dispatch({
                     type: ACTIONS.MAP_START_DRAWING,
                     payload: {
-                        drawingMode
+                        drawingMode: drawingMode
                     }
                 });
             }
