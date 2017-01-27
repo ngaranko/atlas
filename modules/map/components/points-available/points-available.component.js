867feab75811833fa0055d74beead62699bf8b2c
Marco Kotrotsos <kotrotsos@gmail.com>
2017-01-27 13:40:07 +0100

    Cleanup

--- modules/map/components/points-available/points-available.component.js
+++ modules/map/components/points-available/points-available.component.js
@@ -4,9 +4,7 @@
     angular
         .module('dpMap')
         .component('dpPointsAvailable', {
-            bindings: {
-                points: '='
-            },
+            bindings: {},
             templateUrl: 'modules/map/components/points-available/points-available.html',
             controller: DpPointsAvailableController,
             controllerAs: 'vm'
@@ -16,13 +14,12 @@
 
     function DpPointsAvailableController ($scope, store, ACTIONS, DRAW_TOOL_CONFIG, drawTool) {
         let vm = this;
-        vm.store = store;
-        vm.pointsAvailable = 0;
 
         store.subscribe(setPoints);
 
         function setPoints () {
             const state = store.getState();
+
             vm.drawingMode = state.map.drawingMode;
             vm.markers = drawTool.shape.markers;
             vm.markersLeft = drawTool.shape.markersMaxCount - drawTool.shape.markers.length;
