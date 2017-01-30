c4ab9479b02d3749d49551382327e1097b53ce40
vegter <amsjurien@gmail.com>
2017-01-30 16:26:03 +0100

    Review: rename polygon to drawGeometry in map state

--- modules/map/components/map/map.directive.js
+++ modules/map/components/map/map.directive.js
@@ -19,7 +19,7 @@
             scope: {
                 mapState: '=',
                 markers: '=',
-                polygon: '=',
+                drawGeometry: '=',
                 showLayerSelection: '=',
                 resize: '<'
             },
