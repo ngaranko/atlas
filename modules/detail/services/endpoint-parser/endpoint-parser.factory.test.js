c7602d9651363d9ee85b9e234106a2e526ba1922
eriknijland <Fender12!>
2017-01-31 14:50:14 +0100

    Added test cases for URLs with more than 2 folders (needed for explosieven).

--- modules/detail/services/endpoint-parser/endpoint-parser.factory.test.js
+++ modules/detail/services/endpoint-parser/endpoint-parser.factory.test.js
@@ -28,8 +28,11 @@ describe('The endpointParser factory', function () {
                 .toBe('meetbout');
 
             expect(endpointParser.getSubject('http://www.api-root.com/brk/object-wkpb/' +
-                    'NL.KAD.OnroerendeZaak.123456/'))
+                'NL.KAD.OnroerendeZaak.123456/'))
                 .toBe('object-wkpb');
+
+            expect(endpointParser.getSubject('http://www.api-root.com/folder-1/folder-2/folder-3/123/'))
+                .toBe('folder-3');
         });
     });
 
@@ -43,6 +46,9 @@ describe('The endpointParser factory', function () {
 
             expect(endpointParser.getTemplateUrl('http://www.api-root.com/meetbouten/meetbout/654/'))
                 .toBe('modules/detail/components/detail/templates/meetbouten/meetbout.html');
+
+            expect(endpointParser.getTemplateUrl('http://www.api-root.com/folder-1/folder-2/folder-3/654/'))
+                .toBe('modules/detail/components/detail/templates/folder-2/folder-3.html');
         });
 
         it('has special exceptions for zakelijk recht (BRK)', () => {
@@ -62,6 +68,9 @@ describe('The endpointParser factory', function () {
 
             expect(endpointParser.getGlossaryKey('http://www.api-root.com/meetbouten/meetbout/654/'))
                 .toBe('MEETBOUT');
+
+            expect(endpointParser.getGlossaryKey('http://www.api-root.com/milieuthemas/explosieven/inslag/1/'))
+                .toBe('INSLAG');
         });
 
         it('turns dashes (-) in the endpoint into underscores (_) in the glossary key', () => {
