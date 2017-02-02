9f09976120d4e39303357b22d338aca368cc62dc
vegter <amsjurien@gmail.com>
2017-02-02 15:02:43 +0100

    Coverage of leaflet-draw-translations

--- modules/map/services/leaflet-draw/leaflet-draw-translations.factory.test.js
+++ modules/map/services/leaflet-draw/leaflet-draw-translations.factory.test.js
@@ -17,10 +17,22 @@ describe('The leafletDrawTranslations factory', function () {
         expect(L.drawLocal.draw.toolbar.actions.title).not.toBe('');
         expect(L.drawLocal.draw.toolbar.actions.text).not.toBe('');
 
+        L.drawLocal.test = 5;
+
         leafletDrawTranslations.initialize();
 
         // Test any arbitrary texts to be set to ''
         expect(L.drawLocal.draw.toolbar.actions.title).toBe('');
+        expect(L.drawLocal.draw.toolbar.actions.text).toBe('');
+    });
+
+    it('Leaves all L.drawLocal non-string values untouched', function () {
+        L.drawLocal.draw.toolbar.actions.title = 10;
+
+        leafletDrawTranslations.initialize();
+
+        // Test any arbitrary texts to be set to ''
+        expect(L.drawLocal.draw.toolbar.actions.title).toBe(10);
         expect(L.drawLocal.draw.toolbar.actions.text).toBe('');
     });
 });
