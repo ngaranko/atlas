a07cfa97f42673e0b15a7375fba57aa59272685e
eriknijland <Fender12!>
2017-02-02 09:20:58 +0100

    Remove mock page objects

--- modules/map/components/map/map.page-objects.js
+++ modules/map/components/map/map.page-objects.js
@@ -3,7 +3,6 @@
 const isVisible = require('../../../../e2e/helpers/is-visible');
 
 const toggleLayerSelectionPageObjects = require('./../toggle-layer-selection/toggle-layer-selection.page-objects.js');
-// const activeOverlaysPageObjects = require('./../active-overlays/active-overlays.page-objects.js');
 const toggleFullscreenPageObjects = require('./../toggle-fullscreen/toggle-fullscreen.page-objects.js');
 
 module.exports = function (mapElement) {
@@ -12,7 +11,6 @@ module.exports = function (mapElement) {
             isVisible: isVisible(mapElement),
             toggleLayerSelection:
                 toggleLayerSelectionPageObjects(mapElement.element(by.css('dp-toggle-layer-selection'))),
-            // activeOverlays: activeOverlaysPageObjects(mapElement.element(by.css('dp-active-overlays'))),
             toggleFullscreen: toggleFullscreenPageObjects(mapElement.element(by.css('dp-toggle-fullscreen')))
         };
     };
