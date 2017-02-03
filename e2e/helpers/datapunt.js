92ba6eda64c312399cd4d89bcb9c390dd53bce65
eriknijland <Fender12!>
2017-02-03 13:01:19 +0100

    Each helper it's own file.

--- e2e/helpers/datapunt.js
+++ e2e/helpers/datapunt.js
@@ -4,7 +4,5 @@ module.exports = {
     availableStates: require('./available-states'),
     hasClass: require('./has-class'),
     isVisible: require('./is-visible'),
-    require: function (absolutePath) {
-        return require('../../' + absolutePath);
-    }
+    require: require('./require')
 };
