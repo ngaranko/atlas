b9aa650e863e643e3c16307a03736a2e8f47b85b
eriknijland <Fender12!>
2017-02-03 09:54:10 +0100

    removed unnecessary variable and changed variable scope

--- e2e/url-checker.test.js
+++ e2e/url-checker.test.js
@@ -4,12 +4,9 @@ const availableStates = require('./helpers/available-states');
 const navigate = require('./helpers/navigate');
 
 describe('each URL should load the corresponding view', function () {
-    const AVAILABLE_STATE_KEYS = Object.keys(availableStates);
-    let page;
-
-    AVAILABLE_STATE_KEYS.forEach(key => {
+    Object.keys(availableStates).forEach(key => {
         it(key, () => {
-            page = navigate(key);
+            const page = navigate(key);
             availableStates[key].validator(page);
         });
     });
