59d3e8ccb27827b06c8d5efd3fdb5f4c8a04d177
eriknijland <Fender12!>
2017-02-14 16:13:03 +0100

    All e2e scenarios are done.

--- e2e/fullscreen-map-and-layer-selection.test.js
+++ e2e/fullscreen-map-and-layer-selection.test.js
@@ -1,16 +1,16 @@
 let page;
 
-fdescribe('The fullscreen map (or map w/ layerSelection) remembers how you got there', () => {
+describe('The fullscreen map (or map w/ layerSelection) remembers how you got there', () => {
     it('navigates to a new straatbeeld if straatbeeld is active but invisible', () => {
         page = dp.navigate('MAP_STRAATBEELD--DETAIL');
 
         // Open layerSelection
         page.dashboard.middleColumn.map.toggleLayerSelection.click();
-        dp.availableStates['LAYER-SELECTION_MAP'].validator(page);
+        dp.validate('LAYER-SELECTION_MAP', page);
 
         // Click on the map, return to (a different) straatbeeld
         page.dashboard.middleColumn.map.click(100, 100);
-        dp.availableStates['MAP_STRAATBEELD--SEARCH-RESULTS'].validator(page);
+        dp.validate('MAP_STRAATBEELD--SEARCH-RESULTS', page);
     });
 
 
@@ -19,19 +19,19 @@ fdescribe('The fullscreen map (or map w/ layerSelection) remembers how you got t
 
         // Make the map fullscreen
         page.dashboard.middleColumn.map.toggleFullscreen.click();
-        dp.availableStates['MAP'].validator(page);
+        dp.validate('MAP', page);
 
         // Open layer selection
         page.dashboard.middleColumn.map.toggleLayerSelection.click();
-        dp.availableStates['LAYER-SELECTION_MAP'].validator(page);
+        dp.validate('LAYER-SELECTION_MAP', page);
 
         // Close layer selection, return to the fullscreen map
         page.dashboard.middleColumn.map.toggleLayerSelection.click();
-        dp.availableStates['MAP'].validator(page);
+        dp.validate('MAP', page);
 
         // Close the fullscreen map, return to the homepage
         page.dashboard.middleColumn.map.toggleFullscreen.click();
-        dp.availableStates['MAP_PAGE--HOME'].validator(page);
+        dp.validate('MAP_PAGE--HOME', page);
     });
 
     it('remembers the active detail page when opening and closing layer selection', () => {
@@ -39,52 +39,66 @@ fdescribe('The fullscreen map (or map w/ layerSelection) remembers how you got t
 
         // Open layer selection
         page.dashboard.middleColumn.map.toggleLayerSelection.click();
-        dp.availableStates['MAP'].validator(page);
+        dp.validate('LAYER-SELECTION_MAP', page);
 
         // Close layer selection and return to the detail page
         page.dashboard.leftColumn.layerSelection.close();
-        dp.availableStates['MAP_DETAIL--NUMMERAANDUIDING'].validator(page);
-
-
-        /*
-        Actie: ga naar kaart+detail
-        Actie: open kaartlaag selectie
-        Actie: sluit kaartlaag selectie
-        */
+        dp.validate('MAP_DETAIL--NUMMERAANDUIDING', page);
     });
 
     it('remembers the active detail page when playing with layer selection followed by a fullscreen map', () => {
-        /*
-         Actie: ga naar kaart+detail
-         Actie: open kaartlaag selectie
-         Controle: layer selection + map
-         Actie: kaart fullscreen
-         Controle: MAP fullscreen
-         Actie: kaart klein maken (toggle fullscreen)
-         Controle: ben ik bij map+detail en dus niet bij layer-selection
-         */
+        page = dp.navigate('MAP_DETAIL--NUMMERAANDUIDING');
+
+        // Open layer selection
+        page.dashboard.middleColumn.map.toggleLayerSelection.click();
+        dp.validate('LAYER-SELECTION_MAP', page);
+
+        // Make the map fullscreen
+        page.dashboard.middleColumn.map.toggleFullscreen.click();
+        dp.validate('MAP', page);
+
+        // Make the map small again, return to the detail page (and NOT to layer selection)
+        page.dashboard.middleColumn.map.toggleFullscreen.click();
+        dp.validate('MAP_DETAIL--NUMMERAANDUIDING', page);
     });
 
     it('remembers the active detail page when playing with a fullscreen map followed by layer selection', () => {
-        /*
-         Actie: ga naar kaart+detail
-         Actie: maak kaart fullscreen
-         Actie: open layer selection
-         Actie: maak kaart fullscreen
-         Actie: kaart klein maken (toggle fullscreen)
-         Controle: ben ik bij map+detail en dus niet bij layer-selection
-         */
+        page = dp.navigate('MAP_DETAIL--NUMMERAANDUIDING');
+
+        // Make the map fullscreen
+        page.dashboard.middleColumn.map.toggleFullscreen.click();
+        dp.validate('MAP', page);
+
+        // Open layer selection
+        page.dashboard.middleColumn.map.toggleLayerSelection.click();
+        dp.validate('LAYER-SELECTION_MAP', page);
+
+        // Make the map fullscreen (again)
+        page.dashboard.middleColumn.map.toggleFullscreen.click();
+        dp.validate('MAP', page);
+
+        // Make the map small and return to the detail page (and NOT to layer selection)
+        page.dashboard.middleColumn.map.toggleFullscreen.click();
+        dp.validate('MAP_DETAIL--NUMMERAANDUIDING', page);
     });
 
-    it('remebers the active detail page when playing w/ the layerSelection panel', () => {
-        /*
-         Actie: ga naar kaart+detail
-         Actie: maak kaart fullscreen
-         Actie: open kaartlaag selectie
-         Actie: sluit kaartlaag selectie (via close icon linker kolom)
-         Controle: ben ik bij fullscreen map
-         Actie: minimaliseer kaart
-         COntrole: ben ik weer bij detail+map
-         */
+    it('remebers the detail and fullscreen map state when toggling the layer selection panel', () => {
+        page = dp.navigate('MAP_DETAIL--NUMMERAANDUIDING');
+
+        // Make the map fullscreen
+        page.dashboard.middleColumn.map.toggleFullscreen.click();
+        dp.validate('MAP', page);
+
+        // Open layer selection
+        page.dashboard.middleColumn.map.toggleLayerSelection.click();
+        dp.validate('LAYER-SELECTION_MAP', page);
+
+        // Close layer selection, return to the fullscreen map
+        page.dashboard.middleColumn.map.toggleLayerSelection.click();
+        dp.validate('MAP', page);
+
+        // Make the map small, return to the detail page
+        page.dashboard.middleColumn.map.toggleFullscreen.click();
+        dp.validate('MAP_DETAIL--NUMMERAANDUIDING', page);
     });
 });
