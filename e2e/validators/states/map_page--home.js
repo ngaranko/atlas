39994ca0e67b165980ac3f0464ac03e8a8bb5e41
eriknijland <Fender12!>
2017-02-14 11:08:49 +0100

    eslint

--- e2e/validators/states/map_page--home.js
+++ e2e/validators/states/map_page--home.js
@@ -5,6 +5,6 @@ const mapPage = require('./map_page');
 module.exports = function (page) {
     expect(page.dashboard.rightColumn.page.text).toContain('Welkom! Atlas');
     expect(page.dashboard.rightColumn.page.text).toContain('Nieuw in Atlas');
-    
+
     mapPage(page);
 };
--- e2e/validators/states/map_search-results--location.js
+++ e2e/validators/states/map_search-results--location.js
@@ -3,8 +3,6 @@
 const mapSearchResults = require('./map_search-results');
 
 module.exports = function (page) {
-    const searchResults = page.dashboard.rightColumn.searchResults;
-
     expect(page.title)
         .toMatch(/^\d+ resultaten met locatie \d+\.\d+, \d+\.\d+ \(\d+\.\d+, \d+\.\d+\) - Atlas$/);
 
