c40619eaa528fad18befdfa4bc2c79677d5338ac
vegter <amsjurien@gmail.com>
2017-02-23 11:38:29 +0100

    user factory uitgebreid met name en authorization level

--- modules/shared/services/user/user.factory.js
+++ modules/shared/services/user/user.factory.js
@@ -5,52 +5,177 @@
         .module('dpShared')
         .factory('user', userFactory);
 
-    userFactory.$inject = ['userSettings'];
+    userFactory.$inject = ['$log', '$window', 'userSettings'];
 
-    function userFactory (userSettings) {
-        const USER_TYPE = {
+    function userFactory ($log, $window, userSettings) {
+        const USER_TYPE = { // the possible types of a user
             NONE: 'NONE',
             ANONYMOUS: 'ANONYMOUS',
             AUTHENTICATED: 'AUTHENTICATED'
         };
 
-        let accessToken = null;
+        const AUTHORIZATION_LEVEL = {   // The possible user authorization levels
+            NONE: 'NONE',
+            DEFAULT: 'DEFAULT',
+            EMPLOYEE: 'EMPLOYEE',
+            EMPLOYEE_PLUS: 'EMPLOYEE_PLUS'
+        };
+
+        const AUTHORIZATION_LEVEL_MAPPING = {   // maps the backend level codes upon a valid authorization level
+            0: AUTHORIZATION_LEVEL.DEFAULT,
+            1: AUTHORIZATION_LEVEL.EMPLOYEE,
+            3: AUTHORIZATION_LEVEL.EMPLOYEE_PLUS
+        };
+
+        class User {
+            constructor () {
+                this.init = function () {   // initialize private properties
+                    this._accessToken = null;
+                    this._authorizationLevel = AUTHORIZATION_LEVEL.NONE;
+                    this.name = '';
+                };
+
+                this.decodeToken = function (token) {
+                    try {
+                        return angular.fromJson(
+                            $window.atob(token
+                                .split('.')[1]
+                                .replace('-', '+')
+                                .replace('_', '/')
+                            ));
+                    } catch (e) {
+                        return {};
+                    }
+                };
+
+                this.parseToken = function (token) {   // private method to parse a refresh or access token
+                    let content = this.decodeToken(token);
+
+                    if (angular.isDefined(content.sub)) {   // contained in refresh token
+                        this.name = content.sub || '';
+                    }
+
+                    if (angular.isDefined(content.authz)) { // contained in access token
+                        this.authorizationLevel = content.authz;
+                    }
+
+                    this.log();
+                };
+
+                this.init();
+
+                if (this.refreshToken) {                // get any existing refresh token
+                    this.parseToken(this.refreshToken); // and parse its contents
+                }
+            }
+
+            clear () {
+                userSettings.refreshToken.remove();
+                userSettings.userType.remove();
+
+                this.init();
+            }
+
+            get refreshToken () {
+                return userSettings.refreshToken.value; // the refresh token in stored in the session
+            }
+
+            set refreshToken (value) {
+                userSettings.refreshToken.value = value;
+                this.parseToken(value);
+            }
+
+            get accessToken () {
+                return this._accessToken;
+            }
+
+            set accessToken (value) {
+                this._accessToken = value;
+                this.parseToken(value);
+            }
+
+            get type () {
+                return USER_TYPE[userSettings.userType.value] || USER_TYPE.NONE;
+            }
+
+            set type (value) {
+                userSettings.userType.value = USER_TYPE[value];
+            }
+
+            get name () {
+                return this._name;
+            }
+
+            set name (value) {
+                this._name = value;
+            }
+
+            get authorizationLevel () {
+                return AUTHORIZATION_LEVEL[this._authorizationLevel];
+            }
+
+            set authorizationLevel (value) {
+                this._authorizationLevel = AUTHORIZATION_LEVEL_MAPPING[value] || AUTHORIZATION_LEVEL.NONE;
+            }
+
+            log () {
+                $log.debug('User' + '\n' +
+                    'Type: ' + this.type + '\n' +
+                    'Authorization level: ' + this.authorizationLevel + '\n' +
+                    'Name: ' + this.name + '\n' +
+                    'Refresh token: ' + angular.toJson(this.decodeToken(this.refreshToken)) + '\n' +
+                    'Access token: ' + angular.toJson(this.decodeToken(this.accessToken)) + '\n' +
+                    'Raw refresh token: ' + this.refreshToken + '\n' +
+                    'Raw access token: ' + this.accessToken);
+            }
+        }
+
+        let user = new User();
 
         return {
             getRefreshToken,
             setRefreshToken,
             getAccessToken,
             setAccessToken,
+            getName,
+            getAuthorizationLevel,
             getUserType,
             clearToken,
-            USER_TYPE
+            USER_TYPE,
+            AUTHORIZATION_LEVEL
         };
 
         function getRefreshToken () {
-            return userSettings.refreshToken.value;
+            return user.refreshToken;
         }
 
         function setRefreshToken (token, userType) {
-            userSettings.refreshToken.value = token;
-            userSettings.userType.value = USER_TYPE[userType];
+            user.refreshToken = token;
+            user.type = userType;
         }
 
         function getAccessToken () {
-            return accessToken;
+            return user.accessToken;
         }
 
         function setAccessToken (token) {
-            accessToken = token;
+            user.accessToken = token;
         }
 
         function getUserType () {
-            return USER_TYPE[userSettings.userType.value] || USER_TYPE.NONE;
+            return user.type;
+        }
+
+        function getName () {
+            return user.name;
+        }
+
+        function getAuthorizationLevel () {
+            return user.authorizationLevel;
         }
 
         function clearToken () {
-            accessToken = null;
-            userSettings.refreshToken.remove();
-            userSettings.userType.remove();
+            user.clear();
         }
     }
 })();
--- modules/shared/services/user/user.factory.test.js
+++ modules/shared/services/user/user.factory.test.js
@@ -1,13 +1,22 @@
 describe('The user factory', function () {
     let userSettings,
+        refreshToken,
+        userType,
         user;
 
     const testToken = 'test token';
 
-    beforeEach(function () {
-        let refreshToken = 'refreshToken',
-            userType = 'NONE';
+    /* eslint-disable max-len */
+    const
+        testRefreshToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE0ODc4NDMyNDMsImV4cCI6MTQ4Nzg0ODk4Miwic3ViIjoiSSBhbSB0aGUgc3ViIGZpZWxkIiwianRpIjoiZWE3ZjViMzMtYjA0Ni00ZDAxLWJmZTUtY2NiODYwYjFmZTVmIn0.2QAsGMbyRk5g5SOgRBlTqFh2MCMQE4ELaU2JuHVCkBg',
+        testAnonymousRefreshToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE0ODc4NDM0ODAsImV4cCI6MTQ5MDQzNTQ4MCwic3ViIjpudWxsfQ.tT81eOuAFns6EU2kowjrL-4Wxo34mQPBVY8hK4wFGo4',
+        testAccessTokenAuthz0 = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE0ODc4NDMwMjgsImV4cCI6MTQ4Nzg0MzMyOCwiYXV0aHoiOjB9.dFYWAzKl_-eoWlnwhnXW7L0bf5y1dy-ECTzbQtZN3gk',
+        testAccessTokenAuthz1 = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE0ODc4NDMwMjgsImV4cCI6MTQ4Nzg0OTA1NCwiYXV0aHoiOjEsImp0aSI6Ijk1M2U3Y2NlLWEwYzQtNDAyZi1iZTc0LTg3MjY2ZDE3NThjMSJ9.QMu_x1Iys0GX0-zkb3NpZmEPX2rSU9-G7Q59_hlbpFg',
+        testAccessTokenAuthz3 = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE0ODc4NDMwMjgsImV4cCI6MTQ4Nzg0OTExMCwiYXV0aHoiOjMsImp0aSI6ImY4ZDJiNzQ4LTg2MzAtNDFmOC05Y2M4LWE1Njg4OTUzNTFjOCJ9.TlqGvT5HX979p0IcYm2DgGIIqb45Is6_hAS9F3YOnlU',
+        testAccessTokenAuthz99 = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE0ODc4NDMwMjgsImV4cCI6MTQ4Nzg0ODc3NiwiYXV0aHoiOjk5LCJqdGkiOiI4Y2VjYTk1My03YzEzLTQwMmMtODIyMS1jM2RjYWE0ZWY1Y2MifQ.LahevkWI06bVCY66khV1z5WKoAByNC_kMSwjKsABkwY';
+    /* eslint-enable max-len */
 
+    beforeEach(function () {
         angular.mock.module(
             'dpShared',
             {
@@ -32,45 +41,94 @@ describe('The user factory', function () {
         });
     });
 
-    it('can store and tell the refresh token for the user', function () {
-        user.setRefreshToken(testToken);
-        expect(user.getRefreshToken()).toBe(testToken);
+    describe('The regular behaviour', function () {
+        it('can store and tell the refresh token for the user', function () {
+            user.setRefreshToken(testToken);
+            expect(user.getRefreshToken()).toBe(testToken);
+        });
+
+        it('can store and tell the access token for the user', function () {
+            user.setAccessToken(testToken);
+            expect(user.getAccessToken()).toBe(testToken);
+        });
+
+        it('can tell the name of the user', function () {
+            user.setRefreshToken(testRefreshToken);
+            expect(user.getName()).toBe('I am the sub field');
+        });
+
+        it('tells "" as the name of an anonymous user', function () {
+            user.setRefreshToken(testAnonymousRefreshToken);
+            expect(user.getName()).toBe('');
+        });
+
+        it('can tell the authorization level of the user', function () {
+            user.setAccessToken(testAccessTokenAuthz0);
+            expect(user.getAuthorizationLevel()).toBe(user.AUTHORIZATION_LEVEL.DEFAULT);
+            user.setAccessToken(testAccessTokenAuthz1);
+            expect(user.getAuthorizationLevel()).toBe(user.AUTHORIZATION_LEVEL.EMPLOYEE);
+            user.setAccessToken(testAccessTokenAuthz3);
+            expect(user.getAuthorizationLevel()).toBe(user.AUTHORIZATION_LEVEL.EMPLOYEE_PLUS);
+        });
+
+        it('returns authorization level NONE for unknown authorization levels', function () {
+            user.setAccessToken(testAccessTokenAuthz99);
+            expect(user.getAuthorizationLevel()).toBe(user.AUTHORIZATION_LEVEL.NONE);
+        });
+
+        it('can store and tell the type of the user', function () {
+            user.setRefreshToken(testToken, user.USER_TYPE.ANONYMOUS);
+            expect(user.getUserType()).toBe(user.USER_TYPE.ANONYMOUS);
+        });
+
+        it('stores the refresh token and user type in the userSettings', function () {
+            user.setRefreshToken(testToken, user.USER_TYPE.ANONYMOUS);
+            expect(userSettings.refreshToken.value).toBe(testToken);
+            expect(userSettings.userType.value).toBe(user.USER_TYPE.ANONYMOUS);
+        });
+
+        it('returns a default access token null', function () {
+            expect(user.getAccessToken()).toBe(null);
+        });
+
+        it('can clear all tokens for the user', function () {
+            user.clearToken();
+            expect(user.getAccessToken()).toBe(null);
+            expect(user.getRefreshToken()).toBe(null);
+            expect(user.getUserType()).toBe(user.USER_TYPE.NONE);
+        });
+
+        it('returns a default user type NONE for unknown user types', function () {
+            userSettings.userType.value = 'unknown usertype';
+            expect(user.getUserType()).toBe(user.USER_TYPE.NONE);
+        });
     });
 
-    it('can store and tell the access token for the user', function () {
-        user.setAccessToken(testToken);
-        expect(user.getAccessToken()).toBe(testToken);
+    describe('The behaviour when a refreshToken and userType is available from the session storage', function () {
+        beforeEach(function () {
+            refreshToken = 'refreshToken';
+            userType = 'NONE';
+        });
+
+        it('returns a default refresh token and user type as are stored in the userSettings', function () {
+            expect(user.getRefreshToken()).toBe(userSettings.refreshToken.value);
+            expect(user.getUserType()).toBe(userSettings.userType.value);
+        });
     });
 
-    it('can store and tell the type of the user', function () {
-        user.setRefreshToken(testToken, user.USER_TYPE.ANONYMOUS);
-        expect(user.getUserType()).toBe(user.USER_TYPE.ANONYMOUS);
-    });
+    describe('The behaviour when no refreshToken and/or userType is available from the session storage', function () {
+        beforeEach(function () {
+            refreshToken = null;
+            userType = null;
+        });
 
-    it('stores the refresh token and user type in the userSettings', function () {
-        user.setRefreshToken(testToken, user.USER_TYPE.ANONYMOUS);
-        expect(userSettings.refreshToken.value).toBe(testToken);
-        expect(userSettings.userType.value).toBe(user.USER_TYPE.ANONYMOUS);
-    });
+        it('returns refresh token and user type as are stored in the userSettings', function () {
+            expect(user.getRefreshToken()).toBe(null);
+            expect(user.getUserType()).toBe(user.USER_TYPE.NONE);
+        });
 
-    it('returns a default access token null', function () {
-        expect(user.getAccessToken()).toBe(null);
-    });
-
-    it('returns a default refresh token and user type as are stored in the userSettings', function () {
-        expect(user.getRefreshToken()).toBe(userSettings.refreshToken.value);
-        expect(user.getUserType()).toBe(userSettings.userType.value);
-    });
-
-    it('returns a default user type NONE for unknown user types', function () {
-        userSettings.userType.value = 'unknown usertype';
-        expect(user.getUserType()).toBe(user.USER_TYPE.NONE);
-    });
-
-    it('can clear all tokens for the user', function () {
-        user.clearToken();
-        expect(user.getAccessToken()).toBe(null);
-        expect(user.getRefreshToken()).toBe(null);
-        expect(user.getUserType()).toBe(user.USER_TYPE.NONE);
+        it('tells "" as the name of an anonymous user', function () {
+            expect(user.getName()).toBe('');
+        });
     });
 });
