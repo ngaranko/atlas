084ea85b9da72964ada0bfce410d6e75caa3d0cc
JuVe <microsoft.account@jurien.com>
2017-02-28 19:26:58 +0100

    Set focus on input after clear

--- modules/header/components/search/search.directive.js
+++ modules/header/components/search/search.directive.js
@@ -38,6 +38,7 @@
             scope.clear = function () {
                 scope.query = '';
                 scope.getSuggestions();
+                scope.setFocus();
             };
 
             scope.getSuggestions = function () {
