679f87831eccdf32ad98e32dee06b7bc3bbcba3f
eriknijland <Fender12!>
2017-03-03 16:16:40 +0100

    Kadastrale objecten bij verblijfsobject werkt EINDELIJK.

--- e2e/authorization/verblijfsobject.test.js
+++ e2e/authorization/verblijfsobject.test.js
@@ -1,6 +1,25 @@
-describe('The detail page of a verblijfsobject', () => {
+fdescribe('The detail page of a verblijfsobject', () => {
     it('doesn\'t show rechten en aantekeningen voor users with the DEFAULT role', () => {
+        const page = dp.navigate('MAP_DETAIL--VERBLIJFSOBJECT', 'DEFAULT');
+        const detail = page.dashboard.rightColumn.detail;
 
+        expect(detail.kadastraleObjecten(0).link.label).toBe('ASD03 E 10377 A 0001');
+
+        expect(detail.kadastraleObjecten(0).zakelijkRecht.glossaryHeader.subtitle).toBe('Zakelijke rechten');
+        expect(detail.kadastraleObjecten(0).zakelijkRecht.list(0).link.label).toBe('Meesters,Catharina Maria Elisabeth, (V) - Eigendom (recht van) (1/2)');
+        expect(detail.kadastraleObjecten(0).zakelijkRecht.list(1).link.label).toBe('Halder,Adrianus Andreas Everdina Maria,van (M) - Eigendom (recht van) (1/2)');
+
+        expect(detail.kadastraleObjecten(0).beperking.glossaryHeader.subtitle).toBe('Gemeentelijke beperkingen');
+        expect(detail.kadastraleObjecten(0).beperking.list(0).link.label).toBe('Huisvestingsverordening, splitsingsvergunningstelsel, Huisvestingswet');
+
+        expect(detail.kadastraleObjecten(0).aantekeningen.glossaryHeader.subtitle).toBe('Aantekeningen');
+        expect(detail.kadastraleObjecten(0).aantekeningen.list(0).text).toBe('Locatiegegevens ontleend aan Basisregistraties Adressen en Gebouwen');
+        expect(detail.kadastraleObjecten(0).aantekeningen.list(1).text).toBe('Besluit op basis van Monumentenwet 1988, opgelegd door De Staat (Onderwijs, Cultuur en Wetenschappen)');
+
+        expect(detail.kadastraleObjecten(0).ontstaanUit.glossaryHeader.subtitle).toBe('Ontstaan uit');
+        expect(detail.kadastraleObjecten(0).ontstaanUit.list(0).link.label).toBe('ASD03 E 10341 G 0000');
+
+        expect(detail.kadastraleObjecten(1).link.label).toBe('ASD03 E 10377 A 0002');
     });
 
     it('shows everything for users with the EMPLOYEE and EMPLOYEE_PLUS role', () => {
--- modules/detail/components/detail/detail.page-objects.js
+++ modules/detail/components/detail/detail.page-objects.js
@@ -5,7 +5,8 @@ const
     nummeraanduidingHeader = dp.require(
         'modules/detail/components/nummeraanduiding-header/nummeraanduiding-header.page-objects'),
     straatbeeldThumbnail = dp.require(
-        'modules/shared/components/straatbeeld-thumbnail/straatbeeld-thumbnail.page-objects');
+        'modules/shared/components/straatbeeld-thumbnail/straatbeeld-thumbnail.page-objects'),
+    link = dp.require('modules/shared/components/link/link.page-objects');
 
 module.exports = function (detailElement) {
     return {
@@ -15,30 +16,23 @@ module.exports = function (detailElement) {
         get straatbeeldThumbnail () {
             return straatbeeldThumbnail(detailElement.element(by.css('dp-straatbeeld-thumbnail')));
         },
-        get nummeraanduiding () {
-            return groupedDataPageObject(detailElement.element(by.css('.qa-nummeraanduiding')));
-        },
-        get verblijfsobject () {
-            return groupedDataPageObject(detailElement.element(by.css('.qa-verblijfsobject')));
-        },
-
-
-
-
         get aantekeningen () {
             return groupedDataPageObject(detailElement.element(by.css('.qa-aantekeningen')));
-        },
-        get ontstaanUit () {
-            return groupedDataPageObject(detailElement.element(by.css('.qa-ontstaan-uit')));
-        },
-        get betrokkenBij () {
-            return groupedDataPageObject(detailElement.element(by.css('.qa-betrokken-bij')));
         },
         get beperking () {
             return groupedDataPageObject(detailElement.element(by.css('.qa-beperking')));
         },
+        get betrokkenBij () {
+            return groupedDataPageObject(detailElement.element(by.css('.qa-betrokken-bij')));
+        },
         get kadastraalObject () {
             return groupedDataPageObject(detailElement.element(by.css('.qa-kadastraal-object')));
+        },
+        kadastraleObjecten: function (index) {
+            const kadastraleObjecten = detailElement.element(by.css('.qa-kadastraal-object-list'));
+            const kadastraalObject = kadastraleObjecten.all(by.css('.qa-brk-vbo')).get(index);
+
+            return kadastraalObjectPageObject(kadastraalObject);
         },
         get kadastraalSubject () {
             return groupedDataPageObject(detailElement.element(by.css('.qa-kadastraal-subject')));
@@ -48,6 +42,15 @@ module.exports = function (detailElement) {
         },
         get nietNatuurlijkPersoon () {
             return groupedDataPageObject(detailElement.element(by.css('.qa-niet-natuurlijk-persoon')));
+        },
+        get nummeraanduiding () {
+            return groupedDataPageObject(detailElement.element(by.css('.qa-nummeraanduiding')));
+        },
+        get ontstaanUit () {
+            return groupedDataPageObject(detailElement.element(by.css('.qa-ontstaan-uit')));
+        },
+        get verblijfsobject () {
+            return groupedDataPageObject(detailElement.element(by.css('.qa-verblijfsobject')));
         },
         get zakelijkRecht () {
             return groupedDataPageObject(detailElement.element(by.css('.qa-zakelijk-recht')));
@@ -68,6 +71,12 @@ function groupedDataPageObject (groupedDataElement) {
         },
         get descriptionList () {
             return descriptionListPageObject(groupedDataElement.element(by.css('dl')));
+        },
+        list: function (index) {
+            const list = groupedDataElement.element(by.css('ul'));
+            const listItem = list.all(by.css('li')).get(index);
+
+            return listItemPageObject(listItem);
         }
     };
 }
@@ -79,6 +88,40 @@ function descriptionListPageObject (definitionListElement) {
         },
         definition: function (index) {
             return definitionListElement.all(by.css('dd')).get(index).getText();
+        }
+    };
+}
+
+function listItemPageObject (listItemElement) {
+    return {
+        get link () {
+            return link(listItemElement.element(by.css('dp-link')));
+        },
+        get text () {
+            return listItemElement.getText();
+        }
+    };
+}
+
+function kadastraalObjectPageObject (kadastraalObjectElement) {
+    return {
+        get link () {
+            return link(kadastraalObjectElement.all(by.css('dp-link')).get(0));
+        },
+        get aantekeningen () {
+            return groupedDataPageObject(kadastraalObjectElement.element(by.css('.qa-aantekeningen')));
+        },
+        get beperking () {
+            return groupedDataPageObject(kadastraalObjectElement.element(by.css('.qa-beperking')));
+        },
+        get betrokkenBij () {
+            return groupedDataPageObject(kadastraalObjectElement.element(by.css('.qa-betrokken-bij')));
+        },
+        get ontstaanUit () {
+            return groupedDataPageObject(kadastraalObjectElement.element(by.css('.qa-ontstaan-uit')));
+        },
+        get zakelijkRecht () {
+            return groupedDataPageObject(kadastraalObjectElement.element(by.css('.qa-zakelijk-recht')));
         }
     };
 }
