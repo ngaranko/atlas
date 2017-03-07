d4668671a55bb7dd737f5d3d6920249940413c72
eriknijland <Fender12!>
2017-03-07 13:24:21 +0100

    Specs verkeerd gelezen >> de rechten van een niet natuurlijk persoon mag ook door EMPLOYEE gezien worden

--- e2e/authorization/kadastraal-subject.test.js
+++ e2e/authorization/kadastraal-subject.test.js
@@ -26,7 +26,7 @@ describe('The detail page of kadastrale subjecten', () => {
         });
 
         it('shows everything including the rechten for users with the EMPLOYEE_PLUS role', () => {
-            const page = dp.navigate('MAP_DETAIL--KADASTRAAL-SUBJECT-NATUURLIJK-PERSOON', 'EMPLOYEE');
+            const page = dp.navigate('MAP_DETAIL--KADASTRAAL-SUBJECT-NATUURLIJK-PERSOON', 'EMPLOYEE_PLUS');
             const detail = page.dashboard.rightColumn.detail;
 
             expect(detail.kadastraalSubject.glossaryHeader.subtitle).toBe('Kadastraal subject');
@@ -50,40 +50,26 @@ describe('The detail page of kadastrale subjecten', () => {
             expect(detail.kadastraalSubject.glossaryHeader.title).toBe('');
 
             expect(detail.nietNatuurlijkPersoon.isPresent).toBe(false);
-            expect(detail.zakelijkRecht.isPresent).toBe(false);
-        });
-
-        it('shows everything except rechten for users with the EMPLOYEE role', () => {
-            const page = dp.navigate('MAP_DETAIL--KADASTRAAL-SUBJECT-NIET-NATUURLIJK-PERSOON', 'EMPLOYEE');
-            const detail = page.dashboard.rightColumn.detail;
-
-            expect(detail.kadastraalSubject.glossaryHeader.subtitle).toBe('Kadastraal subject');
-            expect(detail.kadastraalSubject.glossaryHeader.title).toBe('De Gemeente Gods Amsterdam');
-
-            expect(detail.nietNatuurlijkPersoon.isPresent).toBe(true);
-            expect(detail.nietNatuurlijkPersoon.descriptionList.term(0)).toBe('Statutaire zetel');
-            expect(detail.nietNatuurlijkPersoon.descriptionList.definition(0)).toBe('AMSTERDAM');
-            expect(detail.nietNatuurlijkPersoon.descriptionList.term(1)).toBe('Rechtsvorm');
-            expect(detail.nietNatuurlijkPersoon.descriptionList.definition(1)).toBe('Kerkelijke organisatie');
-
-            expect(detail.zakelijkRecht.isPresent).toBe(false);
-        });
-
-        it('shows everything including the rechten for users with the EMPLOYEE_PLUS role', () => {
-            const page = dp.navigate('MAP_DETAIL--KADASTRAAL-SUBJECT-NIET-NATUURLIJK-PERSOON', 'EMPLOYEE');
-            const detail = page.dashboard.rightColumn.detail;
-
-            expect(detail.kadastraalSubject.glossaryHeader.subtitle).toBe('Kadastraal subject');
-            expect(detail.kadastraalSubject.glossaryHeader.title).toBe('De Gemeente Gods Amsterdam');
-
-            expect(detail.nietNatuurlijkPersoon.isPresent).toBe(true);
-            expect(detail.nietNatuurlijkPersoon.descriptionList.term(0)).toBe('Statutaire zetel');
-            expect(detail.nietNatuurlijkPersoon.descriptionList.definition(0)).toBe('AMSTERDAM');
-            expect(detail.nietNatuurlijkPersoon.descriptionList.term(1)).toBe('Rechtsvorm');
-            expect(detail.nietNatuurlijkPersoon.descriptionList.definition(1)).toBe('Kerkelijke organisatie');
-
             expect(detail.zakelijkRecht.isPresent).toBe(true);
-            // Todo: test real content here
+        });
+
+        it('shows everything for users with the EMPLOYEE or EMPLOYEE_PLUS role', () => {
+            ['EMPLOYEE', 'EMPLOYEE_PLUS'].forEach(role => {
+                const page = dp.navigate('MAP_DETAIL--KADASTRAAL-SUBJECT-NIET-NATUURLIJK-PERSOON', role);
+                const detail = page.dashboard.rightColumn.detail;
+
+                expect(detail.kadastraalSubject.glossaryHeader.subtitle).toBe('Kadastraal subject');
+                expect(detail.kadastraalSubject.glossaryHeader.title).toBe('De Gemeente Gods Amsterdam');
+
+                expect(detail.nietNatuurlijkPersoon.isPresent).toBe(true);
+                expect(detail.nietNatuurlijkPersoon.descriptionList.term(0)).toBe('Statutaire zetel');
+                expect(detail.nietNatuurlijkPersoon.descriptionList.definition(0)).toBe('AMSTERDAM');
+                expect(detail.nietNatuurlijkPersoon.descriptionList.term(1)).toBe('Rechtsvorm');
+                expect(detail.nietNatuurlijkPersoon.descriptionList.definition(1)).toBe('Kerkelijke organisatie');
+
+                expect(detail.zakelijkRecht.isPresent).toBe(true);
+                // Todo: test real content here
+            });
         });
     });
 });
