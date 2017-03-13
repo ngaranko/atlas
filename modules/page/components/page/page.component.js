9ad9286a3ed18a9edc92b9cfbf6f4492653e9c0a
vegter <amsjurien@gmail.com>
2017-03-13 13:52:31 +0100

    Eerste versie

--- modules/page/components/page/page.component.js
+++ modules/page/components/page/page.component.js
@@ -10,6 +10,31 @@
                 item: '@'
             },
             templateUrl: 'modules/page/components/page/page.html',
+            controller: DpPageComponent,
             controllerAs: 'vm'
         });
+
+    DpPageComponent.inject = ['$scope', 'googleSheet', 'GOOGLE_SHEET_CMS'];
+
+    function DpPageComponent ($scope, googleSheet, GOOGLE_SHEET_CMS) {
+        let vm = this;
+
+        vm.feed = null;
+        vm.entries = [];
+        vm.entry = null;
+
+        $scope.$watchGroup(['vm.type', 'vm.item'], () => {
+            if (vm.type) {
+                vm.feed = null;
+                vm.entries = [];
+                vm.entry = null;
+                googleSheet.getContents(GOOGLE_SHEET_CMS.key, GOOGLE_SHEET_CMS.index[vm.type])
+                    .then(contents => {
+                        vm.feed = contents.feed;
+                        vm.entries = contents.entries;
+                        vm.entry = vm.entries.reduce((result, entry) => entry.id === vm.item ? entry : result, null);
+                    });
+            }
+        });
+    }
 })();
