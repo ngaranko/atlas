5e524728cabc9f281dfcc429dabdd9e4fbf3634e
Rob Maijers <rmaijers@users.noreply.github.com>
2017-04-12 11:02:48 +0200

    tests aangepast, die waren te veel afhankelijk van precieze tekst op homepage

--- e2e/validators/states/map_page--home.js
+++ e2e/validators/states/map_page--home.js
@@ -5,7 +5,7 @@ const mapPage = require('./map_page');
 module.exports = function (page) {
     expect(page.title).toBe('Home - Atlas');
 
-    expect(page.dashboard.rightColumn.page.text).toContain('applicatie van de gemeente Amsterdam');
+    expect(page.dashboard.rightColumn.page.text).toContain('een applicatie van de gemeente Amsterdam');
 
     mapPage(page);
 };
