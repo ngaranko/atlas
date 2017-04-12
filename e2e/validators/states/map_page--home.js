46b7043df39693ff14cabdca8742ee8a93af0760
Rob Maijers <rmaijers@users.noreply.github.com>
2017-04-12 10:59:29 +0200

    tests aangepast, die checkten te specifiek de tekst van de homepage

--- e2e/validators/states/map_page--home.js
+++ e2e/validators/states/map_page--home.js
@@ -5,8 +5,7 @@ const mapPage = require('./map_page');
 module.exports = function (page) {
     expect(page.title).toBe('Home - Atlas');
 
-    expect(page.dashboard.rightColumn.page.text).toContain('Welkom! Atlas');
-    expect(page.dashboard.rightColumn.page.text).toContain('Nieuw in Atlas');
+    expect(page.dashboard.rightColumn.page.text).toContain('applicatie van de gemeente Amsterdam');
 
     mapPage(page);
 };
