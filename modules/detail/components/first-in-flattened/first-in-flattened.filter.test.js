61f45d6466ca6362fa107e4426323cb2cff4fa1d
Jasper Swart <jasper.g.swart@gmail.com>
2017-04-26 12:30:01 +0200

    implemented review comments again tg-3268

--- modules/detail/components/first-in-flattened/first-in-flattened.filter.test.js
+++ modules/detail/components/first-in-flattened/first-in-flattened.filter.test.js
@@ -9,22 +9,22 @@ describe('The flatten filter', () => {
         });
     });
 
-    it('returns the first coordinate from flattened array 1', () => {
+    it('returns the first coordinate from single array with 2 elements', () => {
         expect(firstInFlattenedFilter([1, 2])).toBe('1,2');
     });
 
-    it('returns the first coordinate from flattened array 2', () => {
+    it('returns the first coordinate from nested array', () => {
         expect(firstInFlattenedFilter([[3, 4], [2, 6]])).toBe('3,4');
     });
 
-    it('returns the first coordinate from flattened array 3', () => {
+    it('returns the first coordinate from a doubly nested array', () => {
         expect(firstInFlattenedFilter([[[7, 6], [3, 4]]])).toBe('7,6');
     });
 
-    it('returns the first coordinate from flattened array 4', () => {
+    it('returns empty string when input array is empty', () => {
         expect(firstInFlattenedFilter([])).toBe('');
     });
-    it('returns the first coordinate from flattened array 5', () => {
+    it('returns empty string when input is string', () => {
         expect(firstInFlattenedFilter('aap')).toBe('');
     });
 });
