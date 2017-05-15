f696fed82dbd617c134614a644712fa17a92bb01
Jasper Swart <jasper.g.swart@gmail.com>
2017-05-15 09:03:52 +0200

    added support for rotated hotspots tg-3483

--- modules/straatbeeld/components/hotspot/hotspot.component.js
+++ modules/straatbeeld/components/hotspot/hotspot.component.js
@@ -18,26 +18,32 @@
 
     function DpHotspotController (store, ACTIONS, STRAATBEELD_CONFIG, angleConversion) {
         var vm = this,
-            realLifeHotspotSize = 0.3,
+            realLifeHotspotSize = 0.4,
             minDistance = 4,
             maxDistance = 21,
             correctedDistance,
             viewAngle,
-            viewport;
+            viewport = 960,
+            width,
+            height;
 
         /*
         All hotspots are shown, the min- and maxDistance variables are only used to determine the minimum and maximum
         hotspot size.
         */
         correctedDistance = Math.min(maxDistance, Math.max(minDistance, vm.distance));
-        viewAngle = Math.atan(realLifeHotspotSize / correctedDistance);
+        viewAngle = Math.atan(realLifeHotspotSize / correctedDistance),
+        width = Math.round(angleConversion.radiansToDegrees(viewAngle) * viewport / STRAATBEELD_CONFIG.DEFAULT_FOV);
+        height = width * 0.5 - (correctedDistance / maxDistance * 2);
 
         /*
         The actual hotspot size is dependent on the width of the straatbeeld and the FOV. For this first version we're
         making assumptions about the viewport and FOV.
         */
-        viewport = 960;
-        vm.size = Math.round(angleConversion.radiansToDegrees(viewAngle) * viewport / STRAATBEELD_CONFIG.DEFAULT_FOV);
+        vm.size = {
+            width: width,
+            height: height
+        };
 
         vm.loadScene = function () {
             store.dispatch({
