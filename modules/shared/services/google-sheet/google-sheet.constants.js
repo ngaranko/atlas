f0537779374fa7370124def3a75ef2bd1b7c8e4f
Boris Arkenaar <boris@arkenaar.com>
2017-05-29 13:18:05 +0200

    Cleaned up dead code for content events and version history

--- modules/shared/services/google-sheet/google-sheet.constants.js
+++ modules/shared/services/google-sheet/google-sheet.constants.js
@@ -14,14 +14,12 @@
             key: '1ZExuZHhmvBRP-7rhuY43Lv7dWuAsGKXwfKd1_3BZfbI',
             index: {
                 news: 1,
-                events: 2,
-                beleid: 3,
-                help: 4,
-                snelwegwijs: 5,
-                apis: 6,
-                versions: 7,
-                proclaimer: 8,
-                info: 9
+                beleid: 2,
+                help: 3,
+                snelwegwijs: 4,
+                apis: 5,
+                proclaimer: 6,
+                info: 7
             }
         });
 })();
