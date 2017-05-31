d043020bec3fe0d634dcd34e1f3bfc83c8c80f28
Ruurd Moelker <RRMoelker@users.noreply.github.com>
2017-05-31 13:32:11 +0200

    Video source and poster both passed to video component

--- modules/shared/components/video/video.component.js
+++ modules/shared/components/video/video.component.js
@@ -2,7 +2,6 @@
     'use strict';
 
     /**
-     * The dpVideo component for mp4 video.
      * The video playback is governed by the play variable set from outside this component
      * width and height bindings are required so the browser can reserve spacing before video load.
      * A poster image (splash image) is set on Safari to prevent nothing being displayed.
@@ -15,7 +14,8 @@
             controller: VideoController,
             controllerAs: 'vm',
             bindings: {
-                src: '<',
+                src: '<', // expects *.mp4
+                poster: '<',
                 width: '<',
                 height: '<',
                 play: '<'
@@ -36,7 +36,7 @@
                $window.navigator.userAgent && !$window.navigator.userAgent.match('CriOS');
 
             if (isSafari) {
-                videoElement.setAttribute('poster', vm.src + '.jpg');
+                videoElement.setAttribute('poster', vm.poster);
             }
         };
 
