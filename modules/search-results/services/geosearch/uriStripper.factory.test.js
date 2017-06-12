a2e2463dc8d3b4d2f4ca9c543cfef96f1081a8c3
Ruurd Moelker <RRMoelker@users.noreply.github.com>
2017-06-12 12:04:52 +0200

    URI stripper tested on none root URLs

--- modules/search-results/services/geosearch/uriStripper.factory.test.js
+++ modules/search-results/services/geosearch/uriStripper.factory.test.js
@@ -17,15 +17,35 @@ describe('The geosearch factory', function () {
         });
     });
 
-    it('strips the api root from the uri', () => {
+    it('strips the API root from the URI', () => {
         expect(uriStripper.stripUri(
             'https://api.data.amsterdam.nl/foo'
-            )).toBe('foo');
+        )).toBe(
+            'foo'
+        );
         expect(uriStripper.stripUri(
             'https://api.data.amsterdam.nl/meetbouten/meetbout/1234/'
-            )).toBe('meetbouten/meetbout/1234/');
+        )).toBe(
+            'meetbouten/meetbout/1234/'
+        );
         expect(uriStripper.stripUri(
             'https://api.data.amsterdam.nl/panorama/thumbnail/TMX1234-000030_pano_0001_000010/?width=140&heading=26'
-            )).toBe('panorama/thumbnail/TMX1234-000030_pano_0001_000010/?width=140&heading=26');
+        )).toBe(
+            'panorama/thumbnail/TMX1234-000030_pano_0001_000010/?width=140&heading=26'
+        );
+    });
+
+    it('does not strip none API roots from the URI', () => {
+        expect(uriStripper.stripUri(
+            'https://foo.amsterdam.nl/foo'
+        )).toBe(
+            'https://foo.amsterdam.nl/foo'
+        );
+
+        expect(uriStripper.stripUri(
+            'https://data.amsterdam.nl/foo'
+        )).toBe(
+            'https://data.amsterdam.nl/foo'
+        );
     });
 });
