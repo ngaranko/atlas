24f8bf75acf5f3a22987d6e0ddce823c69d7c873
Ruurd Moelker <RRMoelker@users.noreply.github.com>
2017-06-15 09:02:22 +0200

    Domain name stripping function name applied to test cases

--- modules/shared/services/uri-stripper/uriStripper.factory.test.js
+++ modules/shared/services/uri-stripper/uriStripper.factory.test.js
@@ -18,17 +18,17 @@ describe('The uri stripper factory', function () {
     });
 
     it('strips the API root from the URI', () => {
-        expect(uriStripper.stripUri(
+        expect(uriStripper.stripDomain(
             'https://api.data.amsterdam.nl/foo'
         )).toBe(
             'foo'
         );
-        expect(uriStripper.stripUri(
+        expect(uriStripper.stripDomain(
             'https://api.data.amsterdam.nl/meetbouten/meetbout/1234/'
         )).toBe(
             'meetbouten/meetbout/1234/'
         );
-        expect(uriStripper.stripUri(
+        expect(uriStripper.stripDomain(
             'https://api.data.amsterdam.nl/panorama/thumbnail/TMX1234-000030_pano_0001_000010/?width=140&heading=26'
         )).toBe(
             'panorama/thumbnail/TMX1234-000030_pano_0001_000010/?width=140&heading=26'
@@ -36,13 +36,13 @@ describe('The uri stripper factory', function () {
     });
 
     it('does not strip none API roots from the URI', () => {
-        expect(uriStripper.stripUri(
+        expect(uriStripper.stripDomain(
             'https://foo.amsterdam.nl/foo'
         )).toBe(
             'https://foo.amsterdam.nl/foo'
         );
 
-        expect(uriStripper.stripUri(
+        expect(uriStripper.stripDomain(
             'https://data.amsterdam.nl/foo'
         )).toBe(
             'https://data.amsterdam.nl/foo'
