4a0543994452207c72312097c68c063f2dd0e9a4
Jasper Swart <jasper.g.swart@gmail.com>
2017-06-16 16:04:33 +0200

    added todo tg-3234

--- modules/map/services/highlight/icon-config.constant.js
+++ modules/map/services/highlight/icon-config.constant.js
@@ -4,6 +4,7 @@
     angular
         .module('dpMap')
         .constant('ICON_CONFIG', {
+            // @TODO tg-3432 replace icon-search.png -> search.svg
             search: {
                 iconUrl: 'assets/icons/icon-search.png',
                 iconRetinaUrl: 'assets/icons/icon-search-retina.png',
@@ -15,12 +16,14 @@
                 iconSize: [21, 21],
                 iconAnchor: [10, 10]
             },
+            // @TODO tg-3432 replace icon-straatbeeld-orientation.png -> straatbeeld-orientation.svg
             straatbeeld_orientation: {
                 iconUrl: 'assets/icons/icon-straatbeeld-orientation.png',
                 iconRetinaUrl: 'assets/icons/icon-straatbeeld-orientation-retina.png',
                 iconSize: [62, 62],
                 iconAnchor: [31, 31]
             },
+            // @TODO tg-3432 replace icon-straatbeeld-person.png -> straatbeeld-person.svg
             straatbeeld_person: {
                 iconUrl: 'assets/icons/icon-straatbeeld-person.png',
                 iconRetinaUrl: 'assets/icons/icon-straatbeeld-person-retina.png',
