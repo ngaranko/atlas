6d057a2cc8bc649e69b1b91e85bd9090ac1bdc5a
Jasper Swart <jasper.g.swart@gmail.com>
2017-06-19 16:14:54 +0200

    implemented code review tg-3963

--- e2e/helpers/search.js
+++ e2e/helpers/search.js
@@ -5,6 +5,6 @@
  * @description this function enters a search query and then submits the form
  */
 module.exports = function (query) {
-    browser.driver.findElement(by.css('input.c-search-form__input')).sendKeys(query);
-    browser.driver.findElement(by.css('button.c-search-form__submit')).click();
+    browser.driver.findElement(by.css('.qa-search-form-input')).sendKeys(query);
+    browser.driver.findElement(by.css('.qa-search-form-submit')).click();
 };
