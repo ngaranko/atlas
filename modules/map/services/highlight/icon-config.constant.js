b58d66d9c57be408553939f4fec3123a780f3fe0
Jasper Swart <jasper.g.swart@gmail.com>
2017-06-19 13:09:54 +0200

    refactored search, straatbeeld-person and straatbeeld-orientation icons tg-3234

--- modules/map/services/highlight/icon-config.constant.js
+++ modules/map/services/highlight/icon-config.constant.js
@@ -4,29 +4,23 @@
     angular
         .module('dpMap')
         .constant('ICON_CONFIG', {
-            // @TODO tg-3432 replace icon-search.png -> search.svg
             search: {
-                iconUrl: 'assets/icons/icon-search.png',
-                iconRetinaUrl: 'assets/icons/icon-search-retina.png',
-                iconSize: [30, 48],
-                iconAnchor: [15, 51]
+                iconUrl: 'assets/images/map/search.svg',
+                iconSize: [48, 77],
+                iconAnchor: [23, 65]
             },
             detail: {
                 iconUrl: 'assets/images/map/detail.svg',
                 iconSize: [21, 21],
                 iconAnchor: [10, 10]
             },
-            // @TODO tg-3432 replace icon-straatbeeld-orientation.png -> straatbeeld-orientation.svg
             straatbeeld_orientation: {
-                iconUrl: 'assets/icons/icon-straatbeeld-orientation.png',
-                iconRetinaUrl: 'assets/icons/icon-straatbeeld-orientation-retina.png',
-                iconSize: [62, 62],
-                iconAnchor: [31, 31]
+                iconUrl: 'assets/images/map/straatbeeld-orientation.svg',
+                iconSize: [70, 70],
+                iconAnchor: [35, 35]
             },
-            // @TODO tg-3432 replace icon-straatbeeld-person.png -> straatbeeld-person.svg
             straatbeeld_person: {
-                iconUrl: 'assets/icons/icon-straatbeeld-person.png',
-                iconRetinaUrl: 'assets/icons/icon-straatbeeld-person-retina.png',
+                iconUrl: 'assets/images/map/straatbeeld-person.svg',
                 iconSize: [18, 31],
                 iconAnchor: [9, 22]
             }
