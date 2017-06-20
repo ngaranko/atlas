d7cb82f907d616ffcd79865fa724b07e35f84c3c
Jasper Swart <jasper.g.swart@gmail.com>
2017-06-20 08:57:07 +0200

    fixed e2e test search-results-kadastraal-subject tg-3963

--- e2e/search-results-kadastraal-subject.test.js
+++ e2e/search-results-kadastraal-subject.test.js
@@ -23,20 +23,33 @@ describe('Zoekresultaten kadastraal subjecten', () => {
             dp.authenticate.logout();
         });
 
-        dp.authenticate.roles.forEach(role => {
-            describe('als ' + role, () => {
-                it('er mogen sommige kadastraal subjecten zichtbaar zijn', () => {
-                    dp.authenticate.login(role);
+        describe('als EMPLOYEE', () => {
+            it('sommige kadastraal subjecten mogen zichtbaar zijn (alleen niet natuurlijke personen))', () => {
+                dp.authenticate.login('EMPLOYEE');
 
-                    dp.search('Bakker');
+                dp.search('Bakker');
 
-                    expect(page.title).toBe('Data met \'Bakker\' - Dataportaal');
+                expect(page.title).toBe('Data met \'Bakker\' - Dataportaal');
 
-                    expect(searchResults.categories(3).header).toContain('Maatschappelijke activiteiten');
+                expect(searchResults.categories(3).header).toContain('Maatschappelijke activiteiten');
 
-                    expect(searchResults.categories(4).header).toContain('Kadastrale subjecten');
-                    expect(searchResults.categories(4).listCount).not.toBe(0);
-                });
+                expect(searchResults.categories(4).header).toContain('Kadastrale subjecten (26)');
+                expect(searchResults.categories(4).listCount).not.toBe(0);
+            });
+        });
+
+        describe('als EMPLOYEE_PLUS', () => {
+            it('alle kadastraal subjecten moeten zichtbaar zijn', () => {
+                dp.authenticate.login('EMPLOYEE_PLUS');
+
+                dp.search('Bakker');
+
+                expect(page.title).toBe('Data met \'Bakker\' - Dataportaal');
+
+                expect(searchResults.categories(3).header).toContain('Maatschappelijke activiteiten');
+
+                expect(searchResults.categories(4).header).toContain('Kadastrale subjecten (1.347)');
+                expect(searchResults.categories(4).listCount).not.toBe(0);
             });
         });
     });
