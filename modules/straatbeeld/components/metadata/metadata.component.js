9ba27489e003b60fba01da95394ea48dd221fb68
Boris Arkenaar <boris@arkenaar.com>
2017-06-20 11:50:34 +0200

    First version of the pano history component and its styling

--- modules/straatbeeld/components/metadata/metadata.component.js
+++ modules/straatbeeld/components/metadata/metadata.component.js
@@ -19,21 +19,6 @@
     function DpStraatbeeldMetadataController ($scope) {
         const vm = this;
 
-        $scope.$watch ('vm.location', updateLocation, true);
-
-        function updateLocation () {
-            if (angular.isArray(vm.location)) {
-                vm.lat = vm.location[0];
-                vm.lon = vm.location[1];
-
-                const path = 'http://maps.google.com/maps?q=&layer=c&';
-                const parameters = `cbll=${vm.lat},${vm.lon}&cbp=11,${vm.heading},0,0,0`;
-
-                vm.streetviewUrl = `${path}${parameters}`;
-                vm.showStreetview = true;
-            }
-        }
-
         vm.showMetaInfo = () => angular.isDate(vm.date) && angular.isArray(vm.location);
     }
 })();
