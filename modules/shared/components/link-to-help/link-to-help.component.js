0de32041f09ced208a6789b2f670b6c622d67f87
Jasper Swart <jasper.g.swart@gmail.com>
2017-06-27 10:41:07 +0200

    added attributes and transclude to link-to-help tg-3923

--- modules/shared/components/link-to-help/link-to-help.component.js
+++ modules/shared/components/link-to-help/link-to-help.component.js
@@ -6,7 +6,29 @@
         .component('dpLinkToHelp', {
             templateUrl: 'modules/shared/components/link-to-help/link-to-help.html',
             transclude: true,
-            bindings: {},
+            bindings: {
+                type: '@',
+                item: '@'
+            },
+            controller: dpLinkToHelpController,
             controllerAs: 'vm'
         });
+
+    dpLinkToHelpController.$inject = ['$transclude'];
+
+    function dpLinkToHelpController ($transclude) {
+        const vm = this,
+            label = $transclude().text();
+
+        vm.payload = {
+            name: 'content-overzicht',
+            type: vm.type || 'snelwegwijs'
+        };
+
+        if (vm.item) {
+            vm.payload.item = `item${vm.item}`;
+        }
+
+        vm.label = label || 'Help > Bediening > Inloggen';
+    }
 })();
