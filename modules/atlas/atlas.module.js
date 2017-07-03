2262659080ab1c9ca0019b5e3067ac69dfa24af8
Boris Arkenaar <boris@arkenaar.com>
2017-07-03 12:31:49 +0200

    Add Sentry's Raven to our project

--- modules/atlas/atlas.module.js
+++ modules/atlas/atlas.module.js
@@ -1,6 +1,18 @@
 (function () {
     'use strict';
 
+    /* eslint-disable angular/window-service */
+    const Raven = window.Raven;
+    /* eslint-enable angular/window-service */
+
+    // Configure Raven (client library for Sentry) and register the ngRaven
+    // angular plugin with it. This needs to be done here first, so the atlas
+    // module can include ngRaven
+    Raven
+        .config('https://c2a0fef4816844efac43c156559d6fdc@sentry.io/185574')
+        .addPlugin(Raven.Plugins.Angular)
+        .install();
+
     angular
         .module('atlas', [
             // Main modules
@@ -17,6 +29,7 @@
             'dpShared',
 
             // Third party modules
-            'angulartics.piwik'
+            'angulartics.piwik',
+            'ngRaven'
         ]);
 })();
