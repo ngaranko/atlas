1dbe4f16fe4921487221bcd41fec25b5f957959b
Jasper Swart <jasper.g.swart@gmail.com>
2017-07-03 17:09:46 +0200

    updated correct link and html in embed tg-4019

--- modules/header/components/embed-header/embed-header.component.js
+++ modules/header/components/embed-header/embed-header.component.js
@@ -20,9 +20,14 @@
         function update () {
             const ghostState = angular.copy(store.getState());
 
+            // create link without embed AND embed preview in href
             ghostState.atlas.isEmbedPreview = false;
-
+            ghostState.atlas.isEmbed = false;
             vm.link = embed.getLink(ghostState);
+
+            // create iframe with embed but not embed preview in the html
+            ghostState.atlas.isEmbedPreview = false;
+            ghostState.atlas.isEmbed = true;
             vm.html = embed.getHtml(ghostState);
         }
     }
