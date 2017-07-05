a04292ac0a5d6343791c47b6c073583ee7744b04
Robbin Habermehl <robbin@habermehl.net>
2017-07-05 18:37:31 +0200

    Create Raven mock for each module

--- modules/shared/shared.module.test.js
+++ modules/shared/shared.module.test.js
@@ -1,12 +1,8 @@
 /* eslint-disable angular/window-service */
-describe('Mock Raven', () => {
-    beforeAll(() => {
-        window.Raven = {
-            config: () => ({ addPlugin: () => ({ install: angular.noop }) }),
-            Plugins: { Angular: {} }
-        };
-        angular.module('ngRaven', []).factory('Raven', () => {
-            return { captureMessage: angular.noop };
-        });
-    });
+window.Raven = {
+    config: () => ({ addPlugin: () => ({ install: angular.noop }) }),
+    Plugins: { Angular: {} }
+};
+angular.module('ngRaven', []).factory('Raven', () => {
+    return { captureMessage: angular.noop };
 });
