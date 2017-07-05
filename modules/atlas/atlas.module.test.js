61d5df348b4ac03b1d35b895f8b212fe3e7f902b
Robbin Habermehl <robbin@habermehl.net>
2017-07-05 15:12:05 +0200

    Stub Raven in unit tests

---
+++ modules/atlas/atlas.module.test.js
@@ -0,0 +1,12 @@
+/* eslint-disable angular/window-service */
+describe('Mock Raven', () => {
+    beforeAll(() => {
+        window.Raven = window.Raven || {
+            config: () => ({ addPlugin: () => ({ install: angular.noop }) }),
+            Plugins: { Angular: {} }
+        };
+        angular.module('ngRaven', []).factory('Raven', () => {
+            return { captureMessage: angular.noop };
+        });
+    });
+});
