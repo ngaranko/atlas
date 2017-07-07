1b8cf0e0e2f514d2ff339fd66d313c94a23933cd
Jasper Swart <jasper.g.swart@gmail.com>
2017-07-07 15:59:58 +0200

    fixed typo

--- e2e/helpers/log.js
+++ e2e/helpers/log.js
@@ -4,12 +4,12 @@ module.exports = (label, object) => {
     if (!object) {
         /* eslint-disable no-console, angular/log */
         console.log(label);
-        /* eslint-emable no-console, angular/log */
+        /* eslint-enable no-console, angular/log */
     } else {
         object.then(value => {
             /* eslint-disable no-console, angular/log */
             console.log(`${label}: `, value);
-            /* eslint-emable no-console, angular/log */
+            /* eslint-enable no-console, angular/log */
         });
     }
 };
