cfc0f0a2fe773b7e3fcb664788d3ff770368c4fd
Jasper Swart <jasper.g.swart@gmail.com>
2017-07-10 11:55:46 +0200

    added remove unused branches

---
+++ scripts/remove-merged-branches.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CURRENT_BRANCH=$(git status | head -n 1 | awk '{print $3}')
+
+if [ $CURRENT_BRANCH != "develop" ]; then
+    echo "Current branch is not 'develop'" >&2
+    exit 1
+else
+    echo "Removing merged branches:"
+    git branch --merged | grep -v dev | xargs git branch -d
+fi
