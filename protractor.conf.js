a30a2cdbc89531c9818420365de94d1b1b7be335
Jasper Swart <jasper.g.swart@gmail.com>
2017-07-10 09:20:49 +0200

    turned on e2e testing tg-4023

--- protractor.conf.js
+++ protractor.conf.js
@@ -15,14 +15,15 @@ for (var VARIABLE of ENV_VARIABLES) {
 exports.config = {
     baseUrl: 'http://localhost:8000/',
     framework: 'jasmine',
-    specs: 'e2e/**/*.disabled.js',
+    specs: 'e2e/**/*.test.js',
     capabilities: {
         browserName: 'phantomjs',
         shardTestFiles: true,
         maxInstances: 4,
         'phantomjs.binary.path': './node_modules/phantomjs-prebuilt/bin/phantomjs'
     },
-    seleniumServerJar: 'node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-3.4.0.jar',
+    seleniumServerJar: 'node_modules/protractor/node_modules/webdriver-manager/selenium/' +
+        'selenium-server-standalone-3.4.0.jar',
     chromeDriver: './node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.29',
     onPrepare: function () {
         browser.driver.manage().window().setSize(1024, 768);
