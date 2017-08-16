c035b876296d1ae3a7ec76631c6668511c3ca3b6
Jasper Swart <jasper.g.swart@gmail.com>
2017-08-16 13:14:58 +0200

    fixed typo tg-4326

--- modules/search-results/components/search-results/search-results.component.test.js
+++ modules/search-results/components/search-results/search-results.component.test.js
@@ -519,7 +519,7 @@ describe('The dp-search-results component', function () {
             expect(isolated.vm.layerWarning).toBe('foo, bar');
         });
 
-        it('shows no layer warning when tehre is a query present', () => {
+        it('shows no layer warning when there is a query present', () => {
             activeOverlays.getOverlaysWarning.and.returnValue('foo, bar');
 
             component = getComponent(22, 'water', [51.123, 4.789]);
