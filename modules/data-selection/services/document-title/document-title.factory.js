e0a8e2296fa526ccb2e138625964a60e7db47495
Jasper Swart <jasper.g.swart@gmail.com>
2017-08-24 13:28:22 +0200

    fixed data selection document title tg-3876

--- modules/data-selection/services/document-title/document-title.factory.js
+++ modules/data-selection/services/document-title/document-title.factory.js
@@ -15,66 +15,65 @@
 
         // TODO: Might be worth replacing with more advanced templating that allows conditions like Mustache (#3335)
         // eslint-disable-next-line complexity
-        function getTitle (dataSelectionState) {
-            // let output,
-            //     view,
-            //     variant,
-            //     markers,
-            //     criteria;
-            //
-            // const VIEW_NAMES = {
-            //     TABLE: 'Tabel',
-            //     LIST: 'Lijst',
-            //     CARDS: 'Datasets'
-            // };
-            //
-            // if (dataSelectionState.view === 'CARDS' && Object.keys(dataSelectionState.filters).length === 0) {
-            //     if (dataSelectionState.query) {
-            //         return `Datasets met '${dataSelectionState.query}'`;
-            //     } else {
-            //         return 'Datasets';
-            //     }
-            // } else {
-            //     view = VIEW_NAMES[dataSelectionState.view];
-            //     variant = lowercaseFilter(DATA_SELECTION_CONFIG.datasets[dataSelectionState.dataset].TITLE);
-            //     markers = dataSelectionState.geometryFilter.markers || [];
-            //     criteria = DATA_SELECTION_CONFIG.datasets[dataSelectionState.dataset].FILTERS
-            //     // Retrieve all the active filters
-            //         .filter(availableFilter => angular.isDefined(dataSelectionState.filters[availableFilter.slug]))
-            //         // Show the value of each active filter
-            //         .map(activeFilter => dataSelectionState.filters[activeFilter.slug])
-            //         .join(', ');
-            //
-            //     output = view;
-            //
-            //     if (variant !== 'catalogus') {
-            //         output += ` ${variant}`;
-            //     }
-            //
-            //     if (markers.length || dataSelectionState.query || criteria.length) {
-            //         output += ' met ';
-            //     }
-            //
-            //     if (markers.length) {
-            //         // NB: Manual replacement of the superscript 2 is required due to improper browser rendering
-            //         const geometryFilterDescription = dataSelectionState.geometryFilter.description
-            //                 .replace('&sup2;', '²');
-            //         output += `ingetekend (${geometryFilterDescription})`;
-            //     }
-            //
-            //     if (dataSelectionState.query) {
-            //         output += `'${dataSelectionState.query}'`;
-            //     }
-            //
-            //     if ((markers.length && criteria.length) || (dataSelectionState.query && criteria.length)) {
-            //         output += ', ';
-            //     }
-            //
-            //     output += criteria;
-            //
-            //     return output;
-            // }
-            return 'Datasets';
+        function getTitle (dataSelectionState, filtersState) {
+            let output,
+                view,
+                variant,
+                markers,
+                criteria;
+
+            const VIEW_NAMES = {
+                TABLE: 'Tabel',
+                LIST: 'Lijst',
+                CARDS: 'Datasets'
+            };
+
+            if (dataSelectionState.view === 'CARDS' && Object.keys(filtersState).length === 0) {
+                if (dataSelectionState.query) {
+                    return `Datasets met '${dataSelectionState.query}'`;
+                } else {
+                    return 'Datasets';
+                }
+            } else {
+                view = VIEW_NAMES[dataSelectionState.view];
+                variant = lowercaseFilter(DATA_SELECTION_CONFIG.datasets[dataSelectionState.dataset].TITLE);
+                markers = dataSelectionState.geometryFilter.markers || [];
+                criteria = DATA_SELECTION_CONFIG.datasets[dataSelectionState.dataset].FILTERS
+                // Retrieve all the active filters
+                    .filter(availableFilter => angular.isDefined(filtersState[availableFilter.slug]))
+                    // Show the value of each active filter
+                    .map(activeFilter => filtersState[activeFilter.slug])
+                    .join(', ');
+
+                output = view;
+
+                if (variant !== 'catalogus') {
+                    output += ` ${variant}`;
+                }
+
+                if (markers.length || dataSelectionState.query || criteria.length) {
+                    output += ' met ';
+                }
+
+                if (markers.length) {
+                    // NB: Manual replacement of the superscript 2 is required due to improper browser rendering
+                    const geometryFilterDescription = dataSelectionState.geometryFilter.description
+                            .replace('&sup2;', '²');
+                    output += `ingetekend (${geometryFilterDescription})`;
+                }
+
+                if (dataSelectionState.query) {
+                    output += `'${dataSelectionState.query}'`;
+                }
+
+                if ((markers.length && criteria.length) || (dataSelectionState.query && criteria.length)) {
+                    output += ', ';
+                }
+
+                output += criteria;
+
+                return output;
+            }
         }
     }
 })();
