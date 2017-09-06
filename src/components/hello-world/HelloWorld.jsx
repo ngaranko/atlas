d267f1fd80eb6d540f93b8f463732e5361b84c96
Robbin Habermehl <robbin@habermehl.net>
2017-09-06 10:35:37 +0200

    Add Redux to example

--- src/components/hello-world/HelloWorld.jsx
+++ src/components/hello-world/HelloWorld.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
-const HelloWorld = () => {
-  return <p>Hello world</p>;
-};
+import DemoContainer from '../../containers/demo/DemoContainer.jsx';
+
+const HelloWorld = () => (
+  <Provider store={window.reduxStore}>
+    <DemoContainer></DemoContainer>
+  </Provider>
+);
 
 export default HelloWorld;
