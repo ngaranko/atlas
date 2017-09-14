dc3564213ecf0f0d2f6243129e553046652aeaa7
Robbin Habermehl <robbin@habermehl.net>
2017-09-14 14:28:15 +0200

    Adjust root saga implementation

--- src/map/sagas/index.js
+++ src/map/sagas/index.js
@@ -1,9 +1,9 @@
-import { fork } from 'redux-saga/effects';
+import { all, fork } from 'redux-saga/effects';
 
 import watchFetchMapLayers from './layers/map-layers';
 
 export default function* rootSaga() {
-  yield [
+  yield all([
     fork(watchFetchMapLayers)
-  ];
+  ]);
 }
