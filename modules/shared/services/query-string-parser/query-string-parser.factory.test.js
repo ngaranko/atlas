ccf96e855de53fed766bc7e1a6791df94aa83ff3
Boris Arkenaar <boris@arkenaar.com>
2017-09-26 15:16:04 +0200

    Implemented query string parser unit test suite

--- modules/shared/services/query-string-parser/query-string-parser.factory.test.js
+++ modules/shared/services/query-string-parser/query-string-parser.factory.test.js
@@ -1,68 +1,40 @@
 describe('The query string parser factory', function () {
-    const byteString = '048>IYceiv{ÈÌÐàð';
-    const asciiString = 'abcd+efgh==';
-
-    let $window;
-    let stateTokenGenerator;
+    let queryStringParser;
 
     beforeEach(function () {
-        angular.mock.module(
-            'dpShared',
-            {
-                $window: {
-                    crypto: {
-                        getRandomValues: (list) => {
-                            list[0] = 48; // 0
-                            list[1] = 52; // 4
-                            list[2] = 56; // 8
-                            list[3] = 62; // >
-                            list[4] = 73; // I
-                            list[5] = 89; // Y
-                            list[6] = 99; // c
-                            list[7] = 101; // e
-                            list[8] = 105; // i
-                            list[9] = 118; // v
-                            list[10] = 123; // {
-                            list[11] = 200; // È
-                            list[12] = 204; // Ì
-                            list[13] = 208; // Ð
-                            list[14] = 224; // à
-                            list[15] = 240; // ð
-                        }
-                    },
-                    btoa: angular.noop
-                }
-            }
-        );
+        angular.mock.module('dpShared');
 
-        angular.mock.inject(function (_$window_, _stateTokenGenerator_) {
-            $window = _$window_;
-            stateTokenGenerator = _stateTokenGenerator_;
+        angular.mock.inject(function (_queryStringParser_) {
+            queryStringParser = _queryStringParser_;
         });
-
-        spyOn($window, 'btoa').and.returnValue(asciiString);
     });
 
-    it('uses the msCrypto library when crypto is not available (IE11)', () => {
-        $window.msCrypto = $window.crypto;
-        delete $window.crypto;
-        const stateToken = stateTokenGenerator();
-
-        expect($window.btoa).toHaveBeenCalledWith(byteString);
-        expect(stateToken).toBe(asciiString);
+    it('turns a query string into an object', () => {
+        expect(
+            queryStringParser('?a=b&one=1&bool=false')
+        ).toEqual({
+            a: 'b',
+            one: '1',
+            bool: 'false'
+        });
     });
 
-    it('returns an empty string when the crypto library is not available', () => {
-        delete $window.crypto;
-        const stateToken = stateTokenGenerator();
-
-        expect($window.btoa).not.toHaveBeenCalled();
-        expect(stateToken).toBe('');
+    it('ignores the first character', () => {
+        expect(
+            queryStringParser('/a=b&one=1&bool=false')
+        ).toEqual({
+            a: 'b',
+            one: '1',
+            bool: 'false'
+        });
     });
 
-    it('it generates a random string of characters and url encodes it', () => {
-        const stateToken = stateTokenGenerator();
-        expect($window.btoa).toHaveBeenCalledWith(byteString);
-        expect(stateToken).toBe(asciiString);
+    it('decodes keys and values', () => {
+        expect(
+            queryStringParser('?a=b%20c&one%2Ftwo=12')
+        ).toEqual({
+            a: 'b c',
+            'one/two': '12'
+        });
     });
 });
