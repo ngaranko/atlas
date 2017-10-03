e3fe27fc9d0cef3a7b6bf8afcfc5e1dbbff1a084
Jasper Swart <jasper.g.swart@gmail.com>
2017-10-03 09:07:09 +0200

    refactored with vm.isMapHighlight tg-4534

--- modules/detail/components/detail/detail.component.js
+++ modules/detail/components/detail/detail.component.js
@@ -62,8 +62,6 @@
         });
 
         function getData (endpoint) {
-            const state = store.getState();
-
             vm.location = null;
 
             vm.includeSrc = endpointParser.getTemplateUrl(endpoint);
@@ -72,7 +70,7 @@
             // Derive whether more info is available if the user would be authenticated
             // stored as separate variable to prevent vm manipulation to change the controller logic
             vm.showMoreInfoWarning = !vm.isEmployee;
-            vm.geosearchButton = state.map.highlight ? false : nearestDetail.getLocation();
+            vm.geosearchButton = vm.isMapHighlight ? false : nearestDetail.getLocation();
 
             const [category, subject] = endpointParser.getParts(endpoint);
             if (!vm.isEmployee && category === 'brk' && subject === 'subject') {
