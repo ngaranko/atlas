a962bfa06cd3cd2b71cd9c0d424c72a108e960fe
Jasper Swart <jasper.g.swart@gmail.com>
2017-10-04 13:09:31 +0200

    updated sagas watcher tg-4382

--- src/map/sagas/index.js
+++ src/map/sagas/index.js
@@ -1,9 +1,11 @@
 import { all, fork } from 'redux-saga/effects';
 
+import watchFetchBaseLayers from './layers/base-layers';
 import watchFetchMapLayers from './layers/map-layers';
 
 export default function* rootSaga() {
   yield all([
+    fork(watchFetchBaseLayers),
     fork(watchFetchMapLayers)
   ]);
 }
