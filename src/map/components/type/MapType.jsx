9a5fb5d67e6a9d7847be780d281249917b10ba1e
Jasper Swart <jasper.g.swart@gmail.com>
2017-10-05 13:33:37 +0200

    fixed styling tg-4382

--- src/map/components/type/MapType.jsx
+++ src/map/components/type/MapType.jsx
@@ -7,13 +7,23 @@ const MapType = ({ layers }) => (
   <section className="map-panel__map-type">maptype
     <Select name="topography" value="topo_rd">
       {layers.topography ? layers.topography.map(layer => (
-        <option value={layer.id} key={layer.id}>{layer.label}</option>
+        <option
+          value={layer.id}
+          key={layer.id}
+        >
+          {layer.label}
+        </option>
       )) : ''}
     </Select>
 
     <Select name="aerial" value="lf2017">
       {layers.aerial ? layers.aerial.map(layer => (
-        <option value={layer.id} key={layer.id}>{layer.label}</option>
+        <option
+          value={layer.id}
+          key={layer.id}
+        >
+          {layer.label}
+        </option>
       )) : ''}
     </Select>
   </section>
