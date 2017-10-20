40bf900be18030b602d97db292f158500f176f41
Robbin Habermehl <robbin@habermehl.net>
2017-10-20 17:20:23 +0200

    Configure Jest/Enzyme and create first unit test

---
+++ src/map/containers/panel/MapPanelContainer.test.jsx
@@ -0,0 +1,40 @@
+ /* eslint-disable */
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import { shallow } from 'enzyme';
+
+import MapPanelContainer from './MapPanelContainer';
+import MapLayers from '../../components/layers/MapLayers';
+import MapLegend from '../../components/legend/MapLegend';
+import MapType from '../../components/type/MapType';
+
+describe('MapPanelContainer', () => {
+  let store;
+  let wrapper;
+
+  beforeEach(() => {
+    store = configureMockStore()({
+      map: {
+        baseLayer: '',
+        overlays: []
+      },
+      mapLayers: [],
+      overlays: [{}]
+    });
+    wrapper = shallow(<MapPanelContainer />, { context: { store } }).dive();
+  });
+
+  it('should render MapType and MapLayers', () => {
+    expect(wrapper.find(MapType).length).toBe(1);
+    expect(wrapper.find(MapLayers).length).toBe(1);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render MapLegend if store contains active map layers', () => {
+    expect(wrapper.find(MapLegend).length).toBe(0);
+    wrapper.setProps({ activeMapLayers: [{}] });
+    expect(wrapper.find(MapLegend).length).toBe(1);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
