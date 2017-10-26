690c6a2f378899b41e1ab3c2c86787a1bb078e61
Boris Arkenaar <boris@arkenaar.com>
2017-10-26 09:40:46 +0200

    Removed most of grunt dependencies and config. Only for svg-sprite is left.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,11 @@
 module.exports = function (grunt) {
-    require('./grunt/tasks/config')(grunt);
-    require('./grunt/tasks/build-tasks')(grunt);
-    require('./grunt/tasks/js-tasks')(grunt);
-    require('./grunt/tasks/css-tasks')(grunt);
-    require('./grunt/tasks/qa-tasks')(grunt);
+    const gruntDir = './grunt/';
 
-    /**
-     * 'default' formerly known as 'grunt serve'
-     */
-    grunt.registerTask('default', [
-        'build-develop',
-        'connect:build',
-        'watch'
-    ]);
+    grunt.initConfig({
+        svg_sprite: require(gruntDir + 'svg-sprite')
+    });
 
-    grunt.registerTask('test', [
-        'build-develop',
-        'test-js',
-        'test-css'
-    ]);
+    grunt.loadNpmTasks('grunt-svg-sprite');
 
-    grunt.registerTask('fulltest', [
-        'build-develop',
-        'test-js-full',
-        'test-css'
-    ]);
+    grunt.registerTask('default', ['svg_sprite']);
 };
