b9b12ef352c0d1a2fa6c4f644aa393326defd0f2
Boris Arkenaar <boris@arkenaar.com>
2017-11-09 18:45:15 +0100

    Fixed linting issue

--- src/shared/components/loading-indicator/LoadingIndicator.jsx
+++ src/shared/components/loading-indicator/LoadingIndicator.jsx
@@ -3,14 +3,10 @@ import React from 'react';
 import './_loading-indicator.scss';
 import SpinnerIcon from '../../../../public/images/icon-tick.svg';
 
-class LoadingIndicator extends React.Component {
-  render() {
-    return (
-      <span className="loading-indicator">
-        <SpinnerIcon />
-      </span>
-    );
-  }
-}
+const LoadingIndicator = () => (
+  <span className="loading-indicator">
+    <SpinnerIcon />
+  </span>
+);
 
 export default LoadingIndicator;
