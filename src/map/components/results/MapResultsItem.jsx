a6d444366773d995c321425f3485aeab5340ccfc
Boris Arkenaar <boris@arkenaar.com>
2017-11-13 14:07:54 +0100

    Results item is now a button and has an arrow icon

--- src/map/components/results/MapResultsItem.jsx
+++ src/map/components/results/MapResultsItem.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ArrowRightIcon from '../../../../public/images/icon-arrow-right.svg';
+
 const MapResultsItem = ({ item }) => (
-  <section className="map-results__item">
-    <div className="map-results__item-category">{item.categoryLabel}</div>
-    <div className="map-results__item-name">{item.label}</div>
-  </section>
+  <button className="map-results__item">
+    <section className="map-results__item-content">
+      <div className="map-results__item-category">{item.categoryLabel}</div>
+      <div className="map-results__item-name">{item.label}</div>
+    </section>
+    <ArrowRightIcon className="map-results__item-arrow" />
+  </button>
 );
 
 MapResultsItem.propTypes = {
