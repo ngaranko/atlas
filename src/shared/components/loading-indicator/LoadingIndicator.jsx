46ba3c7e60167ca1b9df67a637184d4422a35976
Boris Arkenaar <boris@arkenaar.com>
2017-11-13 12:06:24 +0100

    Added loading indicator icon

--- src/shared/components/loading-indicator/LoadingIndicator.jsx
+++ src/shared/components/loading-indicator/LoadingIndicator.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './_loading-indicator.scss';
-import SpinnerIcon from '../../../../public/images/icon-tick.svg';
+import SpinnerIcon from '../../../../public/images/icon-spinner.svg';
 
 const LoadingIndicator = () => (
   <span className="loading-indicator">
