5a007fec14344d79052abcf17c310c6700174f01
Robbin Habermehl <robbin@habermehl.net>
2017-11-21 18:02:21 +0100

    Create notification component, show in map panel if unauthorized

--- src/map/components/layers/MapLayers.jsx
+++ src/map/components/layers/MapLayers.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import MapLegend from '../legend/MapLegend';
+import Notification from '../../../shared/components/notification/Notification';
 import AddIcon from '../../../../public/images/icon-plus.svg';
 import RemoveIcon from '../../../../public/images/icon-cross.svg';
 
@@ -14,42 +15,49 @@ const showCategory = (layers, user) => (
 );
 
 const MapLayers = ({ activeMapLayers, layers, onLayerToggle, user }) => (
-  <ul className="map-layers">
-    {[...new Set(showCategory(layers, user).map(layer => layer.category))].map(category => (
-      <li className="map-layers__category" key={category}>
-        {category}
-        <ul>
-          {layers
-            .filter(layer => layer.category === category && showLayer(layer, user))
-            .map(layer => (
-              <li
-                className={`
-                  map-layers__title
-                  map-layers__title--${activeMapLayers.some(mapLayer => layer.title === mapLayer.title) ? 'active' : 'inactive'}
-                `}
-                key={layer.title}
-              >
-                <button onClick={() => {
-                  MapLegend.mapLayersLegendItemsToIds(layer).forEach(mapLayerId =>
-                    onLayerToggle(mapLayerId));
-                }}
+  <div>
+    <ul className="map-layers">
+      {[...new Set(showCategory(layers, user).map(layer => layer.category))].map(category => (
+        <li className="map-layers__category" key={category}>
+          {category}
+          <ul>
+            {layers
+              .filter(layer => layer.category === category && showLayer(layer, user))
+              .map(layer => (
+                <li
+                  className={`
+                    map-layers__title
+                    map-layers__title--${activeMapLayers.some(mapLayer => layer.title === mapLayer.title) ? 'active' : 'inactive'}
+                  `}
+                  key={layer.title}
                 >
-                  <span>
-                    {layer.title}
-                  </span>
-                  <span className="map-layers__toggle map-layers__toggle--remove">
-                    <RemoveIcon />
-                  </span>
-                  <span className="map-layers__toggle map-layers__toggle--add">
-                    <AddIcon />
-                  </span>
-                </button>
-              </li>
-          ))}
-        </ul>
-      </li>
-    ))}
-  </ul>
+                  <button onClick={() => {
+                    MapLegend.mapLayersLegendItemsToIds(layer).forEach(mapLayerId =>
+                      onLayerToggle(mapLayerId));
+                  }}
+                  >
+                    <span>
+                      {layer.title}
+                    </span>
+                    <span className="map-layers__toggle map-layers__toggle--remove">
+                      <RemoveIcon />
+                    </span>
+                    <span className="map-layers__toggle map-layers__toggle--add">
+                      <AddIcon />
+                    </span>
+                  </button>
+                </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
+    {!user.authenticated && (
+      <Notification className="map-layers__authorization-notification">
+        <a href="">Meer kaartlagen</a> na inloggen
+      </Notification>
+    )}
+  </div>
 );
 
 MapLayers.propTypes = {
---
+++ src/shared/components/notification/Notification.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import RemoveIcon from '../../../../public/images/icon-cross.svg';
+
+import './_notification.scss';
+
+class Notification extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isNotificationVisible: true };
+
+    this.hideNotification = this.hideNotification.bind(this);
+  }
+
+  hideNotification() {
+    this.setState({ isNotificationVisible: false });
+  }
+
+  render() {
+    return this.state.isNotificationVisible && (
+      <p className={`
+        ${this.props.className}
+        notification
+        notification--${this.props.level}
+      `}
+      >
+        <span className="notification__content">{this.props.children}</span>
+        <button
+          className="notification__button"
+          onClick={this.hideNotification}
+        >
+          <RemoveIcon />
+        </button>
+      </p>
+    );
+  }
+}
+
+Notification.defaultProps = {
+  className: '',
+  level: 'info'
+};
+
+Notification.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+  className: PropTypes.string,
+  level: PropTypes.oneOf(['alert', 'info'])
+};
+
+export default Notification;
