1f03e4ba41b7bbb8d09d0743cb846615bfaabd3d
Rob Maijers <rmaijers@users.noreply.github.com>
2017-12-01 15:45:17 +0100

    verkeerstromenkaartnaam

--- src/map/services/map-layers.js
+++ src/map/services/map-layers.js
@@ -1785,7 +1785,7 @@ export default [
     ],
     maxZoom: 16,
     minZoom: 8,
-    title: 'Verkeersstromen - Snelheden',
+    title: 'Verkeersstromen - Snelheden (~10 min. geleden)',
     url: '/maps/reistijdenauto?version=1.3.0&service=WMS'
   },
   {
