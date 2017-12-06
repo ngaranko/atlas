5fedf88a53c4ad18a304e9334eb71822f863d16d
Jasper Swart <jasper.g.swart@gmail.com>
2017-12-06 10:41:29 +0100

    added spawning of child process tg-4740

--- pa11y.js
+++ pa11y.js
@@ -3,6 +3,9 @@
 const pa11y = require('pa11y');
 const reporter = require('pa11y-reporter-cli');
 
+const { spawn } = require('child_process');
+const npmStart = spawn('npm', ['start']);
+
 const urls = [
     // homepage
     'http://localhost:8080/#?mpb=topografie&mpz=11&mpv=52.3731081:4.8932945&pgn=home',
@@ -11,15 +14,30 @@ const urls = [
     'http://localhost:8080/#?dsd=bag&dsp=1&dsv=TABLE&mpb=topografie&mpz=11&mpv=52.3731081:4.8932945',
 
     // straatbeeld
-    'http://localhost:8080/#?mpb=topografie&mpz=11&mpo=pano::T&mpv=52.3730353:4.8932471&pgn=home&sbf=Cu&sbh=aS&sbi=TMX7315120208-000073_pano_0005_000449&sbl=ZRWBl:3JJZP'
+    'http://localhost:8080/#?mpb=topografie&mpz=11&mpo=pano::T&mpv=52.3730353:4.8932471&pgn=home&sbf=Cu' +
+        '&sbh=aS&sbi=TMX7315120208-000073_pano_0005_000449&sbl=ZRWBl:3JJZP'
 ];
 
-// console.log('reporter', urls[0], reporter);
-urls.forEach(url => {
-    pa11y(url, {
-        allowedStandards: ['WCAG2AA']
-    }).then((results) => {
-        const cliResults = reporter.results(results);
-        console.log(cliResults);
+/* eslint-disable no-console, angular/log */
+console.log('Testing pa11y...');
+/* eslint-enable no-console, angular/log */
+
+npmStart.stdout.on('data', (buffer) => {
+    if (!buffer.toString('utf8').includes('Child html-webpack-plugin for "index.html":')) {
+        return;
+    }
+    urls.forEach((url, index) => {
+        pa11y(url, {
+            allowedStandards: ['WCAG2AA']
+        }).then((results) => {
+            /* eslint-disable no-console, angular/log */
+            console.log(reporter.results(results));
+            /* eslint-enable no-console, angular/log */
+
+            if (urls.length === index + 1) {
+                process.exit();
+            }
+        });
+
     });
 });
