c11b7c31754d056982ee14320ee6ffed207fe99b
Jasper Swart <jasper.g.swart@gmail.com>
2017-12-07 17:06:34 +0100

    pa11y test now promise tg-4740

--- test/pa11y.js
+++ test/pa11y.js
@@ -10,8 +10,11 @@ const urls = require('./pa11y-urls');
 
 let count = 0;
 
+const allTests = [];
+
 /* eslint-disable no-console, angular/log */
-console.log('Testing pa11y...');
+console.log('Pa11y testing...');
+console.log('Pa11y: starting npm...');
 /* eslint-enable no-console, angular/log */
 
 npmStart.stdout.on('data', (buffer) => {
@@ -25,26 +28,31 @@ npmStart.stdout.on('data', (buffer) => {
         return;
     }
 
-    let index = 0;
-
     urls.forEach((item) => {
-        pa11y(item.url, {
+        allTests.push(pa11y(item.url, {
             allowedStandards: ['WCAG2AA'],
             rootElement: item.rootElement
-        }).then((results) => {
-            /* eslint-disable no-console, angular/log */
-            console.log(`Testing pa11y item: ${item.name}`);
-            /* eslint-enable no-console, angular/log */
+        }));
+    });
 
-            /* eslint-disable no-console, angular/log */
-            console.log(reporter.results(results));
-            /* eslint-enable no-console, angular/log */
-            if (urls.length === index + 1) {
-                process.kill(-npmStart.pid, 'SIGKILL');
-                process.exit();
+    /* eslint-disable no-console, angular/log */
+    console.log('Pa11y: running tests...');
+    /* eslint-enable no-console, angular/log */
+
+    Promise.all(allTests).then(results => {
+        results.forEach(result => {
+            if (result.documentTitle) {
+                /* eslint-disable no-console, angular/log */
+                console.log(`Pa11y: testing item: ${result.documentTitle}`);
+                /* eslint-enable no-console, angular/log */
+
+                /* eslint-disable no-console, angular/log */
+                console.log(reporter.results(result));
+                /* eslint-enable no-console, angular/log */
             }
-
-            index++;
         });
+
+        process.kill(-npmStart.pid, 'SIGKILL');
+        process.exit();
     });
 });
