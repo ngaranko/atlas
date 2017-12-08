49ebc1233a09b8555d055447e2b7c88c526cb179
Jasper Swart <jasper.g.swart@gmail.com>
2017-12-08 09:56:42 +0100

    added reporting with totals tg-4740

--- test/pa11y.js
+++ test/pa11y.js
@@ -2,6 +2,7 @@
 
 const pa11y = require('pa11y');
 const reporter = require('pa11y-reporter-cli');
+const chalk = require('chalk');
 
 const { spawn } = require('child_process');
 const npmStart = spawn('npm', ['start'], { detached: true });
@@ -40,6 +41,12 @@ npmStart.stdout.on('data', (buffer) => {
     /* eslint-enable no-console, angular/log */
 
     Promise.all(allTests).then(results => {
+        const total = {
+            errors: 0,
+            warnings: 0,
+            notices: 0
+        };
+
         results.forEach(result => {
             if (result.documentTitle) {
                 /* eslint-disable no-console, angular/log */
@@ -49,8 +56,32 @@ npmStart.stdout.on('data', (buffer) => {
                 /* eslint-disable no-console, angular/log */
                 console.log(reporter.results(result));
                 /* eslint-enable no-console, angular/log */
+
+                total.errors += result.issues.reduce((acc, issue) => (issue.type === 'error') * 1 + acc, 0);
+                total.warnings += result.issues.reduce((acc, issue) => (issue.type === 'warning') * 1 + acc, 0);
+                total.notices += result.issues.reduce((acc, issue) => (issue.type === 'notice') * 1 + acc, 0);
             }
         });
+
+        /* eslint-disable no-console, angular/log */
+        console.log('Pa11y: TOTALS');
+        /* eslint-enable no-console, angular/log */
+
+        if (total.errors) {
+            /* eslint-disable no-console, angular/log */
+            console.log(chalk.red('Pa11y: total errors', total.errors));
+            /* eslint-enable no-console, angular/log */
+        }
+        if (total.warnings) {
+            /* eslint-disable no-console, angular/log */
+            console.log(chalk.yellow('Pa11y: total warnings', total.warnings));
+            /* eslint-enable no-console, angular/log */
+        }
+        if (total.notices) {
+            /* eslint-disable no-console, angular/log */
+            console.log(chalk.cyan('Pa11y: total notices', total.notices));
+            /* eslint-enable no-console, angular/log */
+        }
 
         process.kill(-npmStart.pid, 'SIGKILL');
         process.exit();
