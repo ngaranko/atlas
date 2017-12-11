74ffe0366f34830458a7684132fa5ec1e40b0e7f
RM <r.r.moelker+kraken@gmail.com>
2017-12-11 09:02:43 +0100

    Combine reducer used for new style reducers.

--- modules/atlas/services/redux/reducer.factory.js
+++ modules/atlas/services/redux/reducer.factory.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 (function () {
     'use strict';
 
@@ -6,163 +8,35 @@
         .factory('reducer', reducerFactory);
 
     reducerFactory.$inject = [
-        '$rootScope',
-        '$timeout',
         '$window',
-        'urlReducers',
-        'freeze',
-        'homeReducers',
-        'layerSelectionReducers',
-        'mapReducers',
-        'pageReducers',
-        'searchReducers',
-        'straatbeeldReducers',
-        'dataSelectionReducers',
-        'printReducers',
-        'embedReducers',
-        'filtersReducers',
-        'environment'
+        'deprecatedReducer'
     ];
 
     // eslint-disable-next-line max-params
-    function reducerFactory ($rootScope,
-                             $timeout,
-                             $window,
-                             urlReducers,
-                             freeze,
-                             homeReducers,
-                             layerSelectionReducers,
-                             mapReducers,
-                             pageReducers,
-                             searchReducers,
-                             straatbeeldReducers,
-                             dataSelectionReducers,
-                             printReducers,
-                             embedReducers,
-                             filtersReducers,
-                             environment) {
+    function reducerFactory (
+             $window,
+             deprecatedReducer
+        ) {
         return function (oldState, action) { // eslint-disable-line complexity
-            const DetailsReducers = $window.reducers.detailReducer;
+            // Run state changes based on old reducers
+            const deprecatedState = deprecatedReducer(oldState, action);
+
+            // Use combine reducer for new reducers
             const UserReducer = $window.reducers.UserReducer;
-            const MapLayersReducer = $window.reducers.MapLayersReducer;
-            const MapPanelReducer = $window.reducers.MapPanelReducer;
-            const MapOverlaysReducer = $window.reducers.MapOverlaysReducer;
-            const MapBaseLayersReducer = $window.reducers.MapBaseLayersReducer;
-            const UiReducer = $window.reducers.UiReducer;
-
-            // TODO: Redux: replace
-            // Warning: angular.merge is deprecated
-            // -- https://docs.angularjs.org/api/ng/function/angular.merge
-
-            const detailReducers = {
-                FETCH_DETAIL: DetailsReducers,
-                SHOW_DETAIL: DetailsReducers,
-                DETAIL_FULLSCREEN: DetailsReducers
+            const newRootReducer = combineReducers({
+                user: UserReducer
+            });
+            const filteredState = {
+                user: oldState.user
             };
 
-            const userReducer = {
-                AUTHENTICATE_USER: UserReducer,
-                AUTHENTICATE_ERROR: UserReducer
+            // Combine old and new reducer states
+            const newState = {
+                ...deprecatedState,
+                ...newRootReducer(filteredState, action)
             };
 
-            const mapLayersReducer = {
-                FETCH_MAP_LAYERS_FAILURE: MapLayersReducer,
-                FETCH_MAP_LAYERS_REQUEST: MapLayersReducer,
-                FETCH_MAP_LAYERS_SUCCESS: MapLayersReducer
-            };
-
-            const mapBaseLayersReducer = {
-                FETCH_MAP_BASE_LAYERS_FAILURE: MapBaseLayersReducer,
-                FETCH_MAP_BASE_LAYERS_REQUEST: MapBaseLayersReducer,
-                FETCH_MAP_BASE_LAYERS_SUCCESS: MapBaseLayersReducer,
-                SET_MAP_BASE_LAYER: MapBaseLayersReducer
-            };
-
-            const mapOverlaysReducer = {
-                TOGGLE_MAP_OVERLAY: MapOverlaysReducer,
-                TOGGLE_MAP_OVERLAYS: MapOverlaysReducer,
-                TOGGLE_MAP_OVERLAY_VISIBILITY: MapOverlaysReducer
-            };
-
-            const mapPanelReducers = {
-                HIDE_MAP_PANEL: MapOverlaysReducer,
-                SHOW_MAP_PANEL: MapOverlaysReducer,
-                TOGGLE_MAP_PANEL: MapOverlaysReducer
-            };
-
-            const uiReducers = {
-                TOGGLE_MAP_PANEL_HANDLE: UiReducer
-            };
-
-            var actions = angular.merge(
-                urlReducers,
-                detailReducers,
-                mapPanelReducers,
-                mapOverlaysReducer,
-                mapBaseLayersReducer,
-                mapLayersReducer,
-                homeReducers,
-                userReducer,
-                layerSelectionReducers,
-                mapReducers,
-                pageReducers,
-                searchReducers,
-                straatbeeldReducers,
-                dataSelectionReducers,
-                printReducers,
-                embedReducers,
-                filtersReducers,
-                environment
-            );
-
-            if (detailReducers.hasOwnProperty(action.type.id)) {
-                action.payload = {
-                    payload: action.payload,
-                    type: action.type.id
-                };
-            }
-
-            if (userReducer.hasOwnProperty(action.type)) {
-                return UserReducer(oldState, action);
-            }
-
-            if (mapLayersReducer.hasOwnProperty(action.type)) {
-                return MapLayersReducer(oldState, action);
-            }
-
-            if (mapBaseLayersReducer.hasOwnProperty(action.type)) {
-                const newState = MapBaseLayersReducer(oldState, action);
-                $timeout(() => $rootScope.$digest());
-                return newState;
-            }
-
-            if (mapPanelReducers.hasOwnProperty(action.type)) {
-                return MapPanelReducer(oldState, action);
-            }
-
-            if (mapOverlaysReducer.hasOwnProperty(action.type)) {
-                const newState = MapOverlaysReducer(oldState, action);
-                $timeout(() => $rootScope.$digest());
-                return newState;
-            }
-
-            /* istanbul ignore if */
-            if (uiReducers.hasOwnProperty(action.type)) {
-                return UiReducer(oldState, action);
-            }
-
-            if (angular.isObject(action) &&
-                angular.isObject(action.type) &&
-                angular.isFunction(actions[action.type.id])) {
-                const result = actions[action.type.id](oldState, action.payload);
-                if (environment.isDevelopment()) {
-                    freeze.deepFreeze(result);
-                }
-                return result;
-            } else {
-                // TODO: Redux: throw error
-                return oldState;
-            }
+            return newState;
         };
     }
 })();
