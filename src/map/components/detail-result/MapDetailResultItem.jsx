e51a3fb0b8f46bb3738e92eadbe12d498c799dc7
Boris Arkenaar <boris@arkenaar.com>
2017-12-13 18:06:07 +0100

    Don't show a detail attribute when the value is empty

--- src/map/components/detail-result/MapDetailResultItem.jsx
+++ src/map/components/detail-result/MapDetailResultItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MapDetailResultItem = ({ label, value }) => (
+const MapDetailResultItem = ({ label, value }) => value && (
   <div className="map-detail-result__item">
     <section className="map-detail-result__item-content">
       <div className="map-detail-result__item-label">{label}</div>
