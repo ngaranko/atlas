c2f6568d1f6f85c2ea66f5ba6523a9bd6ba75d90
Boris Arkenaar <boris@arkenaar.com>
2017-12-27 17:52:15 +0100

    DP-5033 Unit testing 'gebieden' services

---
+++ src/shared/services/gebieden/gebieden-buurt.spec.js
@@ -0,0 +1,53 @@
+import fetchByUri from './gebieden-buurt';
+import getCenter from '../geo-json/geo-json';
+import { rdToWgs84 } from '../coordinate-reference-system/crs-converter';
+
+jest.mock('../geo-json/geo-json');
+jest.mock('../coordinate-reference-system/crs-converter');
+
+describe('The gebieden buurt resource', () => {
+  afterEach(() => {
+    fetch.mockReset();
+  });
+
+  describe('By uri', () => {
+    it('fetches a buurt', () => {
+      const uri = 'https://acc.api.data.amsterdam.nl/gebieden/buurt/123456';
+
+      fetch.mockResponseOnce(JSON.stringify({
+        geometrie: { type: 'Point' },
+        naam: 'Buurt display name 1',
+        something: 'abc123'
+      }));
+      getCenter.mockImplementation(() => ({ x: 1, y: 2 }));
+      rdToWgs84.mockImplementation(() => ({ latitude: 3, longitude: 4 }));
+
+      fetchByUri(uri).then((response) => {
+        expect(response).toEqual({
+          geometrie: { type: 'Point' },
+          label: 'Buurt display name 1',
+          location: { latitude: 3, longitude: 4 },
+          naam: 'Buurt display name 1',
+          something: 'abc123'
+        });
+      });
+
+      expect(fetch.mock.calls[0][0]).toBe(uri);
+    });
+
+    it('fetches with empty result object', () => {
+      const uri = 'https://acc.api.data.amsterdam.nl/gebieden/buurt/123456';
+
+      fetch.mockResponseOnce(JSON.stringify({}));
+
+      fetchByUri(uri).then((response) => {
+        expect(response).toEqual({
+          label: undefined,
+          location: null
+        });
+      });
+
+      expect(fetch.mock.calls[0][0]).toBe(uri);
+    });
+  });
+});
---
+++ src/shared/services/gebieden/gebieden-grootstedelijk.spec.js
@@ -0,0 +1,53 @@
+import fetchByUri from './gebieden-grootstedelijk';
+import getCenter from '../geo-json/geo-json';
+import { rdToWgs84 } from '../coordinate-reference-system/crs-converter';
+
+jest.mock('../geo-json/geo-json');
+jest.mock('../coordinate-reference-system/crs-converter');
+
+describe('The gebieden grootstedelijk resource', () => {
+  afterEach(() => {
+    fetch.mockReset();
+  });
+
+  describe('By uri', () => {
+    it('fetches a grootstedelijk', () => {
+      const uri = 'https://acc.api.data.amsterdam.nl/gebieden/grootstedelijk/123456';
+
+      fetch.mockResponseOnce(JSON.stringify({
+        _display: 'Grootstedelijk display name 1',
+        geometrie: { type: 'Point' },
+        something: 'abc123'
+      }));
+      getCenter.mockImplementation(() => ({ x: 1, y: 2 }));
+      rdToWgs84.mockImplementation(() => ({ latitude: 3, longitude: 4 }));
+
+      fetchByUri(uri).then((response) => {
+        expect(response).toEqual({
+          _display: 'Grootstedelijk display name 1',
+          geometrie: { type: 'Point' },
+          label: 'Grootstedelijk display name 1',
+          location: { latitude: 3, longitude: 4 },
+          something: 'abc123'
+        });
+      });
+
+      expect(fetch.mock.calls[0][0]).toBe(uri);
+    });
+
+    it('fetches with empty result object', () => {
+      const uri = 'https://acc.api.data.amsterdam.nl/gebieden/grootstedelijk/123456';
+
+      fetch.mockResponseOnce(JSON.stringify({}));
+
+      fetchByUri(uri).then((response) => {
+        expect(response).toEqual({
+          label: undefined,
+          location: null
+        });
+      });
+
+      expect(fetch.mock.calls[0][0]).toBe(uri);
+    });
+  });
+});
