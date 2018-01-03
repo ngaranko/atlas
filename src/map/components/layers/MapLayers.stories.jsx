a14698b804d67b1d1deb7722ac679c9a4992e717
Robbin Habermehl <robbin@habermehl.net>
2018-01-03 14:18:08 +0100

    Create Storybook stories for map components

---
+++ src/map/components/layers/MapLayers.stories.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
+import { action } from '@storybook/addon-actions'; // eslint-disable-line import/no-extraneous-dependencies
+
+import MapLayers from './MapLayers';
+import layers from '../../services/map-layers';
+
+storiesOf('Map/Layers', module)
+  .add('authenticated', () => (
+    <MapLayers
+      activeMapLayers={[]}
+      layers={layers.slice(0, 20)}
+      onLayerToggle={action('toggled')}
+      user={{ authenticated: true, scopes: ['HR/R'] }}
+    />
+  ))
+  .add('unauthenticated', () => (
+    <MapLayers
+      activeMapLayers={[]}
+      layers={layers.slice(0, 20)}
+      onLayerToggle={action('toggled')}
+      user={{ authenticated: false, scopes: [] }}
+    />
+  ))
+  .add('active layer', () => (
+    <MapLayers
+      activeMapLayers={[{ title: 'Kadastrale perceelsgrenzen' }]}
+      layers={layers.slice(0, 20)}
+      onLayerToggle={action('toggled')}
+      user={{ authenticated: false, scopes: [] }}
+    />
+  ));
