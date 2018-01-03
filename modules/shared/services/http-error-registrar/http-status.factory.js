d9c1e98b3bd744f4c8432a8aa4f313c5d2bd1b39
RM <r.r.moelker+kraken@gmail.com>
2018-01-03 13:23:31 +0100

    http status factory simply passes error to redux

--- modules/shared/services/http-error-registrar/http-status.factory.js
+++ modules/shared/services/http-error-registrar/http-status.factory.js
@@ -1,5 +1,4 @@
 import {
-    ERROR_TYPES,
     setGlobalError
 } from '../../../../src/shared/ducks/error-message.js';
 
@@ -15,54 +14,12 @@ import {
     ];
 
     function httpStatusFactory ($window) {
-        // The sole reponsability is to register if there have been any http errors
-        const errorTypes = ['SERVER_ERROR', 'NOT_FOUND_ERROR', 'LOGIN_ERROR'];
-        const exportObject = exportErrorTypes({
-            getStatus,
+        return {
             registerError
-        });
-
-        const currentStatus = {
-            hasErrors: false
         };
 
-        resetTypeFlags();
-
-        return exportObject;
-
-        function getStatus () {
-            // console.log('currentStatus: ', currentStatus);
-            return currentStatus;
-        }
-
         function registerError (errorType) {
-            console.log('errorType: ', errorType);
-            // Make sure the key is valid. Default to SERVER_ERROR
-            const key = errorTypes.filter(type => type === errorType)[0] || errorTypes[0];
-
-            $window.reduxStore.dispatch(setGlobalError(key));
-
-            // if (!currentStatus.hasErrors) {
-            //     resetTypeFlags();
-            // }
-
-            // currentStatus[key] = true;
-            // currentStatus.hasErrors = true;
-        }
-
-        function resetTypeFlags () {
-            errorTypes.forEach(key => {
-                currentStatus[key] = false;
-            });
-        }
-
-        // TODO replace with export
-        function exportErrorTypes (object) {
-            errorTypes.forEach(key => {
-                object[key] = key;
-            });
-
-            return object;
+            $window.reduxStore.dispatch(setGlobalError(errorType));
         }
     }
 })();
--- modules/shared/services/http-error-registrar/http-status.factory.test.js
+++ modules/shared/services/http-error-registrar/http-status.factory.test.js
@@ -1,66 +1,29 @@
+import {
+    setGlobalError
+} from '../../../../src/shared/ducks/error-message.js';
+
 describe('The http-status component', function () {
-    let httpStatus;
+    let httpStatus,
+        $window;
 
     beforeEach(function () {
         angular.mock.module('dpShared');
 
-        angular.mock.inject(function (_httpStatus_) {
+        angular.mock.inject(function (_$window_, _httpStatus_) {
+            $window = _$window_;
             httpStatus = _httpStatus_;
         });
-    });
 
-    it('is able to register any http errors', function () {
-        httpStatus.registerError();
-        expect(httpStatus.getStatus().hasErrors).toBe(true);
-    });
-
-    it('defaults to error type SERVER when no error type is given', function () {
-        httpStatus.registerError();
-        expect(httpStatus.getStatus()[httpStatus.SERVER_ERROR]).toBe(true);
-        expect(httpStatus.getStatus()[httpStatus.NOT_FOUND_ERROR]).toBe(false);
+        $window.reduxStore = {
+            dispatch: jasmine.createSpy('dispatch')
+        };
     });
 
     it('sets the error type given', function () {
-        httpStatus.registerError(httpStatus.NOT_FOUND_ERROR);
-        expect(httpStatus.getStatus()[httpStatus.SERVER_ERROR]).toBe(false);
-        expect(httpStatus.getStatus()[httpStatus.NOT_FOUND_ERROR]).toBe(true);
-    });
-
-    it('silently defaults to error type SERVER when an erroneous error type is given', function () {
-        httpStatus.registerError('FAULTY_ERROR_TYPE');
-        expect(httpStatus.getStatus()[httpStatus.SERVER_ERROR]).toBe(true);
-        expect(httpStatus.getStatus()[httpStatus.NOT_FOUND_ERROR]).toBe(false);
-    });
-
-    it('is able to register multiple http errors', function () {
-        [1, 2, 3, 4, 5].forEach(() => httpStatus.registerError());
-        expect(httpStatus.getStatus().hasErrors).toBe(true);
-    });
-
-    it('is able to register multiple http errors of different types', function () {
-        [
-            httpStatus.SERVER_ERROR,
-            httpStatus.LOGIN_ERROR,
-            httpStatus.NOT_FOUND_ERROR,
-            httpStatus.SERVER_ERROR
-        ].forEach(
-            type => httpStatus.registerError(type));
-
-        expect(httpStatus.getStatus()[httpStatus.SERVER_ERROR]).toBe(true);
-        expect(httpStatus.getStatus()[httpStatus.NOT_FOUND_ERROR]).toBe(true);
-        expect(httpStatus.getStatus()[httpStatus.LOGIN_ERROR]).toBe(true);
-    });
-
-    it('shows nothing when nothing has happened', function () {
-        expect(httpStatus.getStatus().hasErrors).toBe(false);
-    });
-
-    it('resets the error flags when hasErrors has been set to false', function () {
-        httpStatus.registerError(httpStatus.NOT_FOUND_ERROR);
-        httpStatus.getStatus().hasErrors = false;
-        httpStatus.registerError(httpStatus.SERVER_ERROR);
-
-        expect(httpStatus.getStatus()[httpStatus.SERVER_ERROR]).toBe(true);
-        expect(httpStatus.getStatus()[httpStatus.NOT_FOUND_ERROR]).toBe(false);
+        const errorType = 'foo';
+        httpStatus.registerError(errorType);
+        expect($window.reduxStore.dispatch).toHaveBeenCalledWith(
+            setGlobalError(errorType)
+        );
     });
 });
