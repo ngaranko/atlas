ad70af2b2c1383458312e7c2d098fb430c4c421d
RM <r.r.moelker+kraken@gmail.com>
2018-01-09 11:03:16 +0100

    React test syntax tweaked

--- src/shared/components/select-button/SelectButton.test.jsx
+++ src/shared/components/select-button/SelectButton.test.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 import SelectButton from './SelectButton';
 import Icon from '../../../../public/images/icon-aerial.svg';
 
-let options;
-
-beforeEach(() => {
-  options = [
-    {
-      value: 'topografie',
-      label: 'Topografie'
-    }, {
-      value: 'topo_rd_light',
-      label: 'Topografie licht'
-    }, {
-      value: 'topo_rd_zw',
-      label: 'Topografie grijs'
-    }
-  ];
-});
+const options = [
+  {
+    value: 'topografie',
+    label: 'Topografie'
+  }, {
+    value: 'topo_rd_light',
+    label: 'Topografie licht'
+  }, {
+    value: 'topo_rd_zw',
+    label: 'Topografie grijs'
+  }
+];
 
 describe('SelectButton', () => {
   it('should trigger callback when clicked', () => {
@@ -40,9 +36,24 @@ describe('SelectButton', () => {
     expect(handleChange).toHaveBeenLastCalledWith('topografie');
   });
 
-  it('should open the options list when open icon is clicked and trigger callback when an option is clicked', () => {
-    const handleChange = jest.fn();
+  it('should open the options list when open icon is clicked', () => {
+    const wrapper = mount(
+      <SelectButton
+        className="someclass"
+        icon={Icon}
+        isDisabled={false}
+        name="topography"
+        options={options}
+      />
+    );
 
+    expect(wrapper.find('.select-button.select-button--expanded').exists()).toBe(false);
+    wrapper.find('button.select-button__icon-wrapper').simulate('click');
+    expect(wrapper.find('.select-button.select-button--expanded').exists()).toBe(true);
+  });
+
+  it('should trigger callback and close when an option is clicked', () => {
+    const handleChange = jest.fn();
     const wrapper = mount(
       <SelectButton
         className="someclass"
@@ -53,14 +64,10 @@ describe('SelectButton', () => {
         options={options}
       />
     );
-
-    expect(wrapper.find('.select-button.select-button--expanded').exists()).toBe(false);
-
     wrapper.find('button.select-button__icon-wrapper').simulate('click');
-    expect(handleChange).toHaveBeenCalledTimes(0);
-    expect(wrapper.find('.select-button.select-button--expanded').exists()).toBe(true);
 
     wrapper.find('button.select-button__drop-down-button').last().simulate('click');
+
     expect(handleChange).toHaveBeenCalledWith('topo_rd_zw');
     expect(wrapper.find('.select-button.select-button--expanded').exists()).toBe(false);
   });
@@ -75,9 +82,6 @@ describe('SelectButton', () => {
         options={options}
       />
     );
-
-    expect(wrapper.find('.select-button.select-button--expanded').exists()).toBe(false);
-
     wrapper.find('button.select-button__icon-wrapper').simulate('click');
     expect(wrapper.find('.select-button.select-button--expanded').exists()).toBe(true);
 
@@ -87,17 +91,13 @@ describe('SelectButton', () => {
 
     expect(wrapper.find('.select-button.select-button--expanded').exists()).toBe(false);
   });
-
-  it('should open the options list when open icon is clicked and close again when select button is clicked', () => {
-    // todo add test
-  });
 });
 
 describe('rendering', () => {
   it('should render with default value', () => {
     const isDisabled = false;
 
-    const wrapper = mount(
+    const wrapper = shallow(
       <SelectButton
         className="someclass"
         icon={Icon}
@@ -110,7 +110,7 @@ describe('rendering', () => {
   });
 
   it('should render with preselected value then make the select button disabled', () => {
-    const wrapper = mount(
+    const wrapper = shallow(
       <SelectButton
         className="someclass"
         icon={Icon}
@@ -120,19 +120,14 @@ describe('rendering', () => {
         value="topo_rd_light"
       />
     );
-    expect(wrapper).toMatchSnapshot();
-    expect(wrapper.find('.select-button').exists()).toBe(true);
-    expect(wrapper.find('.select-button--disabled').exists()).toBe(false);
+    expect(wrapper.find('.select-button.select-button--disabled').exists()).toBe(false);
 
     wrapper.setProps({ isDisabled: true });
     expect(wrapper.find('.select-button.select-button--disabled').exists()).toBe(true);
-
-    wrapper.setProps({ isDisabled: false });
-    expect(wrapper.find('.select-button.select-button--disabled').exists()).toBe(false);
   });
 
   it('should render with no options', () => {
-    const wrapper = mount(
+    const wrapper = shallow(
       <SelectButton
         icon={Icon}
         name="topography"
