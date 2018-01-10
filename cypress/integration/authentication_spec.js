1db4b1d75f672d1ce13987127fa8f0576d40085f
Aron Martin <mail@aronmartin.com>
2018-01-10 11:35:58 +0100

    Add e2e test for authentication, Remove homepage e2e

---
+++ cypress/integration/authentication_spec.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import { login, logout } from '../services/authentication';
+
+describe('auth module', () => {
+
+  describe('authentication', () => {
+    it('should be verified', () => {
+      login();
+      cy.get('.qa-menu__title').should('contain', 'atlas.emp...');
+      logout();
+    });
+  });
+});
