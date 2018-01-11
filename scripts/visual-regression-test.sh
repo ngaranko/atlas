a21fbaec9a621703c8043eeca6d6893f04bafa6a
Robbin Habermehl <robbin@habermehl.net>
2018-01-11 13:24:22 +0100

    Run visual regression tests just in Docker locally

--- scripts/visual-regression-test.sh
+++ scripts/visual-regression-test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-docker run \
-  -d \
-  -v $(pwd):/app \
-  -w="/app" \
-  --rm \
-  --shm-size 1G \
-  alekzonder/puppeteer:0.13.0 \
-  npm i babel-cli && \
-  babel-node ./test/visual-regression.js
+
+if [ $NODE_ENV == 'development' ]
+then
+  docker run \
+    -d \
+    -v $(pwd):/app \
+    -w="/app" \
+    --rm \
+    --shm-size 1G \
+    alekzonder/puppeteer:0.13.0 \
+    npm i babel-cli && \
+    babel-node ./test/visual-regression.js
+else
+    babel-node $(pwd)/test/visual-regression.js
+fi
