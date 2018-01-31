668b3c03dd73b469cd6d2dab88ab5d9d5d93e03d
RM <RRMoelker@users.noreply.github.com>
2018-01-31 13:36:38 +0100

    Streetview action creator reducer linked through comment

--- src/map/ducks/streetview/streetview.js
+++ src/map/ducks/streetview/streetview.js
@@ -1,5 +1,6 @@
 export const FETCH_STRAATBEELD_BY_ID = 'FETCH_STRAATBEELD_BY_ID';
 
+// related reducer /modules/atlas/services/redux/reducers/straatbeeld-reducers.factory.js
 export const fetchStraatbeeldById = (id) => ({
   type: { id: FETCH_STRAATBEELD_BY_ID },
   payload: { id }
