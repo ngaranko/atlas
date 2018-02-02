3265382b65b4722d0c86c5ac1fae4213941e1bbe
RM <RRMoelker@users.noreply.github.com>
2018-02-02 11:37:52 +0100

    DrawTool presentational component

--- src/map/components/draw-tool/DrawTool.jsx
+++ src/map/components/draw-tool/DrawTool.jsx
@@ -1,194 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import isEqual from 'lodash.isequal';
-
 import './_draw-tool.scss';
 import drawToolConfig from '../../services/draw-tool/draw-tool-config';
 import ToggleDrawing from '../toggle-drawing/ToggleDrawing';
 import ShapeSummary from '../shape-summary/ShapeSummary';
 import PointsAvailable from '../points-available/PointsAvailable';
 
-class DrawTool extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      drawingMode: props.drawingMode,
-      previousMarkers: []
-    };
-
-    this.onFinishShape = this.onFinishShape.bind(this);
-    this.onDrawingMode = this.onDrawingMode.bind(this);
-    this.onUpdateShape = this.onUpdateShape.bind(this);
-    this.setPolygon = this.setPolygon.bind(this);
-    this.getMarkers = this.getMarkers.bind(this);
-
-    this.props.initialize(
-      window.leafletMap,
-      this.onFinishShape,
-      this.onDrawingMode,
-      this.onUpdateShape
-    );
-
-    this.setPolygon();
-  }
-
-  componentWillReceiveProps(props) {
-    const markers = this.getMarkers();
-
-    if (!isEqual(this.state.previousMarkers, markers)) {
-      // if the markers have changed save the new markers as previous markers
-      this.setPolygon();
-      this.setState({ previousMarkers: [...markers] });
-    }
-
-    if (props.dataSelection === null && props.geometry === null &&
-      props.drawingMode === drawToolConfig.DRAWING_MODE.NONE) {
-      // if dataSelection and geometry are empty then remove the drawn polygon
-      this.props.setPolygon([]);
-    }
-
-    if (this.state.drawingMode !== props.drawingMode) {
-      if (props.drawingMode === drawToolConfig.DRAWING_MODE.NONE) {
-        // after drawing mode has changed the draw tool should be cancelled after navigating
-        this.props.cancel();
-      }
-      this.setState({ drawingMode: props.drawingMode });
-    }
-  }
-
-  onFinishShape(polygon) {
-    const has2Markers = polygon && polygon.markers && polygon.markers.length === 2;
-    const moreThan2Markers = polygon && polygon.markers && polygon.markers.length > 2;
-
-    if (moreThan2Markers && !isEqual(this.state.previousMarkers, polygon.markers)) {
-      this.props.setGeometryFilter({
-        markers: polygon.markers,
-        description: `${polygon.distanceTxt} en ${polygon.areaTxt}`
-      });
-
-      this.props.onEndDrawing({ polygon });
-      this.props.onSetPageName({ name: null });
-
-      this.props.onSetMapFullscreen({ isMapFullscreen: false });
-    } else if (has2Markers) {
-      this.props.onEndDrawing({ polygon });
-    }
-  }
-
-  onDrawingMode(drawingMode) {
-    if (drawingMode !== drawToolConfig.DRAWING_MODE.NONE) {
-      this.setState({ previousMarkers: [...this.props.currentShape.markers] });
-      this.props.resetGeometryFilter({ drawingMode });
-      this.props.onStartDrawing({ drawingMode });
-    } else {
-      this.props.onEndDrawing();
-    }
-  }
-
-  onUpdateShape(newShape) {
-    this.props.onMapUpdateShape({
-      shapeMarkers: newShape.markers.length,
-      shapeDistanceTxt: newShape.distanceTxt,
-      shapeAreaTxt: newShape.areaTxt
-    });
-  }
-
-  setPolygon() {
-    if (!this.props.isEnabled()) {
-      this.props.setPolygon(this.getMarkers());
-    }
-  }
-
-  getMarkers() {
-    return this.props.geometry && this.props.geometry.length > 0 ? this.props.geometry :
-      ((this.props.dataSelection && this.props.dataSelection.geometryFilter &&
-      this.props.dataSelection.geometryFilter.markers) || []);
-  }
-
-  render() {
-    const markersLeft = drawToolConfig.MAX_MARKERS - this.props.shapeMarkers;
-    return (
-      <section className="draw-tool">
-        <ToggleDrawing
-          drawingMode={this.state.drawingMode}
-          shapeMarkers={this.props.shapeMarkers}
-          toggleDrawing={this.props.toggleDrawing}
+const DrawTool = ({
+  drawingMode,
+  isEnabled,
+  onClearDrawing, // TODO: rename
+  shapeDistanceTxt,
+  shapeMarkers,
+  toggleDrawing
+}) => {
+  const markersLeft = drawToolConfig.MAX_MARKERS - shapeMarkers;
+  return (
+    <section className="draw-tool">
+      <ToggleDrawing
+        drawingMode={drawingMode}
+        shapeMarkers={shapeMarkers}
+        toggleDrawing={toggleDrawing}
+      />
+      {
+        !isEnabled() && shapeMarkers === 2
+        &&
+        <ShapeSummary
+          shapeDistanceTxt={shapeDistanceTxt}
+          onClearDrawing={onClearDrawing}
         />
-        {
-          !this.props.isEnabled() && this.props.shapeMarkers === 2
-          &&
-          <ShapeSummary
-            shapeDistanceTxt={this.props.shapeDistanceTxt}
-            onClearDrawing={this.props.onClearDrawing}
-          />
-        }
-        {
-          markersLeft <= drawToolConfig.MARKERS_LEFT_WARNING &&
-          this.state.drawingMode !== drawToolConfig.DRAWING_MODE.NONE &&
-          <PointsAvailable
-            markersLeft={markersLeft}
-            drawingMode={this.state.drawingMode}
-          />
-        }
-      </section>
-    );
-  }
-}
-
-DrawTool.defaultProps = {
-  dataSelection: null,
-  geometry: null
+      }
+      {
+        markersLeft <= drawToolConfig.MARKERS_LEFT_WARNING &&
+        drawingMode !== drawToolConfig.DRAWING_MODE.NONE &&
+        <PointsAvailable
+          markersLeft={markersLeft}
+          drawingMode={drawingMode}
+        />
+      }
+    </section>
+  );
 };
 
 DrawTool.propTypes = {
-  // TODO: reactivate required props
-  // drawingMode: PropTypes.string.isRequired,
-  // shapeMarkers: PropTypes.number.isRequired,
-  // shapeDistanceTxt: PropTypes.string.isRequired,
-  // dataSelection: PropTypes.object,
-  // geometry: PropTypes.array,
-
-  // currentShape: PropTypes.object,
-
-  // toggleDrawing: PropTypes.func.isRequired,
-  // isEnabled: PropTypes.func.isRequired,
-  // cancel: PropTypes.func.isRequired,
-  // setPolygon: PropTypes.func.isRequired,
-  // initialize: PropTypes.func.isRequired,
-
-  // onClearDrawing: PropTypes.func.isRequired,
-  // onMapUpdateShape: PropTypes.func.isRequired,
-  // setGeometryFilter: PropTypes.func.isRequired,
-  // resetGeometryFilter: PropTypes.func.isRequired,
-  // onStartDrawing: PropTypes.func.isRequired,
-  // onEndDrawing: PropTypes.func.isRequired,
-  // onSetPageName: PropTypes.func.isRequired,
-  // onSetMapFullscreen: PropTypes.func.isRequired
-
-  drawingMode: PropTypes.string,
-  shapeMarkers: PropTypes.number,
-  shapeDistanceTxt: PropTypes.string,
-  dataSelection: PropTypes.object,
-  geometry: PropTypes.array,
-
-  currentShape: PropTypes.object,
-
-  toggleDrawing: PropTypes.func,
-  isEnabled: PropTypes.func,
-  cancel: PropTypes.func,
-  setPolygon: PropTypes.func,
-  initialize: PropTypes.func,
-
-  onClearDrawing: PropTypes.func,
-  onMapUpdateShape: PropTypes.func,
-  setGeometryFilter: PropTypes.func,
-  resetGeometryFilter: PropTypes.func,
-  onStartDrawing: PropTypes.func,
-  onEndDrawing: PropTypes.func,
-  onSetPageName: PropTypes.func,
-  onSetMapFullscreen: PropTypes.func
+  drawingMode: PropTypes.string.isRequired,
+  isEnabled: PropTypes.func.isRequired,
+  onClearDrawing: PropTypes.func.isRequired,
+  shapeDistanceTxt: PropTypes.string.isRequired,
+  shapeMarkers: PropTypes.number.isRequired,
+  toggleDrawing: PropTypes.func.isRequired
 };
 
 export default DrawTool;
--- src/map/components/draw-tool/DrawTool.test.jsx
+++ src/map/components/draw-tool/DrawTool.test.jsx
@@ -5,283 +5,45 @@ import DrawTool from './DrawTool';
 import drawToolConfig from '../../services/draw-tool/draw-tool-config';
 
 describe('DrawTool', () => {
-  let initialize;
-  let setPolygon;
   let isEnabled;
-  let cancel;
-  let onMapUpdateShape;
-  let onEndDrawing;
   let defaultProps;
-  let setGeometryFilter;
-  let onSetPageName;
-  let onSetMapFullscreen;
 
   beforeEach(() => {
-    initialize = jest.fn();
-    setPolygon = jest.fn();
     isEnabled = jest.fn();
-    cancel = jest.fn();
-    onEndDrawing = jest.fn();
-    onMapUpdateShape = jest.fn();
-    setGeometryFilter = jest.fn();
-    onSetPageName = jest.fn();
-    onSetMapFullscreen = jest.fn();
     defaultProps = {
-      initialize,
-      setPolygon,
       isEnabled,
-      cancel,
-      onMapUpdateShape,
-      onEndDrawing,
-      setGeometryFilter,
-      onSetPageName,
-      onSetMapFullscreen,
-      onClearDrawing: jest.fn(),
       toggleDrawing: jest.fn(),
+      onClearDrawing: jest.fn(),
       drawingMode: drawToolConfig.DRAWING_MODE.NONE,
-      currentShape: {},
-
-      geometry: null,
       shapeMarkers: 0,
-      shapeDistanceTxt: '',
-      dataSelection: null
+      shapeDistanceTxt: ''
     };
   });
 
-  describe('actions', () => {
-    it('should be initialized first', () => {
-      const wrapper = shallow(<DrawTool {...defaultProps} />);
-
-      expect(initialize).toHaveBeenCalledTimes(1);
-      expect(setPolygon).toHaveBeenCalledWith([]);
-      expect(wrapper).toMatchSnapshot();
-    });
-
-    it('should turn drawing on and off when drawing mode has switched', () => {
-      const wrapper = shallow(<DrawTool {...defaultProps} />);
-
-      wrapper.setProps({ drawingMode: 'draw' });
-      expect(wrapper).toMatchSnapshot();
-      expect(wrapper.state('drawingMode')).toBe('draw');
-
-      wrapper.setProps({ drawingMode: 'none' });
-      expect(wrapper).toMatchSnapshot();
-      expect(wrapper.state('drawingMode')).toBe('none');
-    });
-
-    it('should save previous markers and trigger cancel when line has been drawn', () => {
-      cancel.mockReset();
-
-      const wrapper = shallow(<DrawTool {...defaultProps} />);
-      expect(wrapper.state('previousMarkers')).toEqual([]);
-      expect(cancel).not.toHaveBeenCalled();
-
-      wrapper.setProps({ drawingMode: 'draw' });
-      wrapper.setProps({
-        geometry: [[52.3, 4.8], [52.4, 4.9]],
-        shapeMarkers: 2,
-        shapeDistanceTxt: '1.89 km'
-      });
-      wrapper.setProps({ drawingMode: 'none' });
-
-      expect(setPolygon).toHaveBeenCalledWith([[52.3, 4.8], [52.4, 4.9]]);
-      expect(wrapper.state('previousMarkers')).toEqual([[52.3, 4.8], [52.4, 4.9]]);
-      expect(wrapper).toMatchSnapshot();
-      expect(cancel).toHaveBeenCalledTimes(1);
-    });
-
-    it('should save previous markers and trigger cancel when polygon has been drawn', () => {
-      cancel.mockReset();
-
-      const wrapper = shallow(<DrawTool {...defaultProps} />);
-      expect(wrapper.state('previousMarkers')).toEqual([]);
-      expect(cancel).not.toHaveBeenCalled();
-
-      wrapper.setProps({ drawingMode: 'draw' });
-      wrapper.setProps({
-        dataSelection: {
-          geometryFilter: {
-            markers: [[52.3, 4.8], [52.4, 4.9], [53, 5]]
-          }
-        },
-        shapeMarkers: 3,
-        shapeDistanceTxt: '2.18 km'
-      });
-      wrapper.setProps({ drawingMode: 'none' });
-
-      expect(setPolygon).toHaveBeenCalledWith([[52.3, 4.8], [52.4, 4.9], [53, 5]]);
-      expect(wrapper.state('previousMarkers')).toEqual([[52.3, 4.8], [52.4, 4.9], [53, 5]]);
-      expect(wrapper).toMatchSnapshot();
-      expect(cancel).toHaveBeenCalledTimes(1);
-    });
-
-    it('setPolygon should only render polygon when drawing is not active', () => {
-      const wrapper = shallow(<DrawTool {...defaultProps} />);
-      const instance = wrapper.instance();
-      setPolygon.mockReset();
-
-      isEnabled.mockImplementation(() => true);
-      instance.setPolygon();
-      expect(setPolygon).not.toHaveBeenCalled();
-
-      isEnabled.mockImplementation(() => false);
-      instance.setPolygon();
-      expect(setPolygon).toHaveBeenCalled();
-    });
+  it('should render points available, shape summary and toggle drawing', () => {
+    const wrapper = shallow(<DrawTool {...defaultProps} />);
+    expect(wrapper).toMatchSnapshot();
   });
 
-  describe('callbacks', () => {
-    it('onFinishShape should dispatch action when a line has been drawn', () => {
-      const wrapper = shallow(<DrawTool {...defaultProps} />);
-      const instance = wrapper.instance();
-
-      expect(typeof instance.onFinishShape).toBe('function');
-
-      instance.onFinishShape({
-        markers: [1, 2],
-        distanceTxt: 'a',
-        areaTxt: 'b'
-      });
-
-      expect(onEndDrawing).toHaveBeenCalledWith({
-        polygon: {
-          areaTxt: 'b',
-          distanceTxt: 'a',
-          markers: [1, 2]
-        }
-      });
-    });
-
-    it('onFinishShape should dispatch actions when a polygon has been drawn and map is fullscreen', () => {
-      const wrapper = shallow((<DrawTool
+  it('should render summary for 2 points', () => {
+    isEnabled.mockImplementation(() => false);
+    const wrapper = shallow((
+      <DrawTool
         {...defaultProps}
-        isMapFullscreen
-      />));
-      const instance = wrapper.instance();
+        shapeMarkers={2}
+      />
+    ));
+    expect(wrapper).toMatchSnapshot();
+  });
 
-      instance.onFinishShape({
-        markers: [1, 2, 3, 4],
-        distanceTxt: 'a',
-        areaTxt: 'b'
-      });
-
-      //   expect(store.dispatch).toHaveBeenCalledWith({
-      //     payload: {
-      //       polygon: {
-      //         areaTxt: 'b',
-      //         distanceTxt: 'a',
-      //         markers: [1, 2, 3, 4]
-      //       }
-      //     },
-      //     type: 'MAP_END_DRAWING'
-      //   });
-      //   expect(store.dispatch).toHaveBeenCalledWith({
-      //     payload: {
-      //       name: null
-      //     },
-      //     type: 'SET_PAGE_NAME'
-      //   });
-      //   expect(store.dispatch).toHaveBeenCalledWith({
-      //     payload: {
-      //       description: 'a en b',
-      //       markers: [1, 2, 3, 4]
-      //     },
-      //     type: 'SET_DATA_SELECTION_GEOMETRY_FILTER'
-      //   });
-      //   expect(store.dispatch).toHaveBeenCalledWith({
-      //     payload: { isMapFullscreen: false },
-      //     type: 'SET_MAP_FULLSCREEN'
-      //   });
-
-      //   // trigger again with same markers should not dispatch any action
-      //   expect(store.dispatch.mock.calls.length).toBe(4);
-      //   wrapper.setState({
-      //     previousMarkers: [1, 2, 3, 4]
-      //   });
-      //   instance.onFinishShape({
-      //     markers: [1, 2, 3, 4]
-      //   });
-      //   expect(store.dispatch.mock.calls.length).toBe(4);
-      // });
-    });
-
-    // it('onDrawingMode should call prop functions', () => {
-    //   // const store = configureMockStore()({ ...initialState });
-
-    //   const wrapper = shallow(<DrawTool {...defaultProps} />);
-    //   const instance = wrapper.instance();
-
-    //   expect(typeof instance.onDrawingMode).toBe('function');
-
-    //   instance.onDrawingMode('draw');
-    //   // expect(store.dispatch).toHaveBeenCalledWith({
-    //   //   payload: {
-    //   //     drawingMode: 'draw'
-    //   //   },
-    //   //   type: 'MAP_START_DRAWING'
-    //   // });
-    //   // expect(store.dispatch).toHaveBeenCalledWith({
-    //   //   payload: {
-    //   //     drawingMode: 'draw'
-    //   //   },
-    //   //   type: 'RESET_DATA_SELECTION_GEOMETRY_FILTER'
-    //   // });
-
-    //   instance.onDrawingMode('none');
-    //   // expect(store.dispatch).toHaveBeenCalledWith({ type: 'MAP_END_DRAWING' });
-    // });
-
-    it('onUpdateShape should call function', () => {
-      // const store = configureMockStore()({ ...initialState });
-
-      const wrapper = shallow(<DrawTool {...defaultProps} />);
-      const instance = wrapper.instance();
-
-      expect(typeof instance.onUpdateShape).toBe('function');
-
-      instance.onUpdateShape({
-        markers: [1, 2, 3],
-        distanceTxt: 'a',
-        areaTxt: 'b'
-      });
-
-      expect(onMapUpdateShape).toHaveBeenCalledWith({
-        shapeAreaTxt: 'b',
-        shapeDistanceTxt: 'a',
-        shapeMarkers: 3
-      });
-    });
-
-  // it('should render points available, shape summary and toggle drawing', () => {
-  //   // const store = configureMockStore()({ ...initialState });
-  //   const wrapper = shallow(<DrawTool {...defaultProps} />);
-  //   expect(wrapper).toMatchSnapshot();
-  // });
-
-  // it('should render summary for 2 points', () => {
-  //   // const store = configureMockStore()({
-  //   //   ...initialState,
-  //   //   map: {
-  //   //     ...initialState.map,
-  //   //     shapeMarkers: 2
-  //   //   }
-  //   // });
-  //   isEnabled.mockImplementation(() => true);
-  //   const wrapper = shallow(<DrawTool {...defaultProps} />);
-  //   expect(wrapper).toMatchSnapshot();
-  // });
-
-  // it('should render points available almost max points drawn', () => {
-  //   // const store = configureMockStore()({
-  //   //   ...initialState,
-  //   //   map: {
-  //   //     ...initialState.map,
-  //   //     shapeMarkers: 10
-  //   //   }
-  //   // });
-  //   const wrapper = shallow(<DrawTool {...defaultProps} />);
-  //   wrapper.setState({ drawingMode: 'bar' });
-  //   expect(wrapper).toMatchSnapshot();
-  // });
+  it('should render points available almost max points drawn', () => {
+    const wrapper = shallow((
+      <DrawTool
+        {...defaultProps}
+        drawingMode={'bar'}
+        shapeMarkers={10}
+      />
+    ));
+    expect(wrapper).toMatchSnapshot();
   });
 });
