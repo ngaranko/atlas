51d6e2bc43d1ad0e81ac0549bd2c413a757c4b84
RM <RRMoelker@users.noreply.github.com>
2018-02-08 17:34:43 +0100

    Freeze service to vanilla JS

---
+++ src/shared/services/freeze/freeze.js
@@ -0,0 +1,24 @@
+//
+// Based on https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
+// When packager is available (WebPack), consider switching to https://github.com/buunguyen/redux-freeze
+// TODO: tg 4069
+//
+const deepFreeze = (obj) => {
+  // Retrieve the property names defined on obj
+  const propNames = Object.getOwnPropertyNames(obj);
+
+  // Freeze properties before freezing self
+  propNames.forEach((name) => {
+    const prop = obj[name];
+
+    // Freeze prop if it is an object
+    if (typeof prop === 'object' && prop !== null) {
+      deepFreeze(prop);
+    }
+  });
+
+  // Freeze self (no-op if already frozen)
+  return Object.freeze(obj);
+};
+
+export default deepFreeze;
