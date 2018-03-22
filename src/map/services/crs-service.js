108f11c667dfb35b20bc116981c004dffe075ca2
Aron Martin <mail@aronmartin.com>
2018-03-22 13:33:30 +0100

    Refactor crs service

--- src/map/services/crs-service.js
+++ src/map/services/crs-service.js
@@ -1,23 +1,23 @@
-import proj4 from 'proj4'; //eslint-disable-line 
+import L from 'leaflet';
+import proj4 from 'proj4'; //eslint-disable-line
 import proj4leaflet from 'proj4leaflet'; //eslint-disable-line
 
 import CRS_CONFIG from './crs-config.constant';
 
 function getCrs() {
   const rdSettings = CRS_CONFIG.RD;
-
   rdSettings.transformation.bounds =
-    window.L.bounds.apply(null, CRS_CONFIG.RD.transformation.bounds);
-  const test = new window.L.Proj.CRS(
+    L.bounds.apply(null, CRS_CONFIG.RD.transformation.bounds);
+  const crs = new L.Proj.CRS(
           rdSettings.code,
           rdSettings.projection,
           rdSettings.transformation
       );
 
-  test.distance = window.L.CRS.Earth.distance;
-  test.R = CRS_CONFIG.EARTH_RADIUS;
+  crs.distance = L.CRS.Earth.distance;
+  crs.R = CRS_CONFIG.EARTH_RADIUS;
 
-  return test;
+  return crs;
 }
 
 export default getCrs;
