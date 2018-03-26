7ce9f079951b7d3c46de9cbd8cd50d2cf0e9a762
Boris Arkenaar <boris@arkenaar.com>
2018-03-26 18:20:53 +0200

    DP-5420 Explicit requests for datasets-search tests

--- test/cypress/integration/datasets-search_spec.js
+++ test/cypress/integration/datasets-search_spec.js
@@ -1,5 +1,6 @@
-/* eslint-disable */
 import { login, logout } from '../services/authentication';
+import { defineSearchRoutes, waitForSearch } from '../services/routing';
+
 describe('datasets search module', () => {
   before(() => {
     login();
@@ -11,10 +12,15 @@ describe('datasets search module', () => {
 
   describe('user should be to type and see suggestions', () => {
     it('should open the autocomplete panel', () => {
+      cy.server();
+      cy.route('/typeahead?q=Park').as('getTypeAhead');
+
       cy.visit('/');
       cy.get('input.js-search-input').trigger('focus');
       cy.get('input.js-search-input').type('Park');
       cy.get('input.js-search-input').trigger('change');
+      cy.wait('@getTypeAhead');
+
       cy.get('.c-autocomplete').should('exist').and('be.visible');
     });
   });
@@ -22,24 +28,28 @@ describe('datasets search module', () => {
   describe('user should be able to search and see results', () => {
     it('should open the datasets results', () => {
       cy.server()
-      cy.route('https://acc.api.data.amsterdam.nl/catalogus/api/3/action/*').as('getResults')
+      defineSearchRoutes();
+
       cy.visit('/');
       cy.get('input.js-search-input').trigger('focus');
       cy.get('input.js-search-input').type('Park');
       cy.get('.c-search-form').submit();
-      cy.wait('@getResults');
+      waitForSearch();
+
       cy.get('.o-tabs__tab--link').contains('Datasets').click();
       cy.get('.c-data-selection-card').should('exist').and('be.visible');
     });
 
     it('should not open the datasets results because there are no results', () => {
       cy.server()
-      cy.route('https://acc.api.data.amsterdam.nl/catalogus/api/3/action/*').as('getResults')
+      defineSearchRoutes();
+
       cy.visit('/');
       cy.get('input.js-search-input').trigger('focus');
       cy.get('input.js-search-input').type('NORESULTS');
       cy.get('.c-search-form').submit();
-      cy.wait('@getResults');
+      waitForSearch();
+
       cy.get('.o-tabs__tab--link').should('not.exist').and('not.be.visible');
       cy.get('.c-data-selection-card').should('not.exist').and('not.be.visible');
     });
