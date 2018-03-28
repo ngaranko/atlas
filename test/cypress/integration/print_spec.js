c577e92bfae1dd7e6363e24495a83458c8d9a45e
Boris Arkenaar <boris@arkenaar.com>
2018-03-28 08:33:31 +0200

    DP-5420 Explicit requests for print tests

--- test/cypress/integration/print_spec.js
+++ test/cypress/integration/print_spec.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 describe('print module', () => {
   beforeEach(() => {
     // go to the homepage
@@ -6,11 +5,24 @@ describe('print module', () => {
   });
 
   it('should show a print version of the page when the user click on the print button', () => {
+    cy.server();
+    cy.route('/typeahead?q=10581111').as('getTypeAhead');
+    cy.route('/meetbouten/meetbout/*').as('getResults');
+    cy.route('/meetbouten/meting/?meetbout=*').as('getMeeting');
+    cy.route('/panorama/thumbnail/?*').as('getPanoThumbnail');
+
     cy.get('#global-search').type('10581111');
+
+    cy.wait('@getTypeAhead');
     cy.get('.c-autocomplete').contains('10581111').click();
-    cy.get('button.qa-menu__link').click();
+
+    cy.wait('@getResults');
+    cy.wait('@getMeeting');
+    cy.wait('@getPanoThumbnail');
     cy.get('img.c-straatbeeld-thumbnail--img').should('exist').and('be.visible');
     cy.get('h2.qa-title').should('exist').and('be.visible').contains('10581111');
+
+    cy.get('button.qa-menu__link').click();
     cy.get('a.c-menu__subitem').contains('Printen').click();
     cy.get('h1.c-print-header__title').should('exist').and('be.visible');
     cy.get('.c-print-header__close').click();
