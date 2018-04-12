995404e6a323c7c8a07d7ca55e5a241befc981bb
Vijay Jawalapersad <info@vjawala.nl>
2018-04-12 16:40:24 +0200

    remove delay

--- src/header/sagas/auto-suggest/auto-suggest.js
+++ src/header/sagas/auto-suggest/auto-suggest.js
@@ -1,11 +1,9 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
-import { delay } from 'redux-saga';
 
 import autoSuggestSearch from '../../services/auto-suggest/auto-suggest';
 
 function* fetchSuggestions(action) {
   try {
-    yield call(delay, 100);
     const suggestions = yield call(autoSuggestSearch, action.query);
     yield put({ type: 'FETCH_SUGGESTIONS_SUCCESS', suggestions });
   } catch (error) {
