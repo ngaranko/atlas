1b53808bf69684827d19b9a147ecc3499cb717be
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-05-04 12:17:33 +0200

    del: data formatter ckan

--- modules/detail/services/data-formatter/data-formatter.factory.js
+++ modules/detail/services/data-formatter/data-formatter.factory.js
@@ -12,38 +12,11 @@
 
         function formatData (data, subject, catalogFilters) {
             switch (subject) {
-                case 'api':
-                    return formatApiData(data);
                 case 'datasets': // dcat data
                     return formatCatalogData(data, catalogFilters);
                 default:
                     return data;
             }
-        }
-
-        function formatApiData (data) {
-            const formattedData = {
-                _display: data.result.title
-            };
-            return [
-                'title',
-                'publisher',
-                'publisher_email',
-                'contact_name',
-                'contact_email',
-                'metadata_created',
-                'metadata_modified',
-                'notes',
-                'resources',
-                'tags',
-                'license_id',
-                'license_title',
-                'license_url',
-                'groups'
-            ].reduce((result, key) => {
-                result[key] = data.result[key];
-                return result;
-            }, formattedData);
         }
 
         function formatCatalogData (data, catalogFilters) {
