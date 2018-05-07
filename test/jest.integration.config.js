ae2c594d90b7dd803cad4b7859037af427aaa9c4
Vijay Jawalapersad <info@vjawala.nl>
2018-05-07 22:59:12 +0200

    DP-5439 - move jest.integration.config file, as it does not work to reach this file with the cli command if it's located in the root

--- test/jest.integration.config.js
+++ test/jest.integration.config.js
@@ -5,6 +5,7 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/file-mock.js',
     '\\.(svg)$': '<rootDir>/test/file-svg-mock.js'
   },
+  rootDir: '../',
   setupFiles: [
     'raf/polyfill',
     './test/setup-jest.js'
