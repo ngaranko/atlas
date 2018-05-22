949303e7e3c5f4cb3dbb107ef1d6aa24dac0856e
Aron Martin <mail@aronmartin.com>
2018-05-22 17:14:08 +0200

    Remove test

--- src/map/ducks/panel-layers/map-panel-layers.js
+++ src/map/ducks/panel-layers/map-panel-layers.js
@@ -47,15 +47,14 @@ export const getActiveMapLayersWithinZoom = createSelector([getMapZoom, selectAc
 
 export const selectNotClickableVisibleMapLayers =
   createSelector([getActiveMapLayersWithinZoom, getMapOverlays],
-  (activePanelLayers, overlays) => {
-    const test = activePanelLayers
+  (activePanelLayers, overlays) => (
+    activePanelLayers
     .map((mapLayer) => [mapLayer, ...mapLayer.legendItems])
     .reduce((accumulator, legendItems) => accumulator.concat(legendItems), [])
     .filter((legendItem) => legendItem.notClickable)
     .filter((legendItem) => overlays
-      .some((overlay) => overlay.id === legendItem.id && overlay.isVisible));
-    return test;
-  }
+      .some((overlay) => overlay.id === legendItem.id && overlay.isVisible))
+  )
 );
 
 
