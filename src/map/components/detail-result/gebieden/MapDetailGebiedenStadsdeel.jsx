80dd5a1e0ba1f200cf1ade936c8745d4b264ceb8
Aron Martin <mail@aronmartin.com>
2018-06-25 14:17:48 +0200

    Add grex-details to preview panel

--- src/map/components/detail-result/gebieden/MapDetailGebiedenStadsdeel.jsx
+++ src/map/components/detail-result/gebieden/MapDetailGebiedenStadsdeel.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import MapDetailResultWrapper from '../MapDetailResultWrapper';
+import MapDetailResultItem from '../MapDetailResultItem';
 
 const MapDetailGebiedenStadsdeel = ({ panoUrl, stadsdeel, onMaximize, onPanoPreviewClick }) => (
   <MapDetailResultWrapper
@@ -10,7 +11,25 @@ const MapDetailGebiedenStadsdeel = ({ panoUrl, stadsdeel, onMaximize, onPanoPrev
     onPanoPreviewClick={onPanoPreviewClick}
     subTitle={stadsdeel.label}
     title="Stadsdeel"
-  />
+  >
+    { stadsdeel.grexStadsdeel && (
+      <ul className="map-detail-result__list">
+        { stadsdeel.grexStadsdeel.totaal_baten_display && <MapDetailResultItem
+          label="Totale begroting baten"
+          value={stadsdeel.grexStadsdeel.totaal_baten_display}
+        /> }
+        { stadsdeel.grexStadsdeel.totaal_kosten_display && <MapDetailResultItem
+          label="Totale begroting kosten"
+          value={stadsdeel.grexStadsdeel.totaal_kosten_display}
+        /> }
+        { stadsdeel.grexStadsdeel.totaal_resultaat_display && <MapDetailResultItem
+          label="Verschil"
+          value={stadsdeel.grexStadsdeel.totaal_resultaat_display}
+        /> }
+      </ul>
+    )
+  }
+  </MapDetailResultWrapper>
 );
 
 MapDetailGebiedenStadsdeel.propTypes = {
--- src/map/services/map-detail.test.js
+++ src/map/services/map-detail.test.js
@@ -17,11 +17,11 @@ describe('The map detail service', () => {
   it('calls the service fot the endpoint type specified', () => {
     adressenLigplaats.mockImplementation(() => 'ligplaats');
     expect(detail('https://acc.api.data.amsterdam.nl/bag/ligplaats/123')).toEqual('ligplaats');
-    expect(adressenLigplaats).toHaveBeenCalledWith('https://acc.api.data.amsterdam.nl/bag/ligplaats/123');
+    expect(adressenLigplaats).toHaveBeenCalledWith('https://acc.api.data.amsterdam.nl/bag/ligplaats/123', undefined);
 
     napPeilmerk.mockImplementation(() => 'peilmerk');
     expect(detail('https://acc.api.data.amsterdam.nl/nap/peilmerk/123')).toEqual('peilmerk');
-    expect(napPeilmerk).toHaveBeenCalledWith('https://acc.api.data.amsterdam.nl/nap/peilmerk/123');
+    expect(napPeilmerk).toHaveBeenCalledWith('https://acc.api.data.amsterdam.nl/nap/peilmerk/123', undefined);
   });
 
   it('does not call the service when the user does not have the required auth scope', () => {
@@ -34,9 +34,10 @@ describe('The map detail service', () => {
 
   it('calls the service when the user has the required auth scope', () => {
     vestiging.mockImplementation(() => 'vestiging');
-    expect(detail('https://acc.api.data.amsterdam.nl/handelsregister/vestiging/123', {
+    const user = {
       scopes: ['MON/R', 'HR/R']
-    })).toEqual('vestiging');
-    expect(vestiging).toHaveBeenCalledWith('https://acc.api.data.amsterdam.nl/handelsregister/vestiging/123');
+    };
+    expect(detail('https://acc.api.data.amsterdam.nl/handelsregister/vestiging/123', user)).toEqual('vestiging');
+    expect(vestiging).toHaveBeenCalledWith('https://acc.api.data.amsterdam.nl/handelsregister/vestiging/123', user);
   });
 });
