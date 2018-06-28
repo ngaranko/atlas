0624dc946b2f3116dc5bd0a9f18f7defdd2832fa
Boris Arkenaar <boris@arkenaar.com>
2018-06-28 09:10:06 +0200

    DP-5644 Add notification to pand

--- src/map/components/detail-result/adressen/MapDetailAdressenPand.jsx
+++ src/map/components/detail-result/adressen/MapDetailAdressenPand.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import MapDetailResultItem from '../MapDetailResultItem';
 import MapDetailResultStatusItem from '../MapDetailResultStatusItem';
 import MapDetailResultWrapper from '../MapDetailResultWrapper';
+import Notification from '../../../../shared/components/notification/Notification';
 
 const statusToCssModifier = {
   24: 'info',
@@ -12,30 +13,40 @@ const statusToCssModifier = {
   27: 'info',
   28: 'info',
   29: 'info',
+  30: '',
+  31: '',
   32: 'info'
 };
 
-const MapDetailAdressenPand = ({ panoUrl, pand, onMaximize, onPanoPreviewClick }) => (
-  <MapDetailResultWrapper
-    panoUrl={panoUrl}
-    onMaximize={onMaximize}
-    onPanoPreviewClick={onPanoPreviewClick}
-    subTitle={pand.label}
-    title="Pand"
-  >
-    <ul className="map-detail-result__list">
-      <MapDetailResultItem
-        label="Oorspronkelijk bouwjaar"
-        value={pand.year || 'onbekend'}
-      />
-      <MapDetailResultStatusItem
-        label="Status"
-        value={pand.status.description}
-        status={statusToCssModifier[pand.status.code]}
-      />
-    </ul>
-  </MapDetailResultWrapper>
-);
+const MapDetailAdressenPand = ({ panoUrl, pand, onMaximize, onPanoPreviewClick }) => {
+  const statusLevel = statusToCssModifier[pand.status.code];
+  return (
+    <MapDetailResultWrapper
+      panoUrl={panoUrl}
+      onMaximize={onMaximize}
+      onPanoPreviewClick={onPanoPreviewClick}
+      subTitle={pand.label}
+      title="Pand"
+    >
+      <ul className="map-detail-result__list">
+        {statusLevel && <li>
+          <Notification
+            level={statusLevel}
+          >{pand.status.description}</Notification>
+        </li>}
+        <MapDetailResultItem
+          label="Oorspronkelijk bouwjaar"
+          value={pand.year || 'onbekend'}
+        />
+        <MapDetailResultStatusItem
+          label="Status"
+          value={pand.status.description}
+          status={statusLevel}
+        />
+      </ul>
+    </MapDetailResultWrapper>
+  );
+};
 
 MapDetailAdressenPand.propTypes = {
   pand: PropTypes.shape({
