8a3f83f734202a12ab033bd636d97156ce76f89b
Bert Spaan <bert.spaan@gmail.com>
2018-07-11 13:38:03 +0200

    text in attribute preview panel is now selectable

--- src/map/components/detail-result/MapDetailResultWrapper.jsx
+++ src/map/components/detail-result/MapDetailResultWrapper.jsx
@@ -13,31 +13,31 @@ const MapDetailResultWrapper = ({
         map-detail-result__header--${panoUrl ? 'pano' : 'no-pano'}
       `}
     >
-      <button
+      {panoUrl && (
+        <button
         className={`
           map-detail-result__header-pano-button
           map-detail-result__header-pano-button--${panoUrl ? 'enabled' : 'disabled'}
         `}
         disabled={!panoUrl}
-        onClick={onPanoPreviewClick}
         title={panoUrl ? 'Panoramabeeld tonen' : 'Geen Panoramabeeld beschikbaar'}
-      >
-        {panoUrl && (
-          <img
-            alt="Panoramabeeld"
-            className="map-detail-result__header-pano"
-            height="292"
-            src={panoUrl}
-            width="438"
-          />
+        onClick={onPanoPreviewClick}
+        >
+        <img
+          alt="Panoramabeeld"
+          className="map-detail-result__header-pano"
+          height="292"
+          src={panoUrl}
+          width="438"
+        />
+        </button>
+      )}
+      <div className="map-detail-result__header-container">
+        <h1 className="map-detail-result__header-title">{title}</h1>
+        {subTitle && (
+          <h2 className="map-detail-result__header-subtitle">{subTitle}</h2>
         )}
-        <div className="map-detail-result__header-container">
-          <h1 className="map-detail-result__header-title">{title}</h1>
-          {subTitle && (
-            <h2 className="map-detail-result__header-subtitle">{subTitle}</h2>
-          )}
-        </div>
-      </button>
+      </div>
     </header>
     <div className="map-detail-result__scroll-wrapper">
       {children && (
@@ -59,8 +59,7 @@ const MapDetailResultWrapper = ({
 
 MapDetailResultWrapper.defaultProps = {
   children: null,
-  subTitle: '',
-  onPanoPreviewClick: () => null
+  subTitle: ''
 };
 
 MapDetailResultWrapper.propTypes = {
@@ -68,8 +67,7 @@ MapDetailResultWrapper.propTypes = {
   panoUrl: PropTypes.string.isRequired,
   subTitle: PropTypes.string,
   title: PropTypes.string.isRequired,
-  onMaximize: PropTypes.func.isRequired,
-  onPanoPreviewClick: PropTypes.func
+  onMaximize: PropTypes.func.isRequired
 };
 
 export default MapDetailResultWrapper;
