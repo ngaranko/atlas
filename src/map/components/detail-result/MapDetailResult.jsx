f6145272ac79de6ea3a3d0155e89bed90196f6ca
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-08-23 13:46:57 +0200

    fix: test-lint

--- src/map/components/detail-result/MapDetailResult.jsx
+++ src/map/components/detail-result/MapDetailResult.jsx
@@ -25,7 +25,6 @@ import MapDetailMeetbout from './MapDetailMeetbout';
 import MapDetailMonument from './MapDetailMonument';
 import MapDetailNapPeilmerk from './MapDetailNapPeilmerk';
 import MapDetailVestiging from './MapDetailVestiging';
-import { config } from 'bluebird';
 
 const MapDetailResult = ({ panoUrl, result, onMaximize, onPanoPreviewClick }) => {
   switch (result.endpointType) {
