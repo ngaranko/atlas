57d7fe1a097d35521e47b40e75b7b7fe808d4822
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-08-28 10:24:27 +0200

    DP-5806 show vesion in the console at start

--- modules/atlas/atlas.version.js
+++ modules/atlas/atlas.version.js
@@ -4,6 +4,7 @@
     angular
         .module('atlas')
         .constant('ATLAS_VERSION', {
+            version: VERSION,   // eslint-disable-line no-undef
             build: __BUILD_ID__ // eslint-disable-line no-undef
         });
 })();
