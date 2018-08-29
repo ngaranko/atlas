6697116497cedd291e4eb270835a63598d6c7c73
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-08-29 10:25:43 +0200

    DP-5798 add e2e test for navigation from homepage and authorization

--- test/cypress/integration/parcel-ownership_spec.js
+++ test/cypress/integration/parcel-ownership_spec.js
@@ -3,9 +3,9 @@ const homepage = '.c-homepage';
 const notification = '.c-panel--warning';
 const table = '.c-table';
 
-describe('trade-register (vestigingen) module', () => {
-  describe('user should be able to navigate to the trade-register from the homepage', () => {
-    it('should open the trade register', () => {
+describe('parcel-ownership (eigendommen) module', () => {
+  describe('user should be able to navigate to the parcel-ownership from the homepage', () => {
+    it('should open the parcel-ownership page', () => {
       // go to the homepage
       cy.visit('/');
       // the homepage should be visible
@@ -14,14 +14,14 @@ describe('trade-register (vestigingen) module', () => {
       cy.get('#homepage-address-block').should('exist').and('be.visible');
       // the data-selection should not exist yet
       cy.get(dataSelection).should('not.exist');
-      // click on the link to go to the datasets without a specified catalogus theme
-      cy.get('.homepage-block__link').contains('Handelsregister-tabel').click();
+      // click on the link to go to navigate to the page
+      cy.get('.homepage-block__link').contains('Kadaster-tabel').click();
       // the homepage should not be visible anymore
       cy.get(homepage).should('not.be.visible');
       // the data selection should exist
       cy.get(dataSelection).should('exist').and('be.visible');
-      // the title should contain Vestigingen
-      cy.get('h1').contains('Vestigingen').should('exist').and('be.visible');
+      // the title should contain Kadastrale objecten met zakelijk rechthebbenden
+      cy.get('h1').contains('Kadastrale objecten met zakelijk rechthebbenden').should('exist').and('be.visible');
     });
   });
 
@@ -29,11 +29,11 @@ describe('trade-register (vestigingen) module', () => {
     beforeEach(() => {
       // go to the homepage
       cy.visit('/');
-      // click on the link to go to the trade register
-      cy.get('.homepage-block__link').contains('Handelsregister-tabel').click();
+      // click on the link to go navigate to the page
+      cy.get('.homepage-block__link').contains('Kadaster-tabel').click();
     });
 
-    describe('user should not be able to view the trade register', () => {
+    describe('user should not be able to view the kadaster data', () => {
       it('should show a notification that the user must authenticate', () => {
         // a warning notification should be shown that the user must authenticate
         cy.get(notification).should('exist').and('be.visible');
@@ -57,18 +57,18 @@ describe('trade-register (vestigingen) module', () => {
 
     beforeEach(() => {
       cy.server();
-      cy.route('/dataselectie/hr/*').as('getResults');
+      cy.route('/dataselectie/brk/*').as('getResults');
 
       // go to the homepage
       cy.visit('/');
-      // click on the link to go to the trade register
-      cy.get('.homepage-block__link').contains('Handelsregister-tabel').click();
+      // click on the link to go to the eigendommen
+      cy.get('.homepage-block__link').contains('Kadaster-tabel').click();
 
       cy.wait('@getResults');
       cy.scrollTo('top');
     });
 
-    describe('user should be able to view the trade register', () => {
+    describe('user should be able to view the eigendommen', () => {
       it('should not show a notification', () => {
         // a warning notification should not exist
         cy.get(notification).should('not.exist').and('not.be.visible');
@@ -80,14 +80,14 @@ describe('trade-register (vestigingen) module', () => {
       });
     });
 
-    describe('user should be able to navigate to the trade register detail view', () => {
+    describe('user should be able to navigate to the eigendommen detail view', () => {
       it('should open the correct detail view', () => {
         cy.server();
-        cy.route('/handelsregister/vestiging/*').as('getVestiging');
+        cy.route('/brk/object-expand/*').as('getPerceel');
 
         cy.get('.c-table__content-row:first-child').click();
 
-        cy.wait('@getVestiging');
+        cy.wait('@getPerceel');
         cy.get('.qa-detail').should('exist').and('be.visible');
       });
     });
