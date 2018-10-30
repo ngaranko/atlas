1583ef79634550366bc02c40f6159b51dc602c33
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-10-30 16:37:14 +0100

    DP-4883 code review changes

--- src/map/containers/map/MapContainer.test.jsx
+++ src/map/containers/map/MapContainer.test.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import configureMockStore from 'redux-mock-store';
 import { shallow } from 'enzyme';
-// import { LatLngBounds, Bounds } from 'leaflet';
 
 import MapContainer, { overrideLeafletGetBounds } from './MapContainer';
 
