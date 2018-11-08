32203afa0f1a34c9b6f375bd280a2b3b442aa43c
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-11-08 16:40:43 +0100

    DP-6116 remove old cypress configuration

--- test/cypress/integration/authentication_spec.js
+++
@@ -1,9 +0,0 @@
-describe('auth module', () => {
-  describe('authentication', () => {
-    it('should be verified', () => {
-      cy.login();
-      cy.get('.qa-menu__title').should('contain', 'atlas.emp...');
-      cy.logout();
-    });
-  });
-});
