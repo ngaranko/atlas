0ad952941aea54cd2c06a2b16195a6f63b3eef7f
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-11-14 10:30:20 +0100

    react-as-root fix bakkie

--- webpack.common.js
+++ webpack.common.js
@@ -170,23 +170,23 @@ function commonConfig ({ nodeEnv }) {
             rel: 'stylesheet'
           },
           {
-            href: '/leaflet.css',
+            href: 'leaflet.css',
             rel: 'stylesheet'
           },
           {
-            href: '/leaflet.draw.css',
+            href: 'leaflet.draw.css',
             rel: 'stylesheet'
           },
           {
-            href: '/bbga.css',
+            href: 'bbga.css',
             rel: 'stylesheet'
           }
         ],
         scripts: [
-          '/leaflet.js',
-          '/NonTiledLayer.js',
-          '/proj4.js',
-          '/proj4leaflet.js'
+          'leaflet.js',
+          'NonTiledLayer.js',
+          'proj4.js',
+          'proj4leaflet.js'
         ]
       })
     ]
