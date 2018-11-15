18141f6075e216757fa6429da217d3d0fa728323
Chris van Mook <chris@chrisvanmook.com>
2018-11-15 13:05:53 +0100

    Enabled scoped classnames (css modules) for EmbedIframe

--- src/app/components/EmbedIframe/EmbedIframe.jsx
+++ src/app/components/EmbedIframe/EmbedIframe.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { getIframeUrl } from '../../../shared/services/embed-url/embed-url';
-import './_embedIframe.scss';
+import styles from './_embedIframe.scss';
 
 const EmbedIframeComponent = () => (
   <iframe
     title="Grote kaart | Dataportaal"
     id="atlas-iframe-map"
-    className={'c-embed-iframe'}
+    className={styles['c-embed-iframe']}
     width="500"
     height="400"
     src={getIframeUrl()}
