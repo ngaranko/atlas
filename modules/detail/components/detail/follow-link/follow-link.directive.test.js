5e3f707f68f684756b57acf1336f4fa3928d745f
royderks <hello@hackteam.io>
2018-11-20 13:49:09 +0100

    fixed test for onkeypress event on dp-follow-link

--- modules/detail/components/detail/follow-link/follow-link.directive.test.js
+++ modules/detail/components/detail/follow-link/follow-link.directive.test.js
@@ -51,4 +51,23 @@ describe('The follow link directive', function () {
         directive.click();
         expect($window.open).toHaveBeenCalled();
     });
+
+    it('opens a window when pressing key', function () {
+        var directive;
+
+        spyOn($window, 'open');
+
+        directive = getDirective ('http://www.linktofollow.link');
+        expect(directive.attr('dp-follow-link')).toBe('http://www.linktofollow.link');
+
+        var e = $.Event("keypress");
+        e.key = 'Enter';
+
+        directive.triggerHandler(e);
+        expect($window.open).toHaveBeenCalled();
+
+        e.key = 'Shift';
+        directive.triggerHandler(e);
+        expect($window.open).toHaveBeenCalledTimes(1);
+    });
 });
