4f4e44b9bbe7adc5e4dfc34d7c035704092158bf
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-11-22 13:43:58 +0100

    DP-6246 map_spec reviewed

--- test/e2e/cypress/integration/map_spec.js
+++ test/e2e/cypress/integration/map_spec.js
@@ -26,6 +26,7 @@ describe('map module', () => {
 
   describe('user should be able to interact with the map', () => {
     it('should show results based on the interaction with the map', () => {
+      const svgMapPath = '/assets/images/map/';
       cy.server();
       cy.defineGeoSearchRoutes();
       cy.route('/bag/nummeraanduiding/*').as('getNummeraanduiding');
@@ -51,7 +52,7 @@ describe('map module', () => {
       cy.get('.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive')
         .should('exist')
         .and('be.visible')
-        .and('have.attr', 'src', 'assets/images/map/detail.svg');
+        .and('have.attr', 'src', `${svgMapPath}detail.svg`);
       cy.checkPreviewPanel(['Dam 1', 'winkelfunctie']);
 
       // click somewhere in the map (not on a marker)
@@ -61,7 +62,7 @@ describe('map module', () => {
       // check that the search icon is drawn on the map
       cy.get('.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive')
         .should('exist').and('be.visible')
-        .and('have.attr', 'src', 'assets/images/map/search.svg');
+        .and('have.attr', 'src', `${svgMapPath}search.svg`);
 
       cy.wait('@getMonumenten');
       cy.wait('@getNummeraanduiding');
@@ -72,7 +73,7 @@ describe('map module', () => {
 
       cy.get('.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive')
         .should('exist').and('be.visible')
-        .and('have.attr', 'src', 'assets/images/map/detail.svg');
+        .and('have.attr', 'src', `${svgMapPath}detail.svg`);
       cy.get('.map-preview-panel.map-preview-panel--visible')
         .get('img.map-detail-result__header-pano')
         .should('exist').and('be.visible');
@@ -98,7 +99,7 @@ describe('map module', () => {
       // ensure the viewport is always the same in this test, so the clicks can be aligned properly
       cy.viewport(1000, 660);
 
-      cy.visit('/#?mpb=topografie&mpz=11&mpfs=T&mpv=52.3728007:4.899258&pgn=home&uvm=T');
+      cy.visit('/kaart?kaart=&lat=52.3728007&lng=4.899258&zoom=11');
       cy.get('.map-layers__category').contains('Meetbouten - Zaksnelheid').click();
       cy.get(notification)
         .contains('Zichtbaar bij verder inzoomen')
@@ -159,7 +160,7 @@ describe('map module', () => {
   describe('user should be able to use the map', () => {
     it('should render the leaflet map', () => {
       // route to the map by url
-      cy.visit('/#?mpb=topografie');
+      cy.visit('/kaart');
       // the map container should exist
       cy.get(map).should('exist').and('be.visible');
       // the leaflet map should exist
@@ -170,7 +171,7 @@ describe('map module', () => {
 
     it('should add a map-layer to the leaflet map', () => {
       // route to the map
-      cy.visit('/#?mpb=topografie&mpz=11&mpfs=T&mpv=52.3731081:4.8932945&pgn=home&uvm=T');
+      cy.visit('/kaart?lat=52.3731081&lon=4.8932945&zoom=11');
       // get the first map-layer button
       cy.get('.map-layers__title').first().click();
       // check if the map has overlay panes
@@ -183,7 +184,7 @@ describe('map module', () => {
   describe('user should be able to open the map panel when collapsed', () => {
     it('should add open the map panel component', () => {
       // route to the map
-      cy.visit('/#?mpb=topografie');
+      cy.visit('/kaart?legenda=false');
       // the map-panel should have the class collapsed
       cy.get('.map-panel').should('have.class', 'map-panel--collapsed');
       // the scroll wrapper should not be visible when map panel is collapsed
