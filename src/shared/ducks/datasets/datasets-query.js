74f50d3f3f2e01167269430e2e24ef45195d3155
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-11-26 11:41:25 +0100

    DP-6246 remove zoekterm when empty filters

--- src/shared/ducks/datasets/datasets-query.js
+++ src/shared/ducks/datasets/datasets-query.js
@@ -1,14 +1,20 @@
 import { FETCH_DATASETS_SUCCESS, initialState, SET_PAGE } from './data/data';
-import { getPage } from './datasets';
+import { getPage, getSearchText } from './datasets';
+import { EMPTY_FILTERS } from '../filters/filters';
 
 export default {
   page: {
     selector: getPage,
     defaultValue: initialState.page
+  },
+  zoekterm: {
+    selector: getSearchText,
+    defaultValue: initialState.searchText
   }
 };
 
 export const ACTIONS = [
   SET_PAGE,
-  FETCH_DATASETS_SUCCESS
+  FETCH_DATASETS_SUCCESS,
+  EMPTY_FILTERS
 ];
