c2e40dfdae68333ac4a202e0881d86de18ec3e65
Chris van Mook <chris@chrisvanmook.com>
2018-11-27 10:28:16 +0100

    Fix tests

--- src/app/containers/__tests__/PanoramaContainer.test.jsx
+++ src/app/containers/__tests__/PanoramaContainer.test.jsx
@@ -4,9 +4,11 @@ import configureMockStore from 'redux-mock-store';
 import PanoramaContainer from '../PanoramaContainer';
 import { getHotspots, getPanorama } from '../../../shared/ducks/panorama/panorama';
 import { isPrintMode } from '../../../shared/ducks/ui/ui';
+import { getMapCenter } from '../../../map/ducks/map/map-selectors';
 
 jest.mock('../../../shared/ducks/panorama/panorama');
 jest.mock('../../../shared/ducks/ui/ui');
+jest.mock('../../../map/ducks/map/map-selectors');
 
 describe('PanoramaContainer', () => {
   it('should render', () => {
@@ -14,6 +16,7 @@ describe('PanoramaContainer', () => {
     getPanorama.mockReturnValue(({}));
     getHotspots.mockReturnValue([]);
     isPrintMode.mockReturnValue(false);
+    getMapCenter.mockReturnValue([123, 321]);
     const component = shallow(
       <PanoramaContainer isFullscreen={false} />,
       { context: { store } }
