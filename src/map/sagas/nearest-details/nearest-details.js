20da9dcbbfe708922585738ac4b8191bd8eddd27
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-12-12 15:53:54 +0100

    DP-6409 show selected object on the map

--- src/map/sagas/nearest-details/nearest-details.js
+++ src/map/sagas/nearest-details/nearest-details.js
@@ -4,6 +4,8 @@ import { REQUEST_NEAREST_DETAILS } from '../geosearch/geosearch';
 import { getPageActionEndpoint } from '../../../store/redux-first-router';
 import { getDetailView } from '../../../shared/ducks/detail/selectors';
 import { setGeoLocation } from '../../../shared/ducks/data-search/actions';
+import { showDetail } from '../../../shared/ducks/detail/actions';
+import { FETCH_MAP_DETAIL_SUCCESS } from '../../ducks/detail/constants';
 
 export function* fetchNearestDetails(action) {
   const {
@@ -24,6 +26,14 @@ export function* fetchNearestDetails(action) {
   }
 }
 
+export function* fireShowDetail(action) {
+  yield put(showDetail({
+    display: action.mapDetail._display,
+    geometry: action.mapDetail.geometrie
+  }));
+}
+
 export default function* watchFetchNearestDetails() {
   yield takeLatest(REQUEST_NEAREST_DETAILS, fetchNearestDetails);
+  yield takeLatest(FETCH_MAP_DETAIL_SUCCESS, fireShowDetail);
 }
