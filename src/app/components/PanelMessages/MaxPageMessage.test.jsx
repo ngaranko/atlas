f286504b88f9705631394802a4e6119662cf10d0
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-12-19 13:02:49 +0100

    DP-6416 improve coverage

---
+++ src/app/components/PanelMessages/MaxPageMessage.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import MaxPageMessage from './MaxPageMessage';
+
+describe('MaxPageMessage', () => {
+  it('should render everything', () => {
+    const component = shallow(<MaxPageMessage maxAvailablePages={100} />);
+    expect(component).toMatchSnapshot();
+  });
+});
--- src/map/components/leaflet/custom/non-tiled-layer/NonTiledLayer.test.jsx
+++ src/map/components/leaflet/custom/non-tiled-layer/NonTiledLayer.test.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import L from 'leaflet';
 import { shallow } from 'enzyme';
 import NonTiledLayer from './NonTiledLayer';
 
+jest.mock('leaflet');
+
 describe('NonTiledLayer', () => {
-  it('should render everything', () => {
-    const wrapper = shallow(
+  let wrapper;
+  let wrapperInstance;
+  const props = {
+    url: 'https://wms-url',
+    rotationAngle: 0,
+    layers: ['wms-layer'],
+    opacity: 100
+  };
+
+  beforeEach(() => {
+    L.nonTiledLayer = {
+      wms: jest.fn()
+    };
+
+
+    wrapper = shallow(
       <NonTiledLayer
-        url="url"
+        {...props}
       />, {
         context: {
           layerContainer: {
@@ -15,8 +32,56 @@ describe('NonTiledLayer', () => {
         }
       }
     );
-    wrapper.instance().createLeafletElement({ rotationAngle: 0 });
+    wrapperInstance = wrapper.instance();
+  });
 
-    expect(wrapper).toMatchSnapshot();
+  it('should render everything', () => {
+    expect(L.nonTiledLayer.wms).toHaveBeenCalledTimes(1);
+    expect(L.nonTiledLayer.wms).toHaveBeenCalledWith(
+      props.url,
+      {
+        rotationAngle: 0,
+        layers: ['wms-layer'],
+        opacity: 100
+      }
+    );
+  });
+
+  it('should update the layers when the property is changed', () => {
+    wrapperInstance.leafletElement = {
+      _wmsUrl: 'http://old-wms-url',
+      wmsParams: { layers: ['old-layer'] }
+    };
+
+    const fromProps = {
+      url: 'http://old-wms-url',
+      layers: ['old-layer']
+    };
+    const toProps = {
+      url: 'http://new-wms-url',
+      layers: ['new-layer']
+    };
+
+    wrapperInstance.updateLeafletElement(fromProps, toProps);
+    expect(wrapperInstance.leafletElement).toEqual({
+      _wmsUrl: 'http://new-wms-url',
+      wmsParams: { layers: ['new-layer'] }
+    });
+  });
+
+  it('should hide the layer', () => {
+    wrapperInstance.leafletElement = {
+      setOpacity: jest.fn()
+    };
+
+    const fromProps = {
+      opacity: 100
+    };
+    const toProps = {
+      opacity: 0
+    };
+
+    wrapperInstance.updateLeafletElement(fromProps, toProps);
+    expect(wrapperInstance.leafletElement.setOpacity).toHaveBeenCalledWith(toProps.opacity);
   });
 });
