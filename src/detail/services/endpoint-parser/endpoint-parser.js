268c8cae86e60c46826c0f0054d6fd97677955a0
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-01-16 08:57:55 +0100

    DP-6202 moved glossary and endpoint parser to vanilla js

---
+++ src/detail/services/endpoint-parser/endpoint-parser.js
@@ -0,0 +1,46 @@
+const isZakelijkRecht = (someUriParts) => someUriParts[0] === 'brk' &&
+  someUriParts[1] === 'zakelijk-recht' &&
+  someUriParts[someUriParts.length - 1] === 'subject';
+
+export const getParts = (endpoint) => {
+  const anchor = document.createElement('a');
+  anchor.href = endpoint;
+
+  // Transform http://www.api-root.com/this/that/123 to ['this', 'that', '123']
+  const uriParts = anchor.pathname
+    .replace(/^\//, '') // Strip leading slash
+    .replace(/\/$/, '') // Strip trailing slash
+    .split('/');
+
+  if (isZakelijkRecht(uriParts)) {
+    return ['brk', 'subject'];
+  }
+  // Remove the last segment (the ID)
+  uriParts.pop();
+
+  // Return the last two 'path' segments
+  return uriParts.slice(-2);
+};
+
+export const getTemplateUrl = (endpoint) => {
+  const [category, subject] = getParts(endpoint);
+  return `modules/detail/components/detail/templates/${category}/${subject}.html`;
+};
+
+export const getGlossaryKey = (endpoint) => {
+  const [type, subject] = getParts(endpoint);
+  let key = subject;
+  if (type === 'grondexploitatie') {
+    // grondexploitatie subject === "project", 'grondexploitatie' is more descriptive value.
+    key = type;
+  }
+  return key.toUpperCase().replace(/-/g, '_');
+};
+
+const endpointParser = {
+  getTemplateUrl,
+  getGlossaryKey,
+  getParts
+};
+
+export default endpointParser;
