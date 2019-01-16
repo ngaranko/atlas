2097f138523fb99c80cf54a592b4fde849196c50
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-01-16 08:38:25 +0100

    DP-6202 added the static document titles

--- src/app/routes.js
+++ src/app/routes.js
@@ -16,127 +16,127 @@ export const routing = {
     page: PAGES.DATASETS
   },
   addresses: {
-    title: '',
+    title: 'Adressen',
     path: '/data/bag/adressen',
     type: `${ROUTER_NAMESPACE}/${PAGES.ADDRESSES}`,
     page: PAGES.ADDRESSES
   },
   establishments: {
-    title: '',
+    title: 'Vestigingen',
     path: '/data/hr/vestigingen',
     type: `${ROUTER_NAMESPACE}/${PAGES.ESTABLISHMENTS}`,
     page: PAGES.ESTABLISHMENTS
   },
   cadastralObjects: {
-    title: '',
+    title: 'Kadastrale objecten',
     path: '/data/brk/kadastrale-objecten',
     type: `${ROUTER_NAMESPACE}/${PAGES.CADASTRAL_OBJECTS}`,
     page: PAGES.CADASTRAL_OBJECTS
   },
   searchDatasets: {
+    title: 'Datasets zoekresultaten',
     path: '/datasets/zoek',
     type: `${ROUTER_NAMESPACE}/${PAGES.SEARCH_DATASETS}`,
     page: PAGES.SEARCH_DATASETS
   },
   datasetsDetail: {
-    title: '',
+    title: 'Datasets',
     path: '/datasets/:id',
     type: `${ROUTER_NAMESPACE}/${PAGES.DATASETS_DETAIL}`,
     page: PAGES.DATASETS_DETAIL
   },
   dataQuerySearch: {
-    title: 'Data zoeken',
+    title: 'Data zoekresultaten',
     path: '/data/zoek',
     type: `${ROUTER_NAMESPACE}/${PAGES.DATA_QUERY_SEARCH}`,
     page: PAGES.DATA_QUERY_SEARCH
   },
   dataGeoSearch: {
-    title: 'Data zoeken',
+    title: 'Data zoekresultaten op locatie',
     path: '/data/geozoek',
     type: `${ROUTER_NAMESPACE}/${PAGES.DATA_GEO_SEARCH}`,
     page: PAGES.DATA_GEO_SEARCH
   },
   dataSearchCategory: {
-    title: 'Data zoeken',
+    title: 'Data zoekresultaten',
     path: '/data/:category',
     type: `${ROUTER_NAMESPACE}/${PAGES.DATA_SEARCH_CATEGORY}`,
     page: PAGES.DATA_SEARCH_CATEGORY
   },
   panorama: {
-    title: 'Panorama',
+    title: 'Panoramabeeld',
     path: '/data/panorama/:id',
     type: `${ROUTER_NAMESPACE}/${PAGES.PANORAMA}`,
     page: PAGES.PANORAMA
   },
   nieuws: {
-    title: '',
+    title: 'Nieuws',
     path: '/nieuws',
     type: `${ROUTER_NAMESPACE}/${PAGES.NEWS}`,
     page: PAGES.NEWS
   },
   help: {
-    title: '',
+    title: 'Help',
     path: '/help',
     type: `${ROUTER_NAMESPACE}/${PAGES.HELP}`,
     page: PAGES.HELP
   },
   proclaimer: {
-    title: '',
+    title: 'Proclaimer',
     path: '/proclaimer',
     type: `${ROUTER_NAMESPACE}/${PAGES.PROCLAIMER}`,
     page: PAGES.PROCLAIMER
   },
-
   bediening: {
-    title: '',
+    title: 'Bediening',
     path: '/bediening',
     type: `${ROUTER_NAMESPACE}/${PAGES.CONTROLS}`,
     page: PAGES.CONTROLS
   },
   gegevens: {
-    title: '',
+    title: 'Gegevens',
     path: '/gegevens',
     type: `${ROUTER_NAMESPACE}/${PAGES.DATA_INFO}`,
     page: PAGES.DATA_INFO
   },
   apis: {
-    title: '',
+    title: 'Api\'s',
     path: '/apis',
     type: `${ROUTER_NAMESPACE}/${PAGES.ABOUT_API}`,
     page: PAGES.ABOUT_API
   },
   privacy_beveiliging: {
-    title: '',
+    title: 'Privacy en informatiebeveiliging',
     path: '/privacy-en-informatiebeveiliging',
     type: `${ROUTER_NAMESPACE}/${PAGES.PRIVACY_SECURITY}`,
     page: PAGES.PRIVACY_SECURITY
   },
   beschikbaar_kwaliteit: {
-    title: '',
+    title: 'Beschikbaarheid en kwaliteit van de data',
     path: '/beschikbaarheid-en-kwaliteit-data',
     type: `${ROUTER_NAMESPACE}/${PAGES.AVAILABILITY_QUALITY}`,
     page: PAGES.AVAILABILITY_QUALITY
   },
   beheer_werkwijze: {
-    title: '',
+    title: 'Technisch beheer en werkwijze',
     path: '/technisch-beheer-en-werkwijze',
     type: `${ROUTER_NAMESPACE}/${PAGES.MANAGEMENT}`,
     page: PAGES.MANAGEMENT
   },
   statistieken: {
-    title: '',
+    title: 'Statistieken',
     path: '/statistieken',
     type: `${ROUTER_NAMESPACE}/${PAGES.STATISTICS}`,
     page: PAGES.STATISTICS
   },
   verplaatst: {
-    title: '',
+    title: 'Pagina verplaatst',
     path: '/verplaatst',
     type: `${ROUTER_NAMESPACE}/${PAGES.MOVED}`,
     page: PAGES.MOVED
   },
   niet_gevonden: {
-    title: '',
+    title: 'Pagina niet gevonden',
     path: '/niet-gevonden',
     type: `${ROUTER_NAMESPACE}/${PAGES.NOT_FOUND}`,
     page: PAGES.NOT_FOUND
--- src/store/middleware/documentHead.js
+++ src/store/middleware/documentHead.js
@@ -1,14 +1,38 @@
 import get from 'lodash.get';
 import { routing } from '../../app/routes';
+import { VIEWS } from '../../shared/ducks/data-search/constants';
 
 const TITLE_SUFFIX = 'Dataportaal';
-const TITLE_DEFAULT = 'Dataportaal';
+const TITLE_DEFAULT = 'Amsterdam';
 
 const documentHead = () => (next) => (action) => {
   const typeIsRoute = Object.keys(routing).map((key) => routing[key].type).includes(action.type);
   if (typeIsRoute) {
     const page = Object.keys(routing).find((key) => routing[key].type === action.type);
-    const pageTitle = get(routing, `[${page}].title`, TITLE_DEFAULT);
+    const pageType = get(routing, `[${page}].type`, '');
+    let pageTitle = get(routing, `[${page}].title`, TITLE_DEFAULT);
+
+    switch (pageType) {
+      // eslint-disable-next-line no-case-declarations
+      case routing.home.type:
+        const view = get(action, 'meta.query.view', '');
+        const embed = get(action, 'meta.query.embed', 'false');
+        if (view === VIEWS.MAP) {
+          if (embed === 'false') {
+            pageTitle = 'Grote kaart';
+          } else {
+            pageTitle = 'Grote kaart | Embeded ';
+          }
+        }
+        break;
+      // eslint-disable-next-line no-case-declarations
+      case routing.dataDetail.type:
+        const subtype = get(action, 'payload.subtype', 'onbekend');
+        pageTitle = subtype;
+        break;
+      default:
+        break;
+    }
 
     document.title = `${pageTitle} - ${TITLE_SUFFIX}`;
   }
