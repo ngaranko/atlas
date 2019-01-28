941adceb7ec9bc0a398b24965d98cde09020fff2
royderks <hello@hackteam.io>
2019-01-28 14:10:09 +0100

    Added tests for detail selectors, removed useless check on route for data detail

--- src/store/middleware/piwik/routes.js
+++ src/store/middleware/piwik/routes.js
@@ -1,7 +1,6 @@
 import { routing } from '../../../app/routes';
 import { isEmbedded } from '../../../shared/ducks/ui/ui';
 import { getDetail } from '../../../shared/ducks/detail/selectors';
-import { isDataDetailPage } from '../../redux-first-router/selectors';
 import { PIWIK_CONSTANTS } from './piwikMiddleware';
 
 let routes = Object.entries(routing).reduce((acc, [, value]) => ({
@@ -41,7 +40,7 @@ routes = {
   },
   'atlasRouter/DATA_DETAIL': function trackRoute({ firstAction = null, href, title, state, tracking }) {
     return (
-      !firstAction && isDataDetailPage(state) && (tracking && tracking.id !== getDetail(state).id)
+      !firstAction && (tracking && tracking.id !== getDetail(state).id)
     ) ? [
       PIWIK_CONSTANTS.TRACK_VIEW,
       title, // PAGEVIEW -> DETAIL VIEW CLICK THROUGH VIEWS
