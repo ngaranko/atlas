b8aac43d212562f24d2501866e89a365173e267b
Roy Derks <r.m.a.e.a.derks@gmail.com>
2019-02-18 10:49:16 +0100

    DP-6673 + DP-6307: Change configuration for maplayer panorama and taxi routes (#1701)

--- src/map/services/map-panel-layers/map-panel-layers.config.js
+++ src/map/services/map-panel-layers/map-panel-layers.config.js
@@ -42,6 +42,7 @@ export default [
     layers: ['panorama_new'],
     legendItems: [
       {
+        imageRule: '2018',
         selectable: false,
         title: '2018 WOZ'
       }
@@ -72,6 +73,7 @@ export default [
     layers: ['panorama_new'],
     legendItems: [
       {
+        imageRule: '2017',
         selectable: false,
         title: '2017 WOZ'
       }
@@ -960,7 +962,7 @@ export default [
       }
     ],
     maxZoom: 16,
-    minZoom: 11,
+    minZoom: 8,
     title: 'Routes - Taxi en vrachtauto',
     url: '/maps/hoofdroutes?version=1.3.0&service=WMS'
   },
