b7e987daea2b9dbfbe2b1b502b60e4d6bd51dad2
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-03-21 07:44:55 +0100

    DP-6765 fixed unittests for error and content saga

---
+++ src/shared/sagas/content/content.test.js
@@ -0,0 +1,23 @@
+import { testSaga, expectSaga } from 'redux-saga-test-plan';
+import watchContentSaga, { navigate } from './content';
+import { NAVIGATE, CMS_PAGE_MAPPING } from '../../ducks/content/constants';
+import PAGES from '../../../app/pages';
+import { routing } from '../../../app/routes';
+
+describe('watchContentSaga', () => {
+  it('should watch the error actions and call authenticateUser', () => {
+    testSaga(watchContentSaga).next()
+      .takeLatestEffect(NAVIGATE, navigate).next()
+      .isDone();
+  });
+});
+
+describe('navigate', () => {
+  it('should navigate to the right content', () => {
+    const mockPayload = { ...CMS_PAGE_MAPPING[PAGES.NEWS] };
+    const mockAction = (type) => ({ type });
+    expectSaga(navigate, { payload: mockPayload })
+      .put(mockAction(routing.nieuws.type))
+      .run();
+  });
+});
