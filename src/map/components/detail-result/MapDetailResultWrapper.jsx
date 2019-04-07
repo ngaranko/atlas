1c453a599483e3bd05ed66047aff2511d5831f71
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-04-07 08:02:30 +0200

    DP-6835 fix snapshots in windows

--- src/map/components/detail-result/MapDetailResultWrapper.jsx
+++ src/map/components/detail-result/MapDetailResultWrapper.jsx
@@ -4,61 +4,61 @@ import PropTypes from 'prop-types';
 import { isEmbedded } from '../../../shared/ducks/ui/ui';
 
 const MapDetailResultWrapper = ({
-  children, panoUrl, subTitle, title, onMaximize, onPanoPreviewClick, isEmbed
-}) => (
-  <section className="map-detail-result">
-    <header
-      className={`
-        map-detail-result__header
-        map-detail-result__header--${!isEmbed && panoUrl ? 'pano' : 'no-pano'}
-      `}
-    >
-      {panoUrl && (
-        <button
-          className={`
-            map-detail-result__header-pano-button
-            map-detail-result__header-pano-button--${panoUrl ? 'enabled' : 'disabled'}
-          `}
-          disabled={!panoUrl}
-          title={panoUrl ? 'Panoramabeeld tonen' : 'Geen Panoramabeeld beschikbaar'}
-          onClick={onPanoPreviewClick}
-        >
-          <img
-            alt="Panoramabeeld"
-            className="map-detail-result__header-pano"
-            height="292"
-            src={panoUrl}
-            width="438"
-          />
-        </button>
-      )}
-      <div className="map-detail-result__header-container">
-        <h1 className="map-detail-result__header-title">{title}</h1>
-        {subTitle && (
-          <h2 className="map-detail-result__header-subtitle">{subTitle}</h2>
+  children, panoUrl, subTitle, title, onMaximize, onPanoPreviewClick, isEmbed }) => (
+    <section className="map-detail-result">
+      <header
+        className={`
+      map-detail-result__header
+      map-detail-result__header--${!isEmbed && panoUrl ? 'pano' : 'no-pano'}
+    `}
+      >
+        {panoUrl && (
+          <button
+            className={`
+              map-detail-result__header-pano-button
+              map-detail-result__header-pano-button--${panoUrl ? 'enabled' : 'disabled'}
+            `}
+            disabled={!panoUrl}
+            title={panoUrl ? 'Panoramabeeld tonen' : 'Geen Panoramabeeld beschikbaar'}
+            onClick={onPanoPreviewClick}
+          >
+            <img
+              alt="Panoramabeeld"
+              className="map-detail-result__header-pano"
+              height="292"
+              src={panoUrl}
+              width="438"
+            />
+          </button>
         )}
+        <div className="map-detail-result__header-container">
+          <h1 className="map-detail-result__header-title">{title}</h1>
+          {subTitle && (
+            <h2 className="map-detail-result__header-subtitle">{subTitle}</h2>
+          )}
+        </div>
+      </header>
+      <div className="map-detail-result__scroll-wrapper">
+        {children && (
+          [children]
+        )}
+        <footer className="map-search-results__footer">
+          <button
+            className="map-search-results__button"
+            onClick={onMaximize}
+            title="Volledig weergeven"
+          >
+            <span className={`
+                    map-search-results__button-icon
+                    map-search-results__button-icon--maximize
+                  `}
+            />
+            Volledig weergeven
+      </button>
+        </footer>
       </div>
-    </header>
-    <div className="map-detail-result__scroll-wrapper">
-      {children && (
-        [children]
-      )}
-      <footer className="map-search-results__footer">
-        <button
-          className="map-search-results__button"
-          onClick={onMaximize}
-          title="Volledig weergeven"
-        >
-          <span className="
-            map-search-results__button-icon
-            map-search-results__button-icon--maximize"
-          />
-          Volledig weergeven
-        </button>
-      </footer>
-    </div>
-  </section>
-);
+    </section>
+  );
 
 MapDetailResultWrapper.defaultProps = {
   children: null,
