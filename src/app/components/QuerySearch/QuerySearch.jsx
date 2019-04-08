253c04c622860f8bafad99ca863c896882e9b08b
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-04-08 14:25:50 +0200

    DP-6803 remove dubble display in the datasets search

--- src/app/components/QuerySearch/QuerySearch.jsx
+++ src/app/components/QuerySearch/QuerySearch.jsx
@@ -22,50 +22,50 @@ const QuerySearch = ({
   user
 }) => (
   <div className="c-data-selection c-dashboard__content">
-    {(isLoading) && <LoadingIndicator />}
-    {!isLoading && (
-      <div className="qa-data-selection-content">
-        <TabBar
-          totalNumberOfResults={numberOfDataResults + numberOfDatasetResults}
-          showDatasetsButton={currentPage === PAGES.SEARCH_DATASETS}
-        >
-          <Tabs
-            currentTab={(currentPage === PAGES.DATA_QUERY_SEARCH) ? 'Data' : 'Datasets'}
+      {(isLoading) && <LoadingIndicator />}
+      {!isLoading && (
+        <div className="qa-data-selection-content">
+          <TabBar
+            totalNumberOfResults={numberOfDataResults + numberOfDatasetResults}
+            showDatasetsButton={currentPage === PAGES.SEARCH_DATASETS}
           >
-            <Tab
-              label="Data"
-              count={numberOfDataResults}
-              onClick={() => toDataPage(query, filters)}
-            />
-            <Tab
-              label="Datasets"
-              count={numberOfDatasetResults}
-              onClick={() => toDatasetPage(query, filters)}
-            />
-          </Tabs>
-        </TabBar>
-        <div className="qa-search-results">
-          {(currentPage === PAGES.DATA_QUERY_SEARCH &&
-            <div>
-              <DataSearchQuery />
-              {(!!numberOfDataResults && (!user.scopes.includes('HR/R') || !user.scopes.includes('BRK/RS'))) &&
-              <MoreResultsWhenLoggedIn />
-              }
-            </div>
-          )}
-          {(currentPage === PAGES.SEARCH_DATASETS &&
-            <Dataset />
-          )}
-          <div className="u-row">
-            <div className="u-col-sm--12">
-              <div className="u-margin__top--4"><ShareBar /></div>
-            </div>
+            <Tabs
+              currentTab={(currentPage === PAGES.DATA_QUERY_SEARCH) ? 'Data' : 'Datasets'}
+            >
+              <Tab
+                label="Data"
+                count={numberOfDataResults}
+                onClick={() => toDataPage(query, filters)}
+              />
+              <Tab
+                label="Datasets"
+                count={numberOfDatasetResults}
+                onClick={() => toDatasetPage(query, filters)}
+              />
+            </Tabs>
+          </TabBar>
+          <div className="qa-search-results">
+            {(currentPage === PAGES.DATA_QUERY_SEARCH &&
+              <div>
+                <DataSearchQuery />
+                {(!!numberOfDataResults && (!user.scopes.includes('HR/R') || !user.scopes.includes('BRK/RS'))) &&
+                  <MoreResultsWhenLoggedIn />
+                }
+                <div className="u-row">
+                  <div className="u-col-sm--12">
+                    <div className="u-margin__top--4"><ShareBar /></div>
+                  </div>
+                </div>
+              </div>
+            )}
+            {(currentPage === PAGES.SEARCH_DATASETS &&
+              <Dataset />
+            )}
           </div>
         </div>
-      </div>
-    )}
-  </div>
-);
+      )}
+    </div>
+  );
 
 QuerySearch.defaultProps = {
   isLoading: true,
