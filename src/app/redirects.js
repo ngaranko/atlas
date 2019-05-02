6de8266e084d2090c16be8ebb5068213f2b16f5f
Rob Maijers <rmaijers@users.noreply.github.com>
2019-05-02 12:58:46 +0200

    Temp disable verkeersstromen layer

--- src/app/redirects.js
+++ src/app/redirects.js
@@ -71,7 +71,8 @@ const legacyRoutes = [
 const shortUrls = [
   {
     from: '/themakaart/taxi',
-    to: `${routing.data.path}?${VIEW}=kaart&${LAYERS}=pr%3A1%7Coovtig%3A1%7Cvezips%3A1%7Cmzt%3A0%7Cslpb%3A1%7Cslpnb%3A1%7Cbgt%3A1%7Ctar%3A1%7Cpvrts%3A1%7Cpvrll%3A1%7Cpvrpr%3A1&${LEGEND}=true`
+    // temp disable verkeersstromen; reinsert in layers: pr%3A1%7C
+    to: `${routing.data.path}?${VIEW}=kaart&${LAYERS}=oovtig%3A1%7Cvezips%3A1%7Cmzt%3A0%7Cslpb%3A1%7Cslpnb%3A1%7Cbgt%3A1%7Ctar%3A1%7Cpvrts%3A1%7Cpvrll%3A1%7Cpvrpr%3A1&${LEGEND}=true`
   }
 ];
 
