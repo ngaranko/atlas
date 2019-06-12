278c76e85b8752ecd4bfe06ead3df7be2d16b665
royderks <10717410+royderks@users.noreply.github.com>
2019-06-12 15:15:46 +0200

    Updated versions for enzyme

--- src/app/App.test.jsx
+++ src/app/App.test.jsx
@@ -5,29 +5,25 @@ import App from './App';
 import { VIEW_MODE } from '../shared/ducks/ui/ui';
 
 describe('App', () => {
-  jest.mock('react', () => {
-    // eslint-disable-next-line no-shadow
-    const React = jest.requireActual('react');
-    React.Suspense = ({ children }) => children;
-    return React;
-  });
+  const initialState = {
+    ui: {
+      isEmbed: false,
+      isEmbedPreview: false,
+      isPrintMode: false,
+      viewMode: VIEW_MODE.FULL
+    },
+    map: {
+      view: 'home'
+    },
+    user: {},
+    error: {
+      hassErrors: false
+    }
+  }
 
   it('should render the homepage', () => {
-    const store = configureMockStore()({
-      ui: {
-        isEmbed: false,
-        isEmbedPreview: false,
-        isPrintMode: false,
-        viewMode: VIEW_MODE.FULL
-      },
-      map: {
-        view: 'home'
-      },
-      user: {},
-      error: {
-        hassErrors: false
-      }
-    });
+    const store = configureMockStore()({ ...initialState });
+
     const component = shallow(
       <App />, { context: { store } }
     ).dive();
@@ -36,20 +32,13 @@ describe('App', () => {
 
   it('should render the mapview', () => {
     const store = configureMockStore()({
+      ...initialState,
       ui: {
-        isEmbed: false,
-        isEmbedPreview: false,
-        isPrintMode: false,
+        ...initialState.ui,
         viewMode: VIEW_MODE.MAP
-      },
-      map: {
-        view: 'home'
-      },
-      user: {},
-      error: {
-        hassErrors: false
       }
     });
+
     const component = shallow(
       <App />, { context: { store } }
     ).dive();
