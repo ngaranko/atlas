2c2018aafc9383e7076a2bca83014631ef643df0
royderks <10717410+royderks@users.noreply.github.com>
2019-06-26 14:31:11 +0200

    Add util to shorten usernames in the menu

---
+++ src/app/utils/truncateString.js
@@ -0,0 +1,9 @@
+function truncateString(string, char) {
+  if (string.length > char) {
+    return string.replace(new RegExp(`(.{${char}})..+`), '$1…');
+  }
+
+  return string;
+}
+
+export default truncateString;
