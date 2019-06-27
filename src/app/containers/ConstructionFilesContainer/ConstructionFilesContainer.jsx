2544a03db77026f80583052e4a008269f9d27c7f
royderks <10717410+royderks@users.noreply.github.com>
2019-06-27 15:57:18 +0200

    Splitted the container for construction files, added a new detail component with tests

--- src/app/containers/ConstructionFilesContainer/ConstructionFilesContainer.jsx
+++ src/app/containers/ConstructionFilesContainer/ConstructionFilesContainer.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { GridContainer, GridItem, Typography } from '@datapunt/asc-ui';
+import { Typography } from '@datapunt/asc-ui';
 import { getFileName } from '../../../shared/ducks/files/selectors';
 import { getUser } from '../../../shared/ducks/user/user';
 import { SCOPES } from '../../../shared/services/auth/auth';
 import NotAuthorizedMessage from '../../components/PanelMessages/NotAuthorizedMessage';
+import ConstructionFileDetail from '../../components/ConstructionFileDetail/ConstructionFileDetail';
 import SHARED_CONFIG from '../../../shared/services/shared-config/shared-config';
 import { getLocationPayload } from '../../../store/redux-first-router/selectors';
-import Gallery from '../../components/Gallery/Gallery';
 import LoadingIndicator from '../../../shared/components/loading-indicator/LoadingIndicator';
 import ErrorMessage from '../../components/PanelMessages/ErrorMessage/ErrorMessage';
 import { getByUrl } from '../../../shared/services/api/api';
@@ -16,9 +16,7 @@ import './ConstructionFilesContainer.scss';
 import { ConstructionFiles as ContextMenu } from '../../components/ContextMenu';
 import useMatomo from '../../utils/useMatomo';
 import useDocumentTitle from '../../utils/useDocumentTitle';
-import getAddresses from '../../../normalizations/construction-files/getAddresses';
-import getReduxLinkProps from '../../utils/getReduxLinkProps';
-import { toDataDetail } from '../../../store/redux-first-router/actions';
+import withGrid from '../../utils/withGrid';
 
 const ImageViewer = React.lazy(() => import('../../components/ImageViewer/ImageViewer'));
 
@@ -33,13 +31,7 @@ const ConstructionFiles = ({ fileName, user, endpoint }) => {
   const { trackPageView, trackEvent } = useMatomo();
   const { documentTitle, setDocumentTitle } = useDocumentTitle();
 
-  const {
-    titel: title,
-    subdossiers,
-    datering: date,
-    dossier_type: fileType,
-    dossiernr: fileNumber
-  } = results || {};
+  const { titel: title } = results || {};
 
   async function fetchConstructionFiles() {
     setLoading(true);
@@ -79,13 +71,6 @@ const ConstructionFiles = ({ fileName, user, endpoint }) => {
     trackEvent(documentTitle, 'Download-bouwtekening', `bouwtekening-download-${size}`, fileName);
   };
 
-  const withGrid = (children) => (
-    <GridContainer direction="column" gutterX={20} gutterY={20}>
-      <GridItem>
-        {children}
-      </GridItem>
-    </GridContainer>
-  );
   const noResultsTemplate = withGrid(
     <Typography element="em">Geen resultaten gevonden</Typography>
   );
@@ -98,99 +83,6 @@ const ConstructionFiles = ({ fileName, user, endpoint }) => {
     <LoadingIndicator />
   );
 
-  const resultsTemplate = () => (
-    <div className="c-construction-files">
-      {withGrid(
-        <React.Fragment>
-          <Typography
-            className="c-construction-files__subtitle"
-            element="h3"
-          >
-            Bouwdossier
-          </Typography>
-          <Typography
-            className="c-construction-files__title"
-            element="h1"
-          >
-            {title}
-          </Typography>
-        </React.Fragment>
-      )}
-
-      <div className="c-ds-table">
-        <div className="c-ds-table__body">
-          <div className="c-ds-table__row">
-            <div className="c-ds-table__cell">
-              <div className="qa-table-value">Titel</div>
-            </div>
-            <div className="c-ds-table__cell">
-              <div className="qa-table-value">{title}</div>
-            </div>
-          </div>
-          <div className="c-ds-table__row">
-            <div className="c-ds-table__cell">
-              <div className="qa-table-value">Datering</div>
-            </div>
-            <div className="c-ds-table__cell">
-              <div className="qa-table-value">{date}</div>
-            </div>
-          </div>
-          <div className="c-ds-table__row">
-            <div className="c-ds-table__cell">
-              <div className="qa-table-value">Type</div>
-            </div>
-            <div className="c-ds-table__cell">
-              <div className="qa-table-value">{fileType}</div>
-            </div>
-          </div>
-          <div className="c-ds-table__row">
-            <div className="c-ds-table__cell">
-              <div className="qa-table-value">Dossiernummer</div>
-            </div>
-            <div className="c-ds-table__cell">
-              <div className="qa-table-value">{fileNumber}</div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {subdossiers &&
-      subdossiers.length &&
-      subdossiers.map(({ bestanden: files, titel: subdossierTitle }) => (
-        <Gallery
-          id={`${results.stadsdeel}${results.dossiernr}`}
-          key={subdossierTitle}
-          title={subdossierTitle}
-          allThumbnails={files}
-          max={6}
-        />
-      ))}
-      {withGrid(
-        <React.Fragment>
-          <Typography
-            className="c-construction-files__subtitle"
-            element="h3"
-          >
-            Adressen
-          </Typography>
-          <ul className="o-list">
-            {getAddresses(results).map((address) => (
-              <li key={address.id}>
-                <a
-                  {...getReduxLinkProps(toDataDetail([address.id, 'bag', 'nummeraanduiding']))}
-                  className="o-btn o-btn--link qa-dp-link"
-                  title={address.label}
-                >
-                  {address.label}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </React.Fragment>
-      )}
-    </div>
-  );
-
   return user.scopes.includes(SCOPES['BD/R'])
     ? errorMessage ? <ErrorMessage errorMessage={errorMessage} /> : (
       <React.Fragment>
@@ -200,7 +92,7 @@ const ConstructionFiles = ({ fileName, user, endpoint }) => {
           contextMenu={<ContextMenu onDownload={onDownloadFile} fileName={fileName} />}
         />}
         {loading && loadingTemplate}
-        {(!loading && !fileName) && (results ? resultsTemplate() : noResultsTemplate)}
+        {(!loading && !fileName) && (results ? <ConstructionFileDetail results={results} /> : noResultsTemplate)}
       </React.Fragment>)
     : notAuthorizedTemplate;
 };
