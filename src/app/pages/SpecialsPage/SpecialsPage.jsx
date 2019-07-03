c2d2118d6939d3902d097ff0604199e1bf0571a3
royderks <10717410+royderks@users.noreply.github.com>
2019-07-03 14:42:13 +0200

    Created script to check for event handlers on a child iframe, TODO: integrate resize onload in iframe, add grid system

--- src/app/pages/SpecialsPage/SpecialsPage.jsx
+++ src/app/pages/SpecialsPage/SpecialsPage.jsx
@@ -1,79 +1,72 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { iframeResizer } from 'iframe-resizer'
+import throttle from 'lodash.throttle'
+import { Spinner } from '@datapunt/asc-ui'
 import SHARED_CONFIG from '../../../shared/services/shared-config/shared-config'
 import { getLocationPayload } from '../../../store/redux-first-router/selectors'
 import useDataFetching from '../../utils/useDataFetching'
 
 import './SpecialsPage.scss'
 
-const iframeResizerOptions = { checkOrigin: false }
-
 const SpecialsContainer = ({ endpoint }) => {
-  const { fetchData, loading, errorMessage, results } = useDataFetching()
+  const { fetchData, results, loading } = useDataFetching()
   const [iFrameLoading, setIFrameLoading] = React.useState(true)
+  const [iFrameHeight, setIFrameHeight] = React.useState(0)
   const iFrameRef = React.useRef(null)
-  const iFrameContainerRef = React.useRef(null)
 
-  let source
-  if (results) {
-    source = results.data[0].attributes.field_iframe_link.uri
-  }
-
-  React.useEffect(() => {
-    fetchData(endpoint)
-  }, [])
-
-  React.useEffect(() => {
-    // iframeResizer({
-    //   heightCalculationMethod: 'bodyScroll',
-    //   log: true,
-    //   interval:32, maxHeight:1/0,maxWidth:1/0,minHeight:0,minWidth:0,resizeFrom:"parent",scrolling:!1,sizeHeight: '100%',sizeWidth:!1,warningTimeout:5e3,tolerance:0,widthCalculationMethod:"scroll"
-    // }, iFrameRef.current);
-    setIFrameLoading(false)
-
-    const eventMethod = window.addEventListener ? 'addEventListener' : 'attachEvent'
-    const eventer = window[eventMethod]
-    const messageEvent = eventMethod === 'attachEvent' ? 'onmessage' : 'message'
-
-    // Listen for event
-    eventer(
-      messageEvent,
-      function(e) {
-        // Check that message being passed is the documentHeight
+  const handleResize = () =>
+    window.addEventListener(
+      'message',
+      e => {
         if (typeof e.data === 'string' && e.data.indexOf('documentHeight:') > -1) {
-          // Split string from identifier
           const height = e.data.split('documentHeight:')[1]
-
-          // do stuff with the height
-          iFrameRef.current.height = `${height}px`;
+          setIFrameHeight(height)
         }
       },
       false,
     )
-  }, [source])
 
-  if (iFrameLoading) {
-    return <div>loading...</div>
+  const throttledHandleResize = () => {
+    throttle(handleResize, 300)
   }
 
-  // console.log(iFrameRef.current)
+  React.useEffect(() => {
+    fetchData(endpoint)
 
-  // if (iFrameRef.current) {
-  //   console.log(iFrameRef.current.contentWindow.document.body.clientHeight, iFrameRef.current.contentWindow.document.body.scrollHeight)
-  //   iFrameRef.current.height = `${document.body.offsetHeight  }px`;
-  //   iFrameContainerRef.current.height = iFrameRef.current.height;
+    window.addEventListener('resize', throttledHandleResize)
 
-  //   console.log(document.body.scrollHeight, iFrameContainerRef.current.height)
-  // }
+    return function cleanup() {
+      window.removeEventListener('resize', throttledHandleResize)
+    }
+  }, [])
+
+  React.useEffect(() => {
+    if (iFrameRef.current) {
+      iFrameRef.current.height = `${iFrameHeight}px`
+    }
+  }, [iFrameHeight])
+
+  const iFrameLoaded = () => {
+    setIFrameLoading(false)
+    // Handle resize when the iframe is loaded
+    // handleResize()
+  }
+
+  const { field_iframe_link: iFrameLink, title } = results ? results.data[0].attributes : {}
 
   return (
-    <div ref={iFrameContainerRef} className="iframe-container c-dashboard__page o-max-width">
-      {iFrameLoading && <div>loading...</div>}
-      {source && !iFrameLoading && (
+    <div className="iframe-container c-dashboard__page o-max-width">
+      {iFrameLoading && (
+        <div className="loading-indicator">
+          <Spinner size={100} color="secondary" />
+        </div>
+      )}
+      {!loading && iFrameLink && (
         <iframe
-          src="http://demo.bitsofco.de/responsive-iframe/iframe.html"
+          src={iFrameLink.uri}
+          title={title}
           ref={iFrameRef}
+          onLoad={iFrameLoaded}
           width="100%"
           height="100%"
           frameBorder="0"
