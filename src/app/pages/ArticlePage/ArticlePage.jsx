3776a73682088dbfbbdfc007d5170a1c0138af75
Chris van Mook <chris@chrisvanmook.com>
2019-07-23 17:21:12 +0200

    Removed Row from BlogPost

--- src/app/pages/ArticlePage/ArticlePage.jsx
+++ src/app/pages/ArticlePage/ArticlePage.jsx
@@ -15,10 +15,8 @@ import {
 } from '@datapunt/asc-ui'
 import React from 'react'
 import { connect } from 'react-redux'
-import LoadingIndicator from '../../../shared/components/loading-indicator/LoadingIndicator'
 import SHARED_CONFIG from '../../../shared/services/shared-config/shared-config'
 import { getLocationPayload } from '../../../store/redux-first-router/selectors'
-import Footer from '../../components/Footer/Footer'
 import useFromCMS from '../../utils/useFromCMS'
 import './ArticlePage.scss'
 import BlogPage from '../../components/BlogPage/BlogPage'
@@ -52,95 +50,93 @@ import BlogPage from '../../components/BlogPage/BlogPage'
   } = results || {}
 
   return (
-    !loading && (
-      <BlogPage {...{ id, slug, title, loading }}>
-        {!loading && (
-          <div className="article">
-            <Row className="article__row">
-              <Article
-                {...(coverUrl
-                  ? {
-                      image:
-                        typeof coverUrl === 'string'
-                          ? `${SHARED_CONFIG.CMS_ROOT}${coverUrl}`
-                          : undefined,
-                    }
-                  : {})}
-              >
-                <Row className="article__row">
-                  <BlogContent>
+    <BlogPage {...{ id, slug, title, loading }}>
+      {!loading && (
+        <div className="article">
+          <Row className="article__row">
+            <Article
+              {...(coverUrl
+                ? {
+                    image:
+                      typeof coverUrl === 'string'
+                        ? `${SHARED_CONFIG.CMS_ROOT}${coverUrl}`
+                        : undefined,
+                  }
+                : {})}
+            >
+              <Row className="article__row">
+                <BlogContent>
+                  <Column
+                    wrap
+                    span={{ small: 1, medium: 2, big: 5, large: 11, xLarge: 11 }}
+                    push={{ small: 0, medium: 0, big: 1, large: 1, xLarge: 1 }}
+                  >
+                    <Column span={{ small: 1, medium: 2, big: 4, large: 7, xLarge: 7 }}>
+                      <BlogBody>
+                        <BlogHeader title={title}>
+                          <BlogMetaList
+                            dateTime={date}
+                            dateFormatted={localeDate}
+                            fields={byline && [{ id: 1, label: byline }]}
+                          />
+                        </BlogHeader>
+                        <Paragraph strong hasLongText>
+                          {intro}
+                        </Paragraph>
+                        <CustomHTMLBlock body={body} />
+                      </BlogBody>
+                    </Column>
                     <Column
-                      wrap
-                      span={{ small: 1, medium: 2, big: 5, large: 11, xLarge: 11 }}
+                      span={{ small: 1, medium: 2, big: 2, large: 3, xLarge: 3 }}
                       push={{ small: 0, medium: 0, big: 1, large: 1, xLarge: 1 }}
                     >
-                      <Column span={{ small: 1, medium: 2, big: 4, large: 7, xLarge: 7 }}>
-                        <BlogBody>
-                          <BlogHeader title={title}>
-                            <BlogMetaList
-                              dateTime={date}
-                              dateFormatted={localeDate}
-                              fields={byline && [{ id: 1, label: byline }]}
-                            />
-                          </BlogHeader>
-                          <Paragraph strong hasLongText>
-                            {intro}
-                          </Paragraph>
-                          <CustomHTMLBlock body={body} />
-                        </BlogBody>
-                      </Column>
-                      <Column
-                        span={{ small: 1, medium: 2, big: 2, large: 3, xLarge: 3 }}
-                        push={{ small: 0, medium: 0, big: 1, large: 1, xLarge: 1 }}
-                      >
-                        <BlogSidebar>
-                          {downloads && downloads.length ? (
-                            <>
-                              <Heading as="h2">Downloads</Heading>
-                              <LinkList>
-                                {downloads.map(
-                                  ({
-                                    title: fileTitle,
-                                    drupal_internal__nid: key,
-                                    field_file_type: type,
-                                    field_file_size: size,
-                                    field_publication_file: file,
-                                  }) => (
-                                    <LinkListItem
-                                      key={key}
-                                      fileInfo={`${type} ${size}`}
-                                      href={`${SHARED_CONFIG.CMS_ROOT}${file.uri.url}`}
-                                    >
-                                      {fileTitle}
-                                    </LinkListItem>
-                                  ),
-                                )}
-                              </LinkList>
-                            </>
-                          ) : null}
-                          {links && links.length ? (
-                            <>
-                              <Heading as="h2">Links</Heading>
-                              <LinkList>
-                                {links.map(({ uri, title: linkTitle }) => (
-                                  <LinkListItem key={uri} href={`${uri}`}>
-                                    {linkTitle}
+                      <BlogSidebar>
+                        {downloads && downloads.length ? (
+                          <>
+                            <Heading as="h2">Downloads</Heading>
+                            <LinkList>
+                              {downloads.map(
+                                ({
+                                  title: fileTitle,
+                                  drupal_internal__nid: key,
+                                  field_file_type: type,
+                                  field_file_size: size,
+                                  field_publication_file: file,
+                                }) => (
+                                  <LinkListItem
+                                    key={key}
+                                    fileInfo={`${type} ${size}`}
+                                    href={`${SHARED_CONFIG.CMS_ROOT}${file.uri.url}`}
+                                  >
+                                    {fileTitle}
                                   </LinkListItem>
-                                ))}
-                              </LinkList>
-                            </>
-                          ) : null}
-                        </BlogSidebar>
-                      </Column>
+                                ),
+                              )}
+                            </LinkList>
+                          </>
+                        ) : null}
+                        {links && links.length ? (
+                          <>
+                            <Heading as="h2">Links</Heading>
+                            <LinkList>
+                              {links.map(({ uri, title: linkTitle }) => (
+                                <LinkListItem key={uri} href={`${uri}`}>
+                                  {linkTitle}
+                                </LinkListItem>
+                              ))}
+                            </LinkList>
+                          </>
+                        ) : null}
+                      </BlogSidebar>
                     </Column>
-                  </BlogContent>
-                </Row>
-              </Article>
-            </Row>
-          </div>
-        )}
-      </BlogPage>
-    )
+                  </Column>
+                </BlogContent>
+              </Row>
+            </Article>
+          </Row>
+        </div>
+      )}
+    </BlogPage>
   )
 }
 
