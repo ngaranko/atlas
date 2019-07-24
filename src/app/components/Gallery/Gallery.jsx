e2729c53f55b2643510c258b2af60cd2e3b40133
royderks <10717410+royderks@users.noreply.github.com>
2019-07-24 14:58:53 +0200

    Fixed deprecated use of <Typography /> in the construction files

--- src/app/components/Gallery/Gallery.jsx
+++ src/app/components/Gallery/Gallery.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
-import { GridContainer, GridItem, Icon, Typography } from '@datapunt/asc-ui'
+import { GridContainer, GridItem, Icon, Heading } from '@datapunt/asc-ui'
 import { Minimise, Enlarge } from '@datapunt/asc-assets'
 import Thumbnail from '../Thumbnail/Thumbnail'
 import './Gallery.scss'
@@ -16,9 +16,9 @@ const Gallery = ({ title, allThumbnails, id, maxLength }) => {
     <React.Fragment>
       <GridContainer className="c-gallery" key={title} direction="column" gutterX={20}>
         <GridItem>
-          <Typography className="c-gallery__title" element="h3">
+          <Heading color="secondary" className="c-gallery__title" as="h3">
             {title} {allThumbnails.length > maxLength && `(${allThumbnails.length})`}
-          </Typography>
+          </Heading>
           {thumbnails && thumbnails.length ? (
             <React.Fragment>
               <GridContainer as="ul" wrap="wrap" gutterY={7.5} gutterX={7.5} collapse>
@@ -85,7 +85,7 @@ const Gallery = ({ title, allThumbnails, id, maxLength }) => {
                 ))}
             </React.Fragment>
           ) : (
-            <Typography element="em">Geen bouwtekening(en) beschikbaar.</Typography>
+            <Heading as="em">Geen bouwtekening(en) beschikbaar.</Heading>
           )}
         </GridItem>
       </GridContainer>
