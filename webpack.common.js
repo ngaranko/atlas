77e860a58ccc85cefca25a2d99cf740df7c12585
Chris van Mook <chris@chrisvanmook.com>
2019-07-31 10:47:45 +0200

    Updated faulty eslint ignores in webpack files

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,3 @@
-/* eslint-disable global-require,import/no-extraneous-dependencies */
 const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
