0372489c970dc5228433cd5c712c63698bef8081
royderks <10717410+royderks@users.noreply.github.com>
2019-08-06 14:57:09 +0200

    Added a test scenario for this bug

---
+++ src/app/pages/MapSplitPage/MapSplitPage.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import configureMockStore from 'redux-mock-store'
+import MapSplitPage from './MapSplitPage'
+
+describe('MapSplitPage', () => {
+  it('should force the fullscreen view mode for detail pages without geometry', () => {
+    const store = configureMockStore()({
+      ui: { viewMode: 'kaart', isPrintMode: false, isEmbed: false, isEmbedPreview: false },
+      map: { geometry: [] }, // detail pages without geometry only have a fullscreen viewMode
+      location: { type: 'atlasRouter/DATA_DETAIL' },
+      selection: { type: 'NONE' },
+    })
+
+    const component = shallow(<MapSplitPage />, { context: { store } })
+
+    // the shouldShowFullScreen selected shpuld return a value for the forceFullScreen prop
+    expect(component.props().forceFullScreen).toBe(true)
+  })
+})
