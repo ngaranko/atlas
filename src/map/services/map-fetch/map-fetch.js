62c45969c469566b3d52b45fa82eee6b93c7f830
royderks <10717410+royderks@users.noreply.github.com>
2019-08-28 14:56:41 +0200

    Inluded some more tests

--- src/map/services/map-fetch/map-fetch.js
+++ src/map/services/map-fetch/map-fetch.js
@@ -3,20 +3,27 @@ import { rdToWgs84 } from '../../../shared/services/coordinate-reference-system/
 
 import { getByUrl } from '../../../shared/services/api/api'
 
-export default function fetchByUri(uri, normalization = params => params) {
-  return getByUrl(uri).then(async result => {
-    const normalizedData = await normalization(result)
+export default async function fetchByUri(uri, normalization = false) {
+  const result = await getByUrl(uri)
 
-    const geometry = result.geometry || normalizedData.geometry
+  // as some APIs return data in a different format than the frontend wants to display
+  // some API results are normalized or even enhanced with results from additional API calls
+  let normalizedData = false
+  if (normalization) {
+    normalizedData = await normalization(result)
+  }
 
-    const geometryCenter = geometry && getCenter(geometry)
-    const wgs84Center = geometryCenter ? rdToWgs84(geometryCenter) : null
+  // "geometrie" is often returned from the instance API, "gemoetry" is created by the normalizers
+  const geometry = result.geometrie || result.geometry || normalizedData.geometry
 
-    return {
-      label: result._display,
-      ...(normalizedData || result),
-      location: result.location || wgs84Center,
-      geometrie: geometry,
-    }
-  })
+  const geometryCenter = geometry && getCenter(geometry)
+  const wgs84Center = geometryCenter ? rdToWgs84(geometryCenter) : null
+
+  return {
+    label: result._display,
+    ...(normalizedData || result),
+    // "label" may be overwritten by the result or normalizedData, but the location and geometry are constructed above
+    location: result.location || wgs84Center,
+    geometrie: geometry,
+  }
 }
