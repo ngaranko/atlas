0e4f217e564b6745eac4b447485b96e6a317ebce
royderks <10717410+royderks@users.noreply.github.com>
2019-09-02 18:22:51 +0200

    Moved the config of the map to the public directory, added meta information to this configuration

--- src/map/components/leaflet/MapLeaflet.jsx
+++ src/map/components/leaflet/MapLeaflet.jsx
@@ -18,7 +18,7 @@ import { boundsToString, getBounds, isBoundsAPoint, isValidBounds } from './serv
 import LoadingIndicator from '../loading-indicator/LoadingIndicator'
 import { DEFAULT_LAT, DEFAULT_LNG } from '../../ducks/map/constants'
 import RdGeoJson from './custom/geo-json/RdGeoJson'
-import mapLayerTypes from '../../services/map-layers/map-layer-types.config'
+import { mapLayerTypes } from '../../services/map-layers/map-layers.config'
 
 const isIE = false || !!window.document.documentMode
 if (isIE) {
