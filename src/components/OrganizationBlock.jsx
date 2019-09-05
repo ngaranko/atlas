82d280a4923edb11dc190901177f19a46d581795
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-09-05 13:45:55 +0200

    DP-7094 fixed width on loading for OrganizationBlock

--- src/components/OrganizationBlock.jsx
+++ src/components/OrganizationBlock.jsx
@@ -19,6 +19,7 @@ const StyledCardContainer = styled(CardContainer)`
 `
 
 const OrganizationBlockStyle = styled.div`
+  width: 100%;
   margin-top: 40px;
   padding: 40px 20px 0;
   background-color: ${themeColor('tint', 'level2')};
