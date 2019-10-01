d6478b491b4710d745c7f908fcd3a3a072f3da9b
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-10-01 14:00:28 +0200

    DP-7205 replaced dutch words from variables

--- src/app/components/Footer/services/footer-links.js
+++ src/app/components/Footer/services/footer-links.js
@@ -1,17 +1,17 @@
 import {
   cmsIds,
-  OVER_OIS,
+  ABOUT_OIS,
   RESOURCES,
-  AMSTERDAM_EN_DATA,
+  AMSTERDAM_AND_DATA,
   RESEARCH_BY_OIS,
-  OVER_SITE,
+  ABOUT_SITE,
 } from '../../../../shared/config/cms.config'
 
 export const OVER_OIS_LINKS = [
   {
     title: 'Onderzoek, Informatie en Statistiek',
-    id: cmsIds[OVER_OIS],
-    slug: OVER_OIS,
+    id: cmsIds[ABOUT_OIS],
+    slug: ABOUT_OIS,
   },
   { title: 'Onderzoek', id: cmsIds[RESEARCH_BY_OIS], slug: RESEARCH_BY_OIS },
 ]
@@ -19,8 +19,8 @@ export const OVER_OIS_LINKS = [
 export const OVER_DATA_LINKS = [
   {
     title: 'Databeleid',
-    id: cmsIds[AMSTERDAM_EN_DATA],
-    slug: AMSTERDAM_EN_DATA,
+    id: cmsIds[AMSTERDAM_AND_DATA],
+    slug: AMSTERDAM_AND_DATA,
   },
   { title: 'Bronnen', id: cmsIds[RESOURCES], slug: RESOURCES },
 ]
@@ -28,10 +28,10 @@ export const OVER_DATA_LINKS = [
 export const OVER_DEZE_SITE_LINKS = [
   {
     title: 'Over deze site',
-    id: cmsIds[OVER_SITE],
-    slug: OVER_SITE,
+    id: cmsIds[ABOUT_SITE],
+    slug: ABOUT_SITE,
   },
-  { title: 'Over OIS', id: cmsIds[OVER_OIS], slug: OVER_OIS },
+  { title: 'Over OIS', id: cmsIds[ABOUT_OIS], slug: ABOUT_OIS },
 ]
 
 export const OIS_LINKS = [...OVER_OIS_LINKS, ...OVER_DATA_LINKS]
