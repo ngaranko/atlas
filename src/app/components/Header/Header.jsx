b4b4b61215c6a62c4f2d120457f148542209ec80
Chris van Mook <chris@chrisvanmook.com>
2019-10-02 11:28:21 +0200

    Temporary changed header title

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -69,7 +69,7 @@ const Header = ({
       <HeaderWrapper data-test="header">
         <StyledHeader
           tall={homePage}
-          title="Data en informatie"
+          title="City Data"
           homeLink="/"
           className="styled-header"
           fullWidth={!hasMaxWidth}
