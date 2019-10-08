6ca2bc32fcdd365fa75f977875fbb0af62ee4cb6
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-10-08 11:50:17 +0200

    DP-7336 removed google sheets references

--- modules/page/page-config.constant.js
+++ modules/page/page-config.constant.js
@@ -1,15 +1,7 @@
 ;(function() {
   angular.module('dpPage').constant('PAGE_NAMES', {
-    home: 'Home',
     metadata: 'Actualiteit',
-    news: 'Nieuws',
-    beleid: 'Beleid',
-    help: 'Help',
-    snelwegwijs: 'Bediening portaal',
-    apis: "API's gebruiken",
-    proclaimer: 'Proclaimer',
     inloggen: 'Na inloggen',
-    statistieken: 'Statistieken',
     verplaatst: 'Pagina verplaatst',
     nietgevonden: 'Niet gevonden',
   })
