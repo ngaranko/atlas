4daa71491902cb86d2bf0fb0265ec2a1723c0c2b
royderks <10717410+royderks@users.noreply.github.com>
2019-11-22 12:49:45 +0100

    reset the viewport when the users moves to pages that dont incorporate the grid

--- src/app/pages/EditorialOverviewPage/EditorialOverviewPage.jsx
+++ src/app/pages/EditorialOverviewPage/EditorialOverviewPage.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable camelcase */
 import React from 'react'
-import Helmet from 'react-helmet'
 import { Column, Container, Row } from '@datapunt/asc-ui'
 import useFromCMS from '../../utils/useFromCMS'
 import { cmsConfig } from '../../../shared/config/config'
@@ -28,12 +27,6 @@ const EditorialOverviewPage = ({ type = '' }) => {
 
   return (
     <Container>
-      <Helmet>
-        <meta
-          name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-        />
-      </Helmet>
       <Row>
         <ContentContainer>
           <Column
