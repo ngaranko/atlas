b152af14c85160fcbbc09af5e02acf715e3ab225
royderks <10717410+royderks@users.noreply.github.com>
2019-12-09 18:08:31 +0100

    First draft for the datacards on the common search page

--- src/app/pages/SearchPage/DataSearchResults.jsx
+++ src/app/pages/SearchPage/DataSearchResults.jsx
@@ -1,47 +1,74 @@
-import { Heading, Link, themeColor, themeSpacing } from '@datapunt/asc-ui'
+import {
+  Heading,
+  Link,
+  themeColor,
+  themeSpacing,
+  Card,
+  CardContent,
+  Paragraph,
+  LinkList,
+} from '@datapunt/asc-ui'
 import styled from '@datapunt/asc-core'
 import RouterLink from 'redux-first-router-link'
 import React from 'react'
 import { toDataDetail } from '../../../store/redux-first-router/actions'
 
-const Divider = styled.div`
-  width: 200px;
-  height: 3px;
-  background-color: ${themeColor('secondary')};
-  margin: ${themeSpacing(8, 0, 6)};
-`
-
 const StyledLink = styled(Link)`
   margin: ${themeSpacing(2, 0)};
 `
 
-/**
- * Todo: loading state
- * @param results
- * @returns {*}
- */
+const StyledCard = styled(Card)`
+  width: inherit;
+  border: ${themeColor('tint', 'level3')} 1px solid;
+`
+
+const StyledHeading = styled(Heading)``
+
+const StyledCardContent = styled(CardContent)`
+  padding: ${themeSpacing(2)};
+  min-height: inherit;
+`
+
+const StyledParagraph = styled(Paragraph)`
+  white-space: nowrap;
+  overflow: hidden;
+  display: block;
+  text-overflow: ellipsis;
+`
+
+const StyledParagraphLink = styled(Link)`
+  font-weight: normal;
+  color: inherit;
+`
 
 export default ({ results }) => {
   if (results.length) {
     return results.map(result =>
       result.results && result.results.length ? (
-        <div key={result.type}>
-          <Divider />
-          <Heading $as="h2">{result.label}</Heading>
-          <ul>
-            {result.results.map(location => (
-              <li key={location.id}>
-                <StyledLink
-                  to={toDataDetail([location.id, 'bag', 'nummeraanduiding'])}
-                  $as={RouterLink}
-                  variant="with-chevron"
-                >
-                  {location.label}
-                </StyledLink>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <StyledCard key={result.type} horizontal>
+          <StyledCardContent>
+            <div>
+              <StyledHeading $as="h4">{`${result.label} (${result.count})`}</StyledHeading>
+              {console.log(result)}
+            </div>
+
+            <div>
+              <StyledParagraph>
+                {result.results.map((location, index) => (
+                  <>
+                    <StyledParagraphLink
+                      to={toDataDetail([location.id, 'bag', 'nummeraanduiding'])}
+                      $as={RouterLink}
+                    >
+                      {location.label}
+                    </StyledParagraphLink>
+                    {index !== result.results.length - 1 ? `, ` : ''}
+                  </>
+                ))}
+              </StyledParagraph>
+            </div>
+          </StyledCardContent>
+        </StyledCard>
       ) : null,
     )
   }
