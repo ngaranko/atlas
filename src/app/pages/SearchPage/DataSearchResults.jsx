ada0706a894c4859eef4ec4bd7ff6ddcbeb54d07
royderks <10717410+royderks@users.noreply.github.com>
2019-12-10 13:23:05 +0100

    Created a card for the data search types in the complete overview, TODO: return correct types from the GraphQL api and add the correct SVG icons

--- src/app/pages/SearchPage/DataSearchResults.jsx
+++ src/app/pages/SearchPage/DataSearchResults.jsx
@@ -1,75 +1,10 @@
-import {
-  Heading,
-  Link,
-  themeColor,
-  themeSpacing,
-  Card,
-  CardContent,
-  Paragraph,
-  LinkList,
-} from '@datapunt/asc-ui'
-import styled from '@datapunt/asc-core'
-import RouterLink from 'redux-first-router-link'
 import React from 'react'
-import { toDataDetail } from '../../../store/redux-first-router/actions'
-
-const StyledLink = styled(Link)`
-  margin: ${themeSpacing(2, 0)};
-`
-
-const StyledCard = styled(Card)`
-  width: inherit;
-  border: ${themeColor('tint', 'level3')} 1px solid;
-`
-
-const StyledHeading = styled(Heading)``
-
-const StyledCardContent = styled(CardContent)`
-  padding: ${themeSpacing(2)};
-  min-height: inherit;
-`
-
-const StyledParagraph = styled(Paragraph)`
-  white-space: nowrap;
-  overflow: hidden;
-  display: block;
-  text-overflow: ellipsis;
-`
-
-const StyledParagraphLink = styled(Link)`
-  font-weight: normal;
-  color: inherit;
-`
+import DataCard from '../../components/DataCard'
 
 export default ({ results }) => {
   if (results.length) {
     return results.map(result =>
-      result.results && result.results.length ? (
-        <StyledCard key={result.type} horizontal>
-          <StyledCardContent>
-            <div>
-              <StyledHeading $as="h4">{`${result.label} (${result.count})`}</StyledHeading>
-              {console.log(result)}
-            </div>
-
-            <div>
-              <StyledParagraph>
-                {result.results.map((location, index) => (
-                  <>
-                    <StyledParagraphLink
-                      to={toDataDetail([location.id, 'bag', 'nummeraanduiding'])}
-                      $as={RouterLink}
-                    >
-                      {location.label}
-                    </StyledParagraphLink>
-                    {index !== result.results.length - 1 ? `, ` : ''}
-                  </>
-                ))}
-              </StyledParagraph>
-            </div>
-          </StyledCardContent>
-        </StyledCard>
-      ) : null,
+      result.results && result.results.length ? <DataCard {...{ ...result }} /> : null,
     )
   }
   return null
