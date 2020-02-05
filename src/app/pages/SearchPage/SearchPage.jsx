dc539d28068128466ec3a449dd2946d4d872fe16
royderks <10717410+royderks@users.noreply.github.com>
2020-02-05 13:11:01 +0100

    Restore rebase error

--- src/app/pages/SearchPage/SearchPage.jsx
+++ src/app/pages/SearchPage/SearchPage.jsx
@@ -1,25 +1,7 @@
 import React, { memo, useEffect, useState } from 'react'
 import { useMatomo } from '@datapunt/matomo-tracker-react'
 import { clearAllBodyScrollLocks, enableBodyScroll } from 'body-scroll-lock'
-<<<<<<< HEAD
-import {
-  breakpoint,
-  Column,
-  Container,
-  Row,
-  Modal,
-  themeSpacing,
-  TopBar,
-  Button,
-  Heading,
-  Spinner,
-  themeColor,
-} from '@datapunt/asc-ui'
-import styled from '@datapunt/asc-core'
-import { Close } from '@datapunt/asc-assets'
-=======
 import { Container, Row } from '@datapunt/asc-ui'
->>>>>>> Add pagination to the searchpages, deleted unused filed, refactored search results to be less confusing
 import ContentContainer from '../../components/ContentContainer/ContentContainer'
 import SEARCH_PAGE_CONFIG, {
   DEFAULT_LIMIT,
@@ -34,66 +16,6 @@ import useCompare from '../../utils/useCompare'
 import useSelectors from '../../utils/useSelectors'
 import { getActiveFilters, getSort, getPage } from './SearchPageDucks'
 import useDocumentTitle from '../../utils/useDocumentTitle'
-<<<<<<< HEAD
-
-const FilterColumn = styled(Column)`
-  align-content: flex-start;
-
-  @media screen and ${breakpoint('max-width', 'laptop')} {
-    display: none;
-  }
-`
-
-const ModalCloseButton = styled(Button)`
-  margin-left: auto;
-`
-
-const StyledModal = styled(Modal)`
-  ${FilterColumn} {
-    top: 0;
-    width: 100%;
-    display: block;
-    overflow-y: scroll;
-    left: 0;
-    right: ${themeSpacing(5)};
-    padding: ${themeSpacing(5)};
-    flex-grow: 1;
-  }
-
-  [role='dialog'] {
-    max-width: calc(400px + ${themeSpacing(12)});
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    position: absolute;
-    max-height: 100%;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    pointer-events: all;
-    transform: initial;
-  }
-`
-
-const ApplyFilters = styled.div`
-  width: 100%;
-  display: flex;
-  flex-shrink: 0;
-  justify-content: center;
-  padding: ${themeSpacing(4)};
-  background-color: #fff;
-  box-shadow: 0 0 ${themeSpacing(2)} 1px black;
-  margin-top: auto;
-  position: relative; // No idea why, but this will make sure the box-shadow overlaps the borders of the sibling components
-`
-
-const ApplyFiltersButton = styled(Button)`
-  width: 100%;
-  display: flex;
-  text-align: center;
-  justify-content: center;
-`
-=======
 import usePagination from './usePagination'
 
 function getSortIntput(sortString) {
@@ -107,11 +29,6 @@ function getSortIntput(sortString) {
   }
   return sort
 }
->>>>>>> Add pagination to the searchpages, deleted unused filed, refactored search results to be less confusing
-
-const StyledTopBar = styled(TopBar)`
-  border-bottom: 2px solid ${themeColor('tint', 'level3')};
-`
 
 /* TODO: Write tests for the Hooks used in this component */
 /* istanbul ignore next */
@@ -139,33 +56,8 @@ const SearchPage = ({ isOverviewPage, currentPage, query }) => {
     }
   }, [documentTitle])
 
-<<<<<<< HEAD
-  // Hide filter when orientation changes to prevent layout issues.
-  useEffect(() => {
-    function onOrientationChange() {
-      setShowFilter(false)
-    }
-
-    window.addEventListener('orientationchange', onOrientationChange)
-
-    return () => window.removeEventListener('orientationchange', onOrientationChange)
-  }, [])
-
-  const {
-    fetching,
-    errors,
-    totalCount,
-    fetchingMore,
-    results,
-    filters,
-    fetchMore,
-    hasMore,
-  } = usePagination(
-    SEARCH_PAGE_CONFIG[currentPage],
-=======
   const { fetching, errors, totalCount, filters, results, pageInfo } = usePagination(
     SEARCH_PAGE_CONFIG[currentPage].query,
->>>>>>> Add pagination to the searchpages, deleted unused filed, refactored search results to be less confusing
     {
       q: query,
       page: withPagination ? page : null, // In case the pagination doesn't gets deleted when changing routes
@@ -208,39 +100,6 @@ const SearchPage = ({ isOverviewPage, currentPage, query }) => {
     <Container>
       <ContentContainer>
         <Row>
-<<<<<<< HEAD
-          {!showFilter && Filters}
-          <StyledModal
-            aria-labelledby="filters"
-            aria-describedby="filters"
-            open={showFilter}
-            onClose={() => setShowFilter(false)}
-            hideOverFlow={false}
-            zIndexOffset={1}
-          >
-            <StyledTopBar>
-              <Heading style={{ flexGrow: 1 }} as="h4">
-                <ModalCloseButton
-                  variant="blank"
-                  type="button"
-                  size={30}
-                  onClick={() => setShowFilter(false)}
-                  icon={<Close />}
-                />
-              </Heading>
-            </StyledTopBar>
-            {Filters}
-            <ApplyFilters>
-              <ApplyFiltersButton
-                iconLeft={fetching && <Spinner />}
-                onClick={() => setShowFilter(false)}
-                variant="primary"
-              >
-                {!fetching && `${totalCount} resultaten tonen`}
-              </ApplyFiltersButton>
-            </ApplyFilters>
-          </StyledModal>
-=======
           <SearchPageFilters
             filters={filters}
             isOverviewPage={isOverviewPage}
@@ -253,7 +112,6 @@ const SearchPage = ({ isOverviewPage, currentPage, query }) => {
             showFilter={showFilter}
           />
 
->>>>>>> Add pagination to the searchpages, deleted unused filed, refactored search results to be less confusing
           <SearchPageResults
             {...{
               query,
