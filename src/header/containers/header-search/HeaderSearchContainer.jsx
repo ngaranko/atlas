ab2cc4f271d83d1f79f3df6eecd7f161a5de43df
Jon Koops <jon@openweb.nl>
2020-03-02 14:04:55 +0100

    Fix route preservation when search query changes

--- src/header/containers/header-search/HeaderSearchContainer.jsx
+++ src/header/containers/header-search/HeaderSearchContainer.jsx
@@ -1,6 +1,28 @@
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-
+import { TYPES } from '../../../shared/config/cms.config'
+import { emptyFilters } from '../../../shared/ducks/filters/filters'
+import { getViewMode, isMapPage } from '../../../shared/ducks/ui/ui'
+import PARAMETERS from '../../../store/parameters'
+import {
+  toArticleDetail,
+  toArticleSearch,
+  toDataSearch,
+  toDatasetDetail,
+  toDatasetSearch,
+  toDataSuggestion,
+  toPublicationDetail,
+  toPublicationSearch,
+  toSearch,
+  toSpecialSearch,
+} from '../../../store/redux-first-router/actions'
+import {
+  isArticlePage,
+  isDataPage,
+  isDatasetPage,
+  isPublicationPage,
+  isSpecialPage,
+} from '../../../store/redux-first-router/selectors'
 import {
   getActiveSuggestions,
   getAutoSuggestSuggestions,
@@ -10,34 +32,16 @@ import {
   getTypedQuery,
   setActiveSuggestionAction,
 } from '../../ducks/auto-suggest/auto-suggest'
-import { emptyFilters } from '../../../shared/ducks/filters/filters'
-import {
-  toArticleDetail,
-  toDataSearch,
-  toDatasetDetail,
-  toDatasetSearch,
-  toArticleSearch,
-  toPublicationSearch,
-  toSearch,
-  toDataSuggestion,
-  toPublicationDetail,
-} from '../../../store/redux-first-router/actions'
-import {
-  isDatasetPage,
-  isArticlePage,
-  isPublicationPage,
-} from '../../../store/redux-first-router/selectors'
-import PARAMETERS from '../../../store/parameters'
-import { getViewMode, isMapPage } from '../../../shared/ducks/ui/ui'
 import HeaderSearch from './HeaderSearch'
-import { TYPES } from '../../../shared/config/cms.config'
 
 const mapStateToProps = state => ({
   activeSuggestion: getActiveSuggestions(state),
   displayQuery: getDisplayQuery(state),
+  isDataPage: isDataPage(state),
   isDatasetPage: isDatasetPage(state),
   isArticlePage: isArticlePage(state),
   isPublicationPage: isPublicationPage(state),
+  isSpecialsPage: isSpecialPage(state),
   view: getViewMode(state),
   isMapActive: isMapPage(state),
   numberOfSuggestions: getNumberOfSuggestions(state),
@@ -104,6 +108,16 @@ const mapDispatchToProps = dispatch => ({
   onPublicationSearch: query =>
     dispatch(
       toPublicationSearch(
+        {
+          [PARAMETERS.QUERY]: query,
+        },
+        false,
+        true,
+      ),
+    ),
+  onSpecialsSearch: query =>
+    dispatch(
+      toSpecialSearch(
         {
           [PARAMETERS.QUERY]: query,
         },
